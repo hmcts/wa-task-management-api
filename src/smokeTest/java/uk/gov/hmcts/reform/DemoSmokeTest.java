fa0a163c4bcffba208b883831d94815f93bf688c
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-04 09:48:07 +0100

    Added Demo smokeTest

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/DemoSmokeTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoSmokeTest {
+    @Test
+    void exampleOfTest() {
+        assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
+    }
+}
+
