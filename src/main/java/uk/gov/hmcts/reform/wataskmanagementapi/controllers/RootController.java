b286b02c8f98a45c750e74c7591cc545b1113b56
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-07 13:25:15 +0100

    Logging message to test app insights.

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/RootController.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.controllers;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
@@ -7,11 +8,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Default endpoints per application.
  */
 @RestController
 public class RootController {
+
+    private static final Logger LOG = getLogger(RootController.class);
+    private static final String INSTANCE_ID = UUID.randomUUID().toString();
+    private static final String MESSAGE = "Welcome to wa-task-management-api";
 
     private final String testProperty;
 
@@ -33,6 +42,8 @@ public class RootController {
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<String> welcome() {
+        LOG.info("Welcome message '{}' from running instance: {}", MESSAGE, INSTANCE_ID);
+
         return ResponseEntity
             .ok()
             .cacheControl(CacheControl.noCache())
