59fd352dc97b24333d40a18e0eeeb05edcdd2bc0
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-24 18:21:51 +0100

    Suppressing pmd warning

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/TaskController.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.wataskmanagementapi.services.CamundaService;
 
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 @RequestMapping(
     path = "/task",
     consumes = MediaType.APPLICATION_JSON_VALUE,
