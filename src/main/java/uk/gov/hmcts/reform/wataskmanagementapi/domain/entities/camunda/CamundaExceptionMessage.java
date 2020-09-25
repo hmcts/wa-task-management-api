49d5130ba1ebe9c5600eeb274a406ad2d42dfea1
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-25 09:59:43 +0100

    Object mapper tests.

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/camunda/CamundaExceptionMessage.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/camunda/CamundaExceptionMessage.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class CamundaExceptionMessage {
 
     private String type;
