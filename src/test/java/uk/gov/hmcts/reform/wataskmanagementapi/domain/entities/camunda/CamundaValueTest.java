0627553a5018585b2f54bb3262e398c3144dea27
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-28 11:00:22 +0100

    couple more tests to increase codecov coverage

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/camunda/CamundaValueTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/camunda/CamundaValueTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 class CamundaValueTest {
 
@@ -32,5 +33,29 @@ class CamundaValueTest {
 
         assertEquals("json", testObject.getType());
         assertEquals("someJson", testObject.getValue());
+    }
+
+    @Test
+    void hashCode_should_not_be_the_same() {
+
+        CamundaValue<String> testObject1 = CamundaValue.jsonValue("someJson");
+        CamundaValue<String> testObject2 = CamundaValue.jsonValue("someOtherValues");
+        assertNotEquals(testObject1, testObject2);
+        assertNotEquals(testObject2.hashCode(), testObject1.hashCode());
+    }
+    
+    @Test
+    void hashCode_should_be_equal() {
+
+        CamundaValue<String> testObject1 = CamundaValue.jsonValue("someJson");
+        CamundaValue<String> testObject2 = CamundaValue.jsonValue("someJson");
+        assertEquals(testObject1, testObject2);
+        assertEquals(testObject2.hashCode(), testObject1.hashCode());
+    }
+
+    @Test
+    void should_convert_value_to_string() {
+        CamundaValue<String> testObject1 = CamundaValue.jsonValue("someJson");
+        assertEquals("CamundaValue{value='someJson', type='json'}", testObject1.toString());
     }
 }
