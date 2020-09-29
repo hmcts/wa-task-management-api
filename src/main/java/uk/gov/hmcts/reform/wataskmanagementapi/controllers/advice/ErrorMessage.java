0749de3ec4efbb7478d3a2780914c5ea0bd266b1
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-29 10:58:37 +0100

    Added SystemDateProvider.

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/advice/ErrorMessage.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/advice/ErrorMessage.java
@@ -4,19 +4,18 @@ package uk.gov.hmcts.reform.wataskmanagementapi.controllers.advice;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 @Data
 public class ErrorMessage {
 
-    private String timestamp;
+    private LocalDateTime timestamp;
     private String error;
     private int status;
     private String message;
 
-    public ErrorMessage(Exception ex, HttpStatus error) {
-        this.timestamp = ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT);
+    public ErrorMessage(Exception ex, HttpStatus error, LocalDateTime timestamp) {
+        this.timestamp = timestamp;
         this.error = error.getReasonPhrase();
         this.status = error.value();
         this.message = ex.getMessage();
