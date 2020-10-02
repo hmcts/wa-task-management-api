c00a0ca47defde211e887ba4d44f1fa706b8fd48
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-02 14:27:34 +0100

    added keyvault_id

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,18 +6,21 @@ provider "azurerm" {
 data "azurerm_key_vault" "wa_key_vault" {
   name                = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-${var.env}"
-  vault_uri = "https://wa-${var.env}.vault.azure.net/"
+  vault_uri           = "https://wa-${var.env}.vault.azure.net/"
 
 }
 
 data "azurerm_key_vault_secret" "test_law_firm_a_username" {
-  name      = "test-law-firm-a-username"
-  vault_uri = "${data.azurerm_key_vault.wa_key_vault.vault_uri}"
+  name         = "test-law-firm-a-username"
+  vault_uri    = data.azurerm_key_vault.wa_key_vault.vault_uri
+  key_vault_id = data.azurerm_key_vault.wa_key_vault.id
+
 }
 
 data "azurerm_key_vault_secret" "test_law_firm_a_password" {
-  name      = "test-law-firm-a-password"
-  vault_uri = "${data.azurerm_key_vault.wa_key_vault.vault_uri}"
+  name          = "test-law-firm-a-password"
+  vault_uri     = data.azurerm_key_vault.wa_key_vault.vault_uri
+  key_vault_id  = data.azurerm_key_vault.wa_key_vault.id
 }
 
 locals {
