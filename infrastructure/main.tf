4bc7724aeb0c25a371c98e38f05d86b63ad461de
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-02 14:41:52 +0100

    remove vault_uri

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,14 +10,12 @@ data "azurerm_key_vault" "wa_key_vault" {
 
 data "azurerm_key_vault_secret" "test_law_firm_a_username" {
   name         = "test-law-firm-a-username"
-  vault_uri    = data.azurerm_key_vault.wa_key_vault.vault_uri
   key_vault_id = data.azurerm_key_vault.wa_key_vault.id
 
 }
 
 data "azurerm_key_vault_secret" "test_law_firm_a_password" {
   name          = "test-law-firm-a-password"
-  vault_uri     = data.azurerm_key_vault.wa_key_vault.vault_uri
   key_vault_id  = data.azurerm_key_vault.wa_key_vault.id
 }
 
