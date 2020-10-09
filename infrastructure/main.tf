e4578d256a90c162034af72cec9bdb0f6484523a
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-09 13:02:41 +0100

    removed duplicated terraform vault after rebase

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,11 @@ data "azurerm_key_vault" "wa_key_vault" {
   resource_group_name = "${var.product}-${var.env}"
 }
 
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
 data "azurerm_key_vault_secret" "test_law_firm_a_username" {
   name         = "test-law-firm-a-username"
   key_vault_id = data.azurerm_key_vault.wa_key_vault.id
@@ -21,16 +26,6 @@ data "azurerm_key_vault_secret" "test_law_firm_a_password" {
 
 locals {
   S2S_SECRET_TASK_MANAGEMENT_API = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-}
-
-data "azurerm_key_vault" "wa_key_vault" {
-  name                = "${var.product}-${var.env}"
-  resource_group_name = "${var.product}-${var.env}"
-}
-
-data "azurerm_key_vault" "s2s_key_vault" {
-  name                = "s2s-${var.env}"
-  resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
 data "azurerm_key_vault_secret" "s2s_secret" {
