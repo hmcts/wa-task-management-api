5f1ae9f1a4fdf6b3b89c66bf30695e0fc5d4b8f8
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-13 11:06:58 +0100

    Added smoke test to query the index and assert response, content type and body.

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/wataskmanagementapi/SmokeTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.wataskmanagementapi;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+public class SmokeTest {
+
+    private final String targetInstance = System.getProperty("TEST_URL", "http://localhost:8090");
+
+    @Test
+    public void should_check_service_and_return_welcome_message() {
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(targetInstance)
+            .setRelaxedHTTPSValidation()
+            .build();
+
+        given(requestSpecification)
+            .when()
+            .get("/")
+            .then()
+            .assertThat()
+            .statusCode(HttpStatus.OK.value())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(containsString("Welcome to wa-task-management-api"));
+    }
+}
