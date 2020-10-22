6c7859b0318ad6ea2bfe55b07141d206c09b6731
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-22 16:35:10 +0100

    revert test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/GetWelcomeTest.java
@@ -3,23 +3,31 @@ package uk.gov.hmcts.reform.wataskmanagementapi.controllers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootIntegrationBaseTest;
+import uk.gov.hmcts.reform.wataskmanagementapi.Application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class GetWelcomeTest extends SpringBootIntegrationBaseTest {
+@SpringBootTest(classes = {
+    Application.class
+})
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("integration")
+public class GetWelcomeTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
     void welcomeRootEndpoint() throws Exception {
-        MvcResult response = mvc.perform(get("/")).andExpect(status().isOk()).andReturn();
+        MvcResult response = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
 
         assertThat(response.getResponse().getContentAsString()).startsWith("Welcome");
     }
