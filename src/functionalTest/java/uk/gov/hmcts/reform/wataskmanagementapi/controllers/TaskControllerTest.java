10f49ecf7cedf9eec66ae258b504c8f5d03f0dae
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-23 11:23:02 +0100

    revert test assertion as it will be fixed in another pr

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/TaskControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/TaskControllerTest.java
@@ -477,6 +477,6 @@ public class TaskControllerTest extends SpringBootFunctionalBaseTest {
             .post("task/{task-id}/assignee", taskId);
 
         response.then().assertThat()
-            .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            .statusCode(HttpStatus.SERVICE_UNAVAILABLE.value());
     }
 }
