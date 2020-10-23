3ae664318dfae9398b7a2c695ed6c864220ccda0
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-10-23 12:21:21 +0100

    Fix welcome test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/GetWelcomeTest.java
@@ -3,23 +3,15 @@ package uk.gov.hmcts.reform.wataskmanagementapi.controllers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import uk.gov.hmcts.reform.wataskmanagementapi.Application;
+import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootIntegrationBaseTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = {
-    Application.class
-})
-@AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("integration")
-public class GetWelcomeTest {
+public class GetWelcomeTest extends SpringBootIntegrationBaseTest {
 
     @Autowired
     private MockMvc mockMvc;
