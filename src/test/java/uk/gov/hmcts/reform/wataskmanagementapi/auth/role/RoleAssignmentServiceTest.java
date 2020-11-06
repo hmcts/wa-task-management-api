af3e7a6d5412ebe083027d4951a488bcb0104874
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-11-06 15:18:50 +0000

    Added test to cover exception in RoleAssignmentService

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/role/RoleAssignmentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/role/RoleAssignmentServiceTest.java
@@ -1,30 +1,36 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.auth.role;
 
+import feign.FeignException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.Assignment;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.enums.RoleType;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.response.GetRoleAssignmentResponse;
 import uk.gov.hmcts.reform.wataskmanagementapi.clients.RoleAssignmentServiceApi;
+import uk.gov.hmcts.reform.wataskmanagementapi.domain.exceptions.TestFeignClientException;
+import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.InsufficientPermissionsException;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.either;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.wataskmanagementapi.config.SecurityConfiguration.AUTHORIZATION;
 
 @ExtendWith(MockitoExtension.class)
-class RoleManagementServiceTest {
+class RoleAssignmentServiceTest {
 
     @Mock
     private AuthTokenGenerator authTokenGenerator;
@@ -61,6 +67,33 @@ class RoleManagementServiceTest {
             assertThat(roleAssignment.getRoleType(), either(is(RoleType.ORGANISATION)).or(is(RoleType.CASE)));
             assertEquals(idamUserId, roleAssignment.getActorId());
         });
+    }
+
+    @Test
+    void getRolesForUser_should_throw_an_InsufficientPermissionsException_when_feign_exception_is_thrown() {
+
+        String idamUserId = "someIdamUserId";
+        String mockedAuthToken = "authToken";
+        String mockedServiceToken = "serviceToken";
+        HttpHeaders headers = mock(HttpHeaders.class);
+
+        when(headers.getFirst(AUTHORIZATION)).thenReturn(mockedAuthToken);
+        when(authTokenGenerator.generate()).thenReturn(mockedServiceToken);
+
+        TestFeignClientException exception =
+            new TestFeignClientException(
+                HttpStatus.SERVICE_UNAVAILABLE.value(),
+                HttpStatus.SERVICE_UNAVAILABLE.getReasonPhrase()
+            );
+
+        doThrow(exception)
+            .when(roleAssignmentServiceApi).getRolesForUser(idamUserId, mockedAuthToken, mockedServiceToken);
+
+        assertThatThrownBy(() -> roleAssignmentService.getRolesForUser(idamUserId, headers))
+            .isInstanceOf(InsufficientPermissionsException.class)
+            .hasCauseInstanceOf(FeignException.class)
+            .hasMessage("User did not have sufficient permissions to access task");
+
     }
 
     private List<Assignment> createMockRoleAssignments(String idamUserId) {
