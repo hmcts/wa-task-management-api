a4983970acf149733c6ca91d8fd642cc81e12aa4
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-11-20 14:34:08 +0000

    added debug logs

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/access/AccessControlService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/access/AccessControlService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.auth.access;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
@@ -10,6 +11,7 @@ import uk.gov.hmcts.reform.wataskmanagementapi.services.IdamService;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class AccessControlService {
 
@@ -25,6 +27,9 @@ public class AccessControlService {
 
     public AccessControlResponse getRoles(String authToken) {
         UserInfo userInfo = idamService.getUserInfo(authToken);
+        log.info("*** email: " + userInfo.getEmail());
+        log.info("*** name: " + userInfo.getName());
+        log.info("*** uid: " + userInfo.getUid());
         List<Assignment> assignments = roleAssignmentService.getRolesForUser(userInfo.getUid(), authToken);
 
         return new AccessControlResponse(userInfo, assignments);
