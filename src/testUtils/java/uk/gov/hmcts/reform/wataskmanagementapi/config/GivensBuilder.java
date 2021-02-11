661a73e9a61f10f0f769448e006aea29b7701ce7
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-02-11 08:58:18 +0000

    Increase polling timeout to 10s

--- src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/config/GivensBuilder.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/config/GivensBuilder.java
@@ -114,7 +114,7 @@ public class GivensBuilder {
         AtomicReference<List<CamundaTask>> response = new AtomicReference<>();
         await().ignoreException(AssertionError.class)
             .pollInterval(500, MILLISECONDS)
-            .atMost(5, SECONDS)
+            .atMost(10, SECONDS)
             .until(
                 () -> {
                     Response result = camundaApiActions.get(
