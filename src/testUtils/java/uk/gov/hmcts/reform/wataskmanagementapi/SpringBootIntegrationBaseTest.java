cf38bbfa08ec4f5a3f6c28540dded6621297fcde
Vamshidhar Chitti <indvamshi@gmail.com>
2021-03-11 16:33:05 +0000

    Reverted idam-api properties

--- src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/SpringBootIntegrationBaseTest.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/SpringBootIntegrationBaseTest.java
@@ -7,10 +7,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @ActiveProfiles("integration")
 @RunWith(SpringRunner.class)
+@TestPropertySource(properties = {
+    "IDAM_API_URL=https://idam-api.aat.platform.hmcts.net",
+    "OPEN_ID_IDAM_URL=https://idam-web-public.aat.platform.hmcts.net",
+    "CCD_URL=http://ccd-data-store-api-aat.service.core-compute-aat.internal"
+})
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class SpringBootIntegrationBaseTest {
