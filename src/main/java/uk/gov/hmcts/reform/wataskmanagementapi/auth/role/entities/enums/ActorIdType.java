98f82b593157a74b1e9e36f027e84cca8dcf4598
Vamshidhar Chitti <indvamshi@gmail.com>
2021-03-11 17:02:18 +0000

    Rwa 391 - Handling Unknown values returned from RoleAssignment (#159)
    * added unknown enum
    
    * updated role-assigment enums with Uknown value
    
    * reverted idam changes
    
    * fixed tests
    
    * fixed checkstyle
    
    * Added new role attributes. Udpated FTs to handle new roleCategory
    
    * PR suggestions
    
    * add logs
    
    * remove logs
    
    * Ignore tests
    
    * Reverted idam-api properties
    
    Co-authored-by: Alejandro Baeza <alejandro.baeza@enginegroup.com>

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/role/entities/enums/ActorIdType.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/role/entities/enums/ActorIdType.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.enums;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
 public enum ActorIdType {
-    IDAM, CASEPARTY
+    IDAM, CASEPARTY, @JsonEnumDefaultValue UNKNOWN
 }
