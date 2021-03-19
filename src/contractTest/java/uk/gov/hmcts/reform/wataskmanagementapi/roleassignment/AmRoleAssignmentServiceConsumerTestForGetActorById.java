b9aeb8e013f448a6c5353a0c0da28295d8acf92b
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-03-19 09:46:52 +0000

    changes to dsl

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/roleassignment/AmRoleAssignmentServiceConsumerTestForGetActorById.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/roleassignment/AmRoleAssignmentServiceConsumerTestForGetActorById.java
@@ -26,63 +26,63 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AmRoleAssignmentServiceConsumerTestForGetActorById extends SpringBootContractBaseTest {
 
-    private static final String ACTOR_ID = "23486";
-    private static final String RAS_GET_ACTOR_BY_ID_URL = "/am/role-assignments/actors/" + ACTOR_ID;
+    private static final String ORG_ROLE_ACTOR_ID = "00001";
+    private static final String RAS_GET_ACTOR_BY_ID_URL = "/am/role-assignments/actors/";
 
 
     @Pact(provider = "am_role_assignment_service_get_actor_by_id", consumer = "wa_task_management_api")
-    public RequestResponsePact executeGetActorByIdAndGet200(PactDslWithProvider builder) {
+    public RequestResponsePact executeGetActorByIdOrgRoleAssignmentAndGet200(PactDslWithProvider builder) {
 
         return builder
-            .given("An actor with provided id is available in role assignment service")
+            .given("An actor with provided id and organisational role assignment "
+                   + "is available in role assignment service for a WA API")
             .uponReceiving(
                 "Provider receives a GET /am/role-assignments/actors/{user-id} request from a WA API")
-            .path(RAS_GET_ACTOR_BY_ID_URL)
+            .path(RAS_GET_ACTOR_BY_ID_URL + ORG_ROLE_ACTOR_ID)
             .method(HttpMethod.GET.toString())
             .willRespondWith()
             .status(HttpStatus.OK.value())
             .headers(getResponseHeaders())
-            .body(createResponse())
+            .body(createResponseForOrgRoleAssignment())
             .toPact();
     }
 
     @Test
-    @PactTestFor(pactMethod = "executeGetActorByIdAndGet200")
-    void should_get_actor_by_id_and_receive_actor_information_with_200_response(MockServer mockServer)
+    @PactTestFor(pactMethod = "executeGetActorByIdOrgRoleAssignmentAndGet200")
+    void should_get_actor_by_id_and_receive_organisational_role_assignment_with_200_response(MockServer mockServer)
         throws JSONException {
         String actualResponseBody =
             SerenityRest
                 .given()
                 .headers(getHttpHeaders())
-                .get(mockServer.getUrl() + RAS_GET_ACTOR_BY_ID_URL)
+                .get(mockServer.getUrl() + RAS_GET_ACTOR_BY_ID_URL + ORG_ROLE_ACTOR_ID)
                 .then()
                 .extract().asString();
 
         JSONObject jsonResponse = new JSONObject(actualResponseBody);
         JSONArray roleAssignmentResponse = (JSONArray) jsonResponse.get("roleAssignmentResponse");
         JSONObject first = (JSONObject) roleAssignmentResponse.get(0);
-        assertThat(first.get("actorId"), equalTo(ACTOR_ID));
+        assertThat(first.get("actorId"), equalTo(ORG_ROLE_ACTOR_ID));
     }
 
-    private DslPart createResponse() {
+    private DslPart createResponseForOrgRoleAssignment() {
         return newJsonBody(o -> o
             .minArrayLike("roleAssignmentResponse", 1, 1,
                 roleAssignmentResponse -> roleAssignmentResponse
                     .stringType("id", "7694d1ec-1f0b-4256-82be-a8309ab99136")
                     .stringValue("actorIdType", "IDAM")
-                    .stringValue("actorId", ACTOR_ID)
+                    .stringValue("actorId", ORG_ROLE_ACTOR_ID)
                     .stringValue("roleType", "ORGANISATION")
-                    .stringValue("roleName", "senior-tribunal-caseworker")
-                    .stringValue("classification", "PRIVATE")
+                    .stringType("roleName", "tribunal-caseworker")
+                    .stringValue("classification", "PUBLIC")
                     .stringValue("grantType", "STANDARD")
                     .stringType("roleCategory", "LEGAL_OPERATIONS")
                     .booleanValue("readOnly", false)
                     .stringType("created", "2021-03-11T14:22:12.961474Z")
                     .object("attributes", attribute -> attribute
-                        .stringType("region", "north-east")
-                        .stringType("contractType", "SALARIED")
-                        .stringType("caseId", "1234567890123456"))
-
+                        .stringType("primaryLocation", "198444")
+                        .stringType("jurisdiction", "IA")
+                    )
             )).build();
     }
 
