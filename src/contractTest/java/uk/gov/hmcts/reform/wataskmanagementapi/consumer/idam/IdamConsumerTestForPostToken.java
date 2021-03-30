0612449ffc451fd179100cc787f3bad0093f5021
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-03-30 10:12:05 +0100

    Update idam pact tests to new format, Update pact related config and libraries in build.gradle, updated jenkins cnp

---
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/consumer/idam/IdamConsumerTestForPostToken.java
@@ -0,0 +1,86 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.consumer.idam;
+
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import com.google.common.collect.ImmutableMap;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractBaseTest;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.Token;
+import uk.gov.hmcts.reform.wataskmanagementapi.clients.IdamWebApi;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ContextConfiguration(classes = {uk.gov.hmcts.reform.wataskmanagementapi.consumer.idam.IdamConsumerApplication.class})
+@TestPropertySource(locations = {"classpath:application.properties"}, properties = {"idam.baseUrl=http://localhost:8892"})
+public class IdamConsumerTestForPostToken extends SpringBootContractBaseTest {
+
+    @Autowired
+    IdamWebApi idamApi;
+
+    @Pact(provider = "idamApi_oidc", consumer = "wa_taskManagementApi")
+    public RequestResponsePact generatePactFragmentToken(PactDslWithProvider builder) throws JSONException {
+
+        Map<String, String> responseheaders = ImmutableMap.<String, String>builder()
+            .put("Content-Type", "application/json")
+            .build();
+
+        return builder
+            .given("a token is requested")
+            .uponReceiving("Provider receives a POST /o/token request from a WA API")
+            .path("/o/token")
+            .method(HttpMethod.POST.toString())
+            .body("redirect_uri=http%3A%2F%2Fwww.dummy-pact-service.com%2Fcallback"
+                  + "&client_id=" + PACT_TEST_CLIENT_ID_VALUE
+                  + "&grant_type=password"
+                  + "&username=" + PACT_TEST_EMAIL_VALUE
+                  + "&password=" + PACT_TEST_PASSWORD_VALUE
+                  + "&client_secret=" + PACT_TEST_CLIENT_SECRET_VALUE
+                  + "&scope=" + PACT_TEST_SCOPES_VALUE,
+                "application/x-www-form-urlencoded")
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .headers(responseheaders)
+            .body(createAuthResponse())
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "generatePactFragmentToken")
+    public void verifyIdamUserDetailsRolesPactToken() {
+
+        Map<String, String> tokenRequestMap = buildTokenRequestMap();
+        Token token = idamApi.token(tokenRequestMap);
+        assertEquals("eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre", token.getAccessToken(), "Token is not expected");
+    }
+
+    private Map<String, String> buildTokenRequestMap() {
+        Map<String, String> tokenRequestMap = ImmutableMap.<String, String>builder()
+            .put("redirect_uri", "http://www.dummy-pact-service.com/callback")
+            .put("client_id", PACT_TEST_CLIENT_ID_VALUE)
+            .put("client_secret", PACT_TEST_CLIENT_SECRET_VALUE)
+            .put("grant_type", "password")
+            .put("username", PACT_TEST_EMAIL_VALUE)
+            .put("password", PACT_TEST_PASSWORD_VALUE)
+            .put("scope", PACT_TEST_ROLES_VALUE)
+            .build();
+        return tokenRequestMap;
+    }
+
+    private PactDslJsonBody createAuthResponse() {
+
+        return new PactDslJsonBody()
+            .stringType("access_token", "eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre")
+            .stringType("scope", "openid roles profile");
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/consumer/idam/IdamConsumerTestForPostUserInfo.java
@@ -0,0 +1,64 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.consumer.idam;
+
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslJsonRootValue;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractBaseTest;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.UserInfo;
+import uk.gov.hmcts.reform.wataskmanagementapi.clients.IdamWebApi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@ContextConfiguration(classes = {uk.gov.hmcts.reform.wataskmanagementapi.consumer.idam.IdamConsumerApplication.class})
+@TestPropertySource(locations = {"classpath:application.properties"}, properties = {"idam.baseUrl=http://localhost:8892"})
+public class IdamConsumerTestForPostUserInfo extends SpringBootContractBaseTest {
+
+    @Autowired
+    IdamWebApi idamApi;
+
+    @Pact(provider = "idamApi_oidc", consumer = "wa_taskManagementApi")
+    public RequestResponsePact generatePactFragmentUserInfo(PactDslWithProvider builder) throws JSONException {
+
+        return builder
+            .given("userinfo is requested")
+            .uponReceiving("A request for a UserInfo")
+            .path("/o/userinfo")
+            .method("GET")
+            .matchHeader("Authorization", AUTH_TOKEN)
+            .matchHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .willRespondWith()
+            .status(200)
+            .body(createUserDetailsResponse())
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "generatePactFragmentUserInfo")
+    public void verifyIdamUserDetailsRolesPactUserInfo() {
+        UserInfo userInfo = idamApi.userInfo(AUTH_TOKEN);
+        assertEquals(PACT_TEST_EMAIL_VALUE, userInfo.getEmail(), "User is not Case Officer");
+    }
+
+    private PactDslJsonBody createUserDetailsResponse() {
+        PactDslJsonArray array = new PactDslJsonArray().stringValue("caseofficer-ia");
+
+        return new PactDslJsonBody()
+            .stringType("uid", "1111-2222-3333-4567")
+            .stringValue("sub", "ia-caseofficer@fake.hmcts.net")
+            .stringValue("givenName", "Case")
+            .stringValue("familyName", "Officer")
+            .minArrayLike("roles", 1, PactDslJsonRootValue.stringType("caseworker-ia-legalrep-solicitor"), 1)
+            .stringType("IDAM_ADMIN_USER");
+    }
+}
