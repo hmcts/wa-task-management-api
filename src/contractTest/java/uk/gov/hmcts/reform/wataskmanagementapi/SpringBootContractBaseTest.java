f3309d76bdb9a780426d4b029b0d969bb0bee60d
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-04-21 10:05:29 +0100

    Update SpringBootContractBaseTest.java

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/SpringBootContractBaseTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/SpringBootContractBaseTest.java
@@ -26,30 +26,15 @@ public abstract class SpringBootContractBaseTest {
     public static final String PACT_TEST_SCOPES_VALUE = "openid profile roles";
     public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
     public static final String AUTH_TOKEN = "Bearer someAuthorizationToken";
-<<<<<<< HEAD
     public static final String SERVICE_AUTH_TOKEN = "Bearer someServiceAuthorizationToken";
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 6e477b6c078906bb1812d0fd38917be42e32989d
->>>>>>> 7275f41d0b8e5ccdecd242b16d1a8097fec3e1e6
     public static final String EXPERIMENTAL = "true";
 
 
     public HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
-<<<<<<< HEAD
         headers.add(SERVICE_AUTHORIZATION, SERVICE_AUTH_TOKEN);
         headers.add(AUTHORIZATION, AUTH_TOKEN);
         headers.add("Experimental", EXPERIMENTAL);
-<<<<<<< HEAD
-=======
-=======
-        headers.add("ServiceAuthorization", SERVICE_BEARER_TOKEN);
-        headers.add("Experimental", EXPERIMENTAL);
-        headers.add("Authorization", AUTHORIZATION_BEARER_TOKEN);
->>>>>>> 6e477b6c078906bb1812d0fd38917be42e32989d
->>>>>>> 7275f41d0b8e5ccdecd242b16d1a8097fec3e1e6
         return headers;
     }
 }
