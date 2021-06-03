06505104ea94517531e0216609378f9f1095735e
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-06-03 11:10:26 +0100

    checkstyle fixes

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/access/AccessControlService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/access/AccessControlService.java
@@ -33,7 +33,9 @@ public class AccessControlService {
 
         //Safe-guard
         if (assignments.isEmpty()) {
-            throw new NoRoleAssignmentsFoundException("User did not have sufficient permissions to perform this action");
+            throw new NoRoleAssignmentsFoundException(
+                "User did not have sufficient permissions to perform this action"
+            );
         }
 
         assignments.forEach(role -> log.debug("Response from role assignment service '{}'", role.toString()));
