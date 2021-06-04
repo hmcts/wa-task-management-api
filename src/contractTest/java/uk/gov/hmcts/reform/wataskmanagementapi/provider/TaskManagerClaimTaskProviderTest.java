d9647c149821376cacd0248b15ac51bac61c594d
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-06-04 17:03:26 +0100

    Fix imports

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerClaimTaskProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerClaimTaskProviderTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.AccessControlService;
