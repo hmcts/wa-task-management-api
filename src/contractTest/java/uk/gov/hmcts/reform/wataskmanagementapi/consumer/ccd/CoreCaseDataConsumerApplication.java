e83f7fb0129126da34941d44a0f86aff5cbdf137
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-06-22 10:59:34 +0100

    Fix mocks

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/consumer/ccd/CoreCaseDataConsumerApplication.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/consumer/ccd/CoreCaseDataConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 
 @SpringBootApplication
@@ -13,7 +14,7 @@ import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 public class CoreCaseDataConsumerApplication {
 
     @MockBean
-    CoreCaseDataApi coreCaseDataApi;
+    AuthTokenGenerator authTokenGenerator;
 
     @MockBean
     RestTemplate restTemplate;
