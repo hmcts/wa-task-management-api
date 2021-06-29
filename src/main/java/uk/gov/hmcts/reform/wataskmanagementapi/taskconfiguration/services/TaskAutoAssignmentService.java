ab287a7969905e13ae747b2c5bd5e36b89efb90f
david.crespo <david.crespo@hmcts.net>
2021-06-29 19:09:01 +0100

    fix ITs

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/taskconfiguration/services/TaskAutoAssignmentService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/taskconfiguration/services/TaskAutoAssignmentService.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.auth.role.RoleAssignmentService;
+import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.auth.role.TaskConfigurationRoleAssignmentService;
 import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.auth.role.entities.RoleAssignment;
 import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.domain.entities.configuration.AutoAssignmentResult;
 import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.domain.entities.configuration.TaskToConfigure;
@@ -16,13 +16,13 @@ import static uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.domain.e
 @Component
 public class TaskAutoAssignmentService {
 
-    private final RoleAssignmentService roleAssignmentService;
-    private final CamundaService camundaService;
+    private final TaskConfigurationRoleAssignmentService taskConfigurationRoleAssignmentService;
+    private final TaskConfigurationCamundaService taskConfigurationCamundaService;
 
-    public TaskAutoAssignmentService(RoleAssignmentService roleAssignmentService,
-                                     CamundaService camundaService) {
-        this.roleAssignmentService = roleAssignmentService;
-        this.camundaService = camundaService;
+    public TaskAutoAssignmentService(TaskConfigurationRoleAssignmentService taskConfigurationRoleAssignmentService,
+                                     TaskConfigurationCamundaService taskConfigurationCamundaService) {
+        this.taskConfigurationRoleAssignmentService = taskConfigurationRoleAssignmentService;
+        this.taskConfigurationCamundaService = taskConfigurationCamundaService;
     }
 
     public void autoAssignTask(TaskToConfigure taskToConfigure, String currentTaskState) {
@@ -30,7 +30,7 @@ public class TaskAutoAssignmentService {
     }
 
     public AutoAssignmentResult getAutoAssignmentVariables(TaskToConfigure task) {
-        List<RoleAssignment> roleAssignments = roleAssignmentService.searchRolesByCaseId(task.getCaseId());
+        List<RoleAssignment> roleAssignments = taskConfigurationRoleAssignmentService.searchRolesByCaseId(task.getCaseId());
 
         if (roleAssignments.isEmpty()) {
             // the user did not have specific role assignment for this case
@@ -46,11 +46,11 @@ public class TaskAutoAssignmentService {
     private void updateTaskStateAndSetAssignee(TaskToConfigure taskToConfigure,
                                                String currentTaskState) {
 
-        List<RoleAssignment> roleAssignments = roleAssignmentService.searchRolesByCaseId(taskToConfigure.getCaseId());
+        List<RoleAssignment> roleAssignments = taskConfigurationRoleAssignmentService.searchRolesByCaseId(taskToConfigure.getCaseId());
         log.info("Role assignments retrieved for caseId '{}'", taskToConfigure.getCaseId());
         if (roleAssignments.isEmpty()) {
             log.info("The case did not have specific users assigned, Setting task state to '{}'", UNASSIGNED);
-            camundaService.updateTaskStateTo(taskToConfigure.getId(), UNASSIGNED);
+            taskConfigurationCamundaService.updateTaskStateTo(taskToConfigure.getId(), UNASSIGNED);
         } else {
             String assignee = roleAssignments.get(0).getActorId();
             log.info(
@@ -58,7 +58,7 @@ public class TaskAutoAssignmentService {
                 ASSIGNED
             );
 
-            camundaService.assignTask(
+            taskConfigurationCamundaService.assignTask(
                 taskToConfigure.getId(),
                 assignee,
                 currentTaskState
