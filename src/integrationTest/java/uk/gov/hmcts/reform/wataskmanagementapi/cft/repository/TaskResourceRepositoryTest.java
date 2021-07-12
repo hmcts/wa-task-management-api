80bab2f9fd975aa4136052873891e99cbef2ae02
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-07-12 12:27:30 +0100

    Fix test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/repository/TaskResourceRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/repository/TaskResourceRepositoryTest.java
@@ -24,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest
 public class TaskResourceRepositoryTest extends SpringBootIntegrationBaseTest {
 
     @Autowired
