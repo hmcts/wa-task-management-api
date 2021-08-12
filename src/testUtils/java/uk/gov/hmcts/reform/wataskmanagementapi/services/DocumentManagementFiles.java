1a051e50eb00598018614e1cd67f7d750f6488d4
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2021-08-12 14:52:46 +0100

    RWA-694: Add CCD Case document service (#313)

---
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/services/DocumentManagementFiles.java
@@ -0,0 +1,97 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.services;
+
+import io.restassured.http.Headers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.UserInfo;
+import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.documents.Document;
+import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.documents.DocumentNames;
+import uk.gov.hmcts.reform.wataskmanagementapi.utils.BinaryResourceLoader;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static uk.gov.hmcts.reform.wataskmanagementapi.config.SecurityConfiguration.AUTHORIZATION;
+import static uk.gov.hmcts.reform.wataskmanagementapi.config.SecurityConfiguration.SERVICE_AUTHORIZATION;
+
+
+@Component
+public class DocumentManagementFiles {
+
+    private final Map<String, Document> storedDocuments = new ConcurrentHashMap<>();
+    private Collection<Resource> documentResources;
+    @Autowired
+    private AuthorizationHeadersProvider authorizationHeadersProvider;
+    @Autowired
+    private DocumentManagementUploader documentManagementUploader;
+
+    public void prepare() throws IOException {
+        documentResources =
+            BinaryResourceLoader
+                .load("/documents/*")
+                .values();
+    }
+
+    public Document uploadDocument(DocumentNames document) {
+
+        Optional<Resource> maybeResource = documentResources.stream()
+            .filter(res -> {
+                String filename = formatFileName(res.getFilename());
+
+                return filename.equals(document.toString());
+            }).findFirst();
+
+        if (maybeResource.isPresent()) {
+
+            Resource documentResource = maybeResource.get();
+
+            String filename = documentResource.getFilename().toUpperCase();
+
+            String contentType;
+
+            if (filename.endsWith(".PDF")) {
+                contentType = "application/pdf";
+
+            } else if (filename.endsWith(".DOC")) {
+                contentType = "application/msword";
+
+            } else if (filename.endsWith(".DOCX")) {
+                contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+            } else {
+                throw new RuntimeException("Missing content type mapping for document: " + filename);
+            }
+
+            Headers headers = authorizationHeadersProvider.getLawFirmAuthorization();
+            String userToken = headers.getValue(AUTHORIZATION);
+            String serviceToken = headers.getValue(SERVICE_AUTHORIZATION);
+            UserInfo userInfo = authorizationHeadersProvider.getUserInfo(userToken);
+
+            return documentManagementUploader.upload(
+                documentResource,
+                contentType,
+                userToken,
+                serviceToken,
+                userInfo
+            );
+        } else {
+            throw new IllegalStateException(
+                String.format("Resource for document '{}' not found", document));
+        }
+    }
+
+    public Document getDocument(DocumentNames document) {
+        return uploadDocument(document);
+    }
+
+    private String formatFileName(String fileName) {
+        return fileName
+            .replace(".", "_")
+            .replace("-", "_")
+            .toUpperCase();
+    }
+}
