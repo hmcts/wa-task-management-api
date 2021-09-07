2e9998b745d676791fd855aa998782123d7ea307
david.crespo <david.crespo@hmcts.net>
2021-09-07 16:15:38 +0100

    fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/idam/entities/UserInfo.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/idam/entities/UserInfo.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import java.util.List;
 
