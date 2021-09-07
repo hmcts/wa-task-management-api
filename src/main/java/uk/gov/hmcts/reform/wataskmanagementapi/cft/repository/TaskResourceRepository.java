4745510c39afc59ae11a3b6bd1d35fb9b6e34afb
david.crespo <david.crespo@hmcts.net>
2021-09-07 13:53:49 +0100

    add notnull to get rid of intelliJ warnings

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/repository/TaskResourceRepository.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/repository/TaskResourceRepository.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.wataskmanagementapi.cft.repository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskResource;
@@ -14,11 +15,12 @@ import javax.persistence.QueryHint;
 @Repository
 public interface TaskResourceRepository extends CrudRepository<TaskResource, String> {
 
-    @Override
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "0")})
     @Transactional
-    Optional<TaskResource> findById(String id);
+    @NonNull
+    @Override
+    Optional<TaskResource> findById(@NonNull String id);
 
     Optional<TaskResource> getByTaskId(String id);
 
