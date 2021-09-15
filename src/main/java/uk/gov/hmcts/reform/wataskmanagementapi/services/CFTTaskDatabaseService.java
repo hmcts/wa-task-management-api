8fcc61e2ad8d7254fe093caf845a945f7dff70bc
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-09-15 15:52:45 +0100

    Fix tests

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskDatabaseService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskDatabaseService.java
@@ -2,16 +2,10 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.repository.TaskResourceRepository;
-import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.v2.DatabaseConflictException;
-import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.v2.enums.ErrorMessages;
 
 import java.util.Optional;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Slf4j
 @Service
