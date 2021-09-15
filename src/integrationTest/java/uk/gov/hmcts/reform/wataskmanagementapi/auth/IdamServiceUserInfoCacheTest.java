94751ec571f2517f91b008f5f6adf4eaf6574d73
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2021-09-15 15:35:53 +0100

    RWA-719 fixed review comments, added caffeine cache for idam service, added junit test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/IdamServiceUserInfoCacheTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/auth/IdamServiceUserInfoCacheTest.java
@@ -1,20 +1,27 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.auth;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.IdamService;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.UserInfo;
 import uk.gov.hmcts.reform.wataskmanagementapi.clients.IdamWebApi;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles({"integration"})
-@Disabled("cast exception referring to the UserInfo pojo")
 public class IdamServiceUserInfoCacheTest {
+
+    private final String bearerAccessToken1 = "some bearer access token1";
+    private final String bearerAccessToken2 = "some bearer access token2";
+    private final String bearerAccessToken3 = "some bearer access token3";
+    private final String bearerAccessToken4 = "some bearer access token4";
 
     @MockBean
     private IdamWebApi idamWebApi;
@@ -24,11 +31,38 @@ public class IdamServiceUserInfoCacheTest {
 
     @Test
     void getUserInfoIsCached() {
-        idamService.getUserInfo("some user token");
-        idamService.getUserInfo("some user token");
-        idamService.getUserInfo("some user token");
 
-        verify(idamWebApi).userInfo("some user token");
+        when(idamWebApi.userInfo(anyString()))
+            .thenReturn(UserInfo.builder()
+                .uid("some user id1")
+                .build());
+
+        idamService.getUserInfo(bearerAccessToken1);
+        idamService.getUserInfo(bearerAccessToken1);
+        idamService.getUserInfo(bearerAccessToken2);
+        idamService.getUserInfo(bearerAccessToken2);
+
+
+        verify(idamWebApi, times(1)).userInfo(bearerAccessToken1);
+        verify(idamWebApi, times(1)).userInfo(bearerAccessToken2);
+    }
+
+    @Test
+    void getUserIdIsCached() {
+
+        when(idamWebApi.userInfo(anyString()))
+            .thenReturn(UserInfo.builder()
+                .uid("some user id1")
+                .build());
+
+        idamService.getUserId(bearerAccessToken3);
+        idamService.getUserId(bearerAccessToken3);
+        idamService.getUserId(bearerAccessToken4);
+        idamService.getUserId(bearerAccessToken4);
+
+
+        verify(idamWebApi, times(1)).userInfo(bearerAccessToken3);
+        verify(idamWebApi, times(1)).userInfo(bearerAccessToken4);
     }
 
 }
