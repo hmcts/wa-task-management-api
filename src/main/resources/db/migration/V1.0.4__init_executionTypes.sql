c9a6bd3057c1771f8eadae3cd56778ee36a207bd
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-09-15 14:23:17 +0100

    Add migration script

---
+++ src/main/resources/db/migration/V1.0.4__init_executionTypes.sql
@@ -0,0 +1,11 @@
+INSERT INTO execution_types(execution_code, execution_name, description)
+VALUES ('MANUAL',
+        'Manual',
+        'The task is carried out manually, and must be completed by the user in the task management UI.'),
+       ('BUILT_IN',
+        'Built In',
+        'The application through which the task is presented to the user knows how to launch and complete this task, based on its formKey.'),
+       ('CASE_EVENT',
+        'Case Management Task',
+        'The task requires a case management event to be executed by the user. (Typically this will be in CCD.');
+COMMIT;
