0d06c7370132ce1fbed4cdf4e05347b4aa9e23bd
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-09-22 10:39:41 +0100

    Fix after rebase

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/entities/TaskResource.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/entities/TaskResource.java
@@ -26,6 +26,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @ToString
 @Getter
