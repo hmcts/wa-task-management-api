47b87cb29f13dbb82164324cb4e3d06f543b675c
Vamshidhar Chitti <indvamshi@gmail.com>
2021-09-28 17:32:28 +0100

    Rwa 592 master rebase final (#354)
    * Added criteria builder to generate dynamic query for grant type basic
    
    * Upgraded chart version
    
    * Bumping chart version
    
    * Reverted chart version
    
    * Updated chart version
    
    * Updated builder to support all grantTypes
    
    * Updated builder to support sorting,pagination
    
    * code review comments fixed
    
    * CVE Fix
    
    * CVE Fix
    
    * Suppressing CVE-2020-23171
    
    * Suppressing CVE-2020-23171
    
    * Updated builder to support exclude grant type
    
    * Added test for inActive role
    
    * Added test for challenged and excluded
    
    * Added error scenarios
    
    * updated tests
    
    * code review comments fixed
    
    * Added sql scripts for work types
    
    * Fixed conflicts with Master
    
    * Modified flyway scriptsh
    
    * Fixed integration tests
    
    * Fixed integration tests
    
    * Hibernate type cleanup
    
    * Reorganised flyway scripts
    
    Co-authored-by: hmcts-jenkins-wa <69045096+hmcts-jenkins-wa[bot]@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/query/CftQueryService.java
@@ -0,0 +1,44 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.cft.query;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.permission.entities.PermissionTypes;
+import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskResource;
+import uk.gov.hmcts.reform.wataskmanagementapi.cft.repository.TaskResourceRepository;
+import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.SearchTaskRequest;
+
+import java.util.List;
+
+@Service
+public class CftQueryService {
+
+    private final TaskResourceRepository taskResourceRepository;
+
+    public CftQueryService(TaskResourceRepository taskResourceRepository) {
+        this.taskResourceRepository = taskResourceRepository;
+    }
+
+    public List<TaskResource> getAllTasks(
+        int firstResult,
+        int maxResults,
+        SearchTaskRequest searchTaskRequest,
+        AccessControlResponse accessControlResponse,
+        List<PermissionTypes> permissionsRequired
+    ) {
+        final Specification<TaskResource> taskResourceSpecification = TaskResourceSpecification
+            .buildTaskQuery(searchTaskRequest, accessControlResponse, permissionsRequired);
+
+        Sort sort = SortQuery.sortByFields(searchTaskRequest);
+
+        Pageable page = PageRequest.of(firstResult, maxResults, sort);
+        final Page<TaskResource> taskResources = taskResourceRepository.findAll(taskResourceSpecification, page);
+
+        return taskResources.toList();
+    }
+
+}
