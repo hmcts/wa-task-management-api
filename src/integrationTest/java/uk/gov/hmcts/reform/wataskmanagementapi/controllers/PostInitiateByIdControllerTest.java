5a2cb90aece0d33889a6ae2e17e1b032df9b2fd1
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-10-04 16:51:32 +0100

    resolve conflict

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostInitiateByIdControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostInitiateByIdControllerTest.java
@@ -173,11 +173,13 @@ class PostInitiateByIdControllerTest extends SpringBootIntegrationBaseTest {
                     stringValue("Read,Refer,Own,Manage,Cancel"),
                     stringValue("IA,WA"),
                     null,
+                    null,
                     null
                 ),
                 new PermissionsDmnEvaluationResponse(
                     stringValue("senior-tribunal-caseworker"),
                     stringValue("Read,Refer,Own,Manage,Cancel"),
+                    null,
                     null,
                     null,
                     null
