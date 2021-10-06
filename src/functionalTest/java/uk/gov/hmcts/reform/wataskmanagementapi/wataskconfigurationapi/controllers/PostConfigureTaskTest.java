26bbbee398484a55bb023b5b88aee6c752f8e6ef
david.crespo <david.crespo@hmcts.net>
2021-10-06 19:21:19 +0100

    adjust legacy FTs

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostConfigureTaskTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostConfigureTaskTest.java
@@ -74,8 +74,7 @@ public class PostConfigureTaskTest extends SpringBootFunctionalBaseTest {
             .body("caseTypeId.value", is("Asylum"))
             .body("title.value", is("task name"))
             .body("hasWarnings.value", is(false))
-            .body("tribunal-caseworker.value", is("Read,Refer,Own,Manage,Cancel"))
-            .body("senior-tribunal-caseworker.value", is("Read,Refer,Own,Manage,Cancel"));
+            .body("task-supervisor.value", is("Read,Refer,Manage,Cancel"));
     }
 
     @Test
@@ -118,8 +117,7 @@ public class PostConfigureTaskTest extends SpringBootFunctionalBaseTest {
             .body("jurisdiction.value", is("IA"))
             .body("caseTypeId.value", is("Asylum"))
             .body("title.value", is("task name"))
-            .body("tribunal-caseworker.value", is("Read,Refer,Own,Manage,Cancel"))
-            .body("senior-tribunal-caseworker.value", is("Read,Refer,Own,Manage,Cancel"));
+            .body("task-supervisor.value", is("Read,Refer,Manage,Cancel"));
     }
 
 }
