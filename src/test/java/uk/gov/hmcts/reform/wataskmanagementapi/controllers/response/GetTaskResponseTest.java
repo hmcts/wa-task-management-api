a5ecf8880e7a996fb7e8f6132289b63c7a67835d
david.crespo <david.crespo@hmcts.net>
2021-10-25 10:17:29 +0100

    fix issue iw

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/response/GetTaskResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/response/GetTaskResponseTest.java
@@ -36,7 +36,6 @@ class GetTaskResponseTest {
             .testing(Method.GETTER)
             .testing(Method.CONSTRUCTOR)
             .testing(Method.TO_STRING)
-            .testing(Method.EQUALS)
             .areWellImplemented();
     }
 
