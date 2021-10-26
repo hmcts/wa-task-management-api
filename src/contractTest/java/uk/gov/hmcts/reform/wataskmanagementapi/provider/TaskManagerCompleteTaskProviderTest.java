6be63869ca87d6785264d7e9a8a8de9d7eac6c6e
Prasad Pasupuleti <prasad@Prasads-MacBook-Pro.local>
2021-10-26 15:50:40 +0100

    updated function name

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerCompleteTaskProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerCompleteTaskProviderTest.java
@@ -6,14 +6,17 @@ import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.AccessControlService;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
@@ -32,14 +35,14 @@ import static org.mockito.Mockito.when;
 @ExtendWith(SpringExtension.class)
 @Provider("wa_task_management_api_complete_task_by_id")
 //Uncomment this and comment the @PacBroker line to test TaskManagerClaimTaskConsumerTest local consumer.
-//@PactFolder("pacts")
-@PactBroker(
-    scheme = "${PACT_BROKER_SCHEME:http}",
-    host = "${PACT_BROKER_URL:localhost}",
-    port = "${PACT_BROKER_PORT:9292}",
-    consumerVersionSelectors = {
-        @VersionSelector(tag = "master")}
-)
+@PactFolder("pacts")
+//@PactBroker(
+//    scheme = "${PACT_BROKER_SCHEME:http}",
+//    host = "${PACT_BROKER_URL:localhost}",
+//    port = "${PACT_BROKER_PORT:9292}",
+//    consumerVersionSelectors = {
+//        @VersionSelector(tag = "master")}
+//)
 @Import(TaskManagementProviderTestConfiguration.class)
 @IgnoreNoPactsToVerify
 public class TaskManagerCompleteTaskProviderTest {
@@ -55,6 +58,9 @@ public class TaskManagerCompleteTaskProviderTest {
 
     @Mock
     private ClientAccessControlService clientAccessControlService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -76,8 +82,10 @@ public class TaskManagerCompleteTaskProviderTest {
         if (context != null) {
             context.setTarget(testTarget);
         }
-
-    }
+        testTarget.setMessageConverters((
+            new MappingJackson2HttpMessageConverter(
+                objectMapper
+            )));    }
 
     @State({"complete a task using taskId"})
     public void completeTaskById() {
@@ -90,14 +98,14 @@ public class TaskManagerCompleteTaskProviderTest {
     }
 
     private void setInitMockWithoutPrivilegedAccess() {
-        doNothing().when(taskManagementService).claimTask(any(), any());
+        doNothing().when(taskManagementService).completeTask(any(), any());
         AccessControlResponse accessControlResponse = mock((AccessControlResponse.class));
         when(accessControlService.getRoles(anyString())).thenReturn(accessControlResponse);
         when(clientAccessControlService.hasPrivilegedAccess(any(), any())).thenReturn(false);
     }
 
     private void setInitMockWithPrivilegedAccess() {
-        doNothing().when(taskManagementService).claimTask(any(), any());
+        doNothing().when(taskManagementService).completeTaskWithPrivilegeAndCompletionOptions(any(), any(),any());
         AccessControlResponse accessControlResponse = mock((AccessControlResponse.class));
         when(accessControlService.getRoles(anyString())).thenReturn(accessControlResponse);
         when(clientAccessControlService.hasPrivilegedAccess(any(), any())).thenReturn(true);
