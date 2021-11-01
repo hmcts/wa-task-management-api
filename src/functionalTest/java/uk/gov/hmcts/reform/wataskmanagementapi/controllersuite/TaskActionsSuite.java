d303470243ea1ca13fe5c4b59409a28734c87bc7
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-11-01 12:21:03 +0000

    less parallelism

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllersuite/TaskActionsSuite.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllersuite/TaskActionsSuite.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.controllersuite;
 
 import org.junit.Test;
-import org.junit.experimental.ParallelComputer;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
