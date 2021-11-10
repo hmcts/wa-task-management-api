32535d0f569d1054d4ef353520f9992289c6c255
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-11-10 14:01:08 +0000

    Add ft tests

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskInitiateByIdControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskInitiateByIdControllerTest.java
@@ -67,7 +67,106 @@ public class PostTaskInitiateByIdControllerTest extends SpringBootFunctionalBase
             authenticationHeaders
         );
 
-        result.prettyPrint();
+        //Note: this is the TaskResource.class
+        result.then().assertThat()
+            .statusCode(HttpStatus.CREATED.value())
+            .and()
+            .body("task_id", equalTo(taskId))
+            .body("task_name", equalTo("follow Up Overdue Reasons For Appeal"))
+            .body("task_type", equalTo("followUpOverdueReasonsForAppeal"))
+            .body("state", equalTo("UNASSIGNED"))
+            .body("task_system", equalTo("SELF"))
+            .body("security_classification", equalTo("PUBLIC"))
+            .body("title", equalTo("follow Up Overdue Reasons For Appeal"))
+            .body("created", notNullValue())
+            .body("due_date_time", notNullValue())
+            .body("auto_assigned", equalTo(false))
+            .body("has_warnings", equalTo(false))
+            .body("case_id", equalTo(taskVariables.getCaseId()))
+            .body("case_type_id", equalTo("Asylum"))
+            .body("case_name", equalTo("Bob Smith"))
+            .body("case_category", equalTo("Protection"))
+            .body("jurisdiction", equalTo("IA"))
+            .body("region", equalTo("1"))
+            .body("location", equalTo("765324"))
+            .body("location_name", equalTo("Taylor House"))
+            .body("execution_type_code.execution_code", equalTo("CASE_EVENT"))
+            .body("execution_type_code.execution_name", equalTo("Case Management Task"))
+            .body(
+                "execution_type_code.description",
+                equalTo("The task requires a case management event to be executed by the user. "
+                        + "(Typically this will be in CCD.)")
+            )
+            .body("task_role_resources[0].task_role_id", notNullValue())
+            .body("task_role_resources[0].task_id", equalTo(taskId))
+            .body(
+                "task_role_resources[0].role_name",
+                anyOf(is("tribunal-caseworker"), is("senior-tribunal-caseworker"))
+            )
+            .body("task_role_resources[0].read", equalTo(true))
+            .body("task_role_resources[0].own", equalTo(true))
+            .body("task_role_resources[0].execute", equalTo(false))
+            .body("task_role_resources[0].cancel", equalTo(true))
+            .body("task_role_resources[0].refer", equalTo(true))
+            .body("task_role_resources[0].authorizations", equalTo(emptyList()))
+            .body("task_role_resources[0].auto_assignable", equalTo(false))
+            .body(
+                "task_role_resources[0].role_category",
+                anyOf(is("LEGAL_OPERATIONS"), is(nullValue()))
+            )
+            .body("task_role_resources[1].task_role_id", notNullValue())
+            .body("task_role_resources[1].task_id", equalTo(taskId))
+            .body(
+                "task_role_resources[1].role_name",
+                anyOf(is("tribunal-caseworker"), is("senior-tribunal-caseworker"))
+            )
+            .body("task_role_resources[1].read", equalTo(true))
+            .body("task_role_resources[1].own", equalTo(true))
+            .body("task_role_resources[1].execute", equalTo(false))
+            .body("task_role_resources[1].cancel", equalTo(true))
+            .body("task_role_resources[1].refer", equalTo(true))
+            .body("task_role_resources[1].authorizations", equalTo(emptyList()))
+            .body("task_role_resources[1].auto_assignable", equalTo(false))
+            .body("task_role_resources[1].role_category",
+                anyOf(is("LEGAL_OPERATIONS"), is(nullValue())))
+            .body("work_type_resource.id", equalTo("decision_making_work"))
+            .body("work_type_resource.label", equalTo("Decision-making work"));
+
+        assertions.taskVariableWasUpdated(
+            taskVariables.getProcessInstanceId(),
+            "cftTaskState",
+            "unassigned"
+        );
+
+        common.cleanUpTask(taskId);
+    }
+
+
+    @Test
+    public void should_return_a_201_when_initiating_a_task_by_id_with_no_created_date() {
+        TestVariables taskVariables = common.setupTaskAndRetrieveIds();
+        String taskId = taskVariables.getTaskId();
+        common.setupOrganisationalRoleAssignment(authenticationHeaders);
+
+        ZonedDateTime createdDate = ZonedDateTime.now();
+        ZonedDateTime dueDate = createdDate.plusDays(1);
+        String formattedDueDate = CAMUNDA_DATA_TIME_FORMATTER.format(dueDate);
+
+        InitiateTaskRequest req = new InitiateTaskRequest(INITIATION, asList(
+            new TaskAttribute(TASK_TYPE, "followUpOverdueReasonsForAppeal"),
+            new TaskAttribute(TASK_NAME, "follow Up Overdue Reasons For Appeal"),
+            new TaskAttribute(TASK_CASE_ID, taskVariables.getCaseId()),
+            new TaskAttribute(TASK_TITLE, "A test task"),
+            new TaskAttribute(TASK_DUE_DATE, formattedDueDate)
+        ));
+
+        Response result = restApiActions.post(
+            ENDPOINT_BEING_TESTED,
+            taskId,
+            req,
+            authenticationHeaders
+        );
+
         //Note: this is the TaskResource.class
         result.then().assertThat()
             .statusCode(HttpStatus.CREATED.value())
@@ -196,6 +295,43 @@ public class PostTaskInitiateByIdControllerTest extends SpringBootFunctionalBase
                 + "there was a conflict in the database."));
 
         common.cleanUpTask(taskId);
+    }
+
+    @Test
+    public void should_return_a_400_if_no_due_date() {
+        TestVariables taskVariables = common.setupTaskAndRetrieveIds();
+        String taskId = taskVariables.getTaskId();
+        common.setupOrganisationalRoleAssignment(authenticationHeaders);
+
+        ZonedDateTime createdDate = ZonedDateTime.now();
+        String formattedCreatedDate = CAMUNDA_DATA_TIME_FORMATTER.format(createdDate);
+
+        InitiateTaskRequest req = new InitiateTaskRequest(INITIATION, asList(
+            new TaskAttribute(TASK_TYPE, "followUpOverdueReasonsForAppeal"),
+            new TaskAttribute(TASK_NAME, "follow Up Overdue Reasons For Appeal"),
+            new TaskAttribute(TASK_TITLE, "A test task"),
+            new TaskAttribute(TASK_CREATED, formattedCreatedDate),
+            new TaskAttribute(TASK_CASE_ID, taskId)
+        ));
+        //First call
+        Response result = restApiActions.post(
+            ENDPOINT_BEING_TESTED,
+            taskId,
+            req,
+            authenticationHeaders
+        );
+
+        result.then().assertThat()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .contentType(APPLICATION_PROBLEM_JSON_VALUE)
+            .body("type", equalTo(
+                "https://github.com/hmcts/wa-task-management-api/problem/constraint-validation"))
+            .body("title", equalTo("Constraint Violation"))
+            .body("status", equalTo(400))
+            .body("violations.[0].field", equalTo("task_due_date"))
+            .body("violations.[0].message",
+                equalTo("Each task to initiate must contain task_due_date field present and populated.")
+            );
     }
 
     @Test
