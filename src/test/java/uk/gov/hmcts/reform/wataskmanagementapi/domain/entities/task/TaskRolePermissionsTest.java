050d37eef53c2204108187601be3ace7dec2fca5
Vamshidhar Chitti <indvamshi@gmail.com>
2021-11-23 19:43:23 +0000

    Fixed sonar issues

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/task/TaskRolePermissionsTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/task/TaskRolePermissionsTest.java
@@ -5,7 +5,7 @@ import pl.pojo.tester.api.assertion.Method;
 
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
 
-public class TaskRolePermissionsTest {
+class TaskRolePermissionsTest {
 
     @Test
     void isWellImplemented() {
