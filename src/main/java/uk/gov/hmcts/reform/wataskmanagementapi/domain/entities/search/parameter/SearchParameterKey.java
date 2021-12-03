62e723888e0015dd578604ef65f48b722b55e740
Satvinder Hullait <1583258+satvinder-hullait@users.noreply.github.com>
2021-12-03 17:31:05 +0000

    WIP

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/search/parameter/SearchParameterKey.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/search/parameter/SearchParameterKey.java
@@ -4,24 +4,21 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum SearchParameterKey {
 
-    AVAILABLE_TASKS_ONLY("available_tasks_only", SearchParameterBoolean.class),
-    LOCATION("location", SearchParameterList.class),
-    USER("user", SearchParameterList.class),
-    JURISDICTION("jurisdiction", SearchParameterList.class),
-    STATE("state", SearchParameterList.class),
-    TASK_ID("taskId", SearchParameterList.class),
-    TASK_TYPE("taskType", SearchParameterList.class),
-    CASE_ID("caseId", SearchParameterList.class),
-    WORK_TYPE("work_type", SearchParameterList.class); //R2 should be snake_case only
+    AVAILABLE_TASKS_ONLY("available_tasks_only"),
+    LOCATION("location"),
+    USER("user"),
+    JURISDICTION("jurisdiction"),
+    STATE("state"),
+    TASK_ID("taskId"),
+    TASK_TYPE("taskType"),
+    CASE_ID("caseId"),
+    WORK_TYPE("work_type"); //R2 should be snake_case only
 
     @JsonValue
     private final String id;
 
-    private final Class<? extends SearchParameter<?>> searchParameterImpl;
-
-    SearchParameterKey(String id, Class<? extends SearchParameter<?>> searchParameterImpl) {
+    SearchParameterKey(String id) {
         this.id = id;
-        this.searchParameterImpl = searchParameterImpl;
     }
 
     @Override
@@ -33,7 +30,4 @@ public enum SearchParameterKey {
         return id;
     }
 
-    public Class<? extends SearchParameter<?>> getSearchParameterImpl() {
-        return searchParameterImpl;
-    }
 }
