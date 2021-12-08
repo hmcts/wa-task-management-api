35a1e5b51b99753486460481fba4e6d82a2a065c
Vamshidhar Chitti <indvamshi@gmail.com>
2021-12-08 13:03:40 +0000

    Added custom deserializer

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/search/parameter/SearchParameter.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/search/parameter/SearchParameter.java
@@ -1,18 +1,7 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.parameter;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.SearchOperator;
 
-@JsonTypeInfo(
-    use = JsonTypeInfo.Id.NAME,
-    property = "type"
-)
-@JsonSubTypes(
-    {
-        @JsonSubTypes.Type(value = SearchParameterList.class, name = "search_parameter_list"),
-        @JsonSubTypes.Type(value = SearchParameterBoolean.class, name = "search_parameter_boolean")
-    })
 public interface SearchParameter<T> {
 
     SearchParameterKey getKey();
