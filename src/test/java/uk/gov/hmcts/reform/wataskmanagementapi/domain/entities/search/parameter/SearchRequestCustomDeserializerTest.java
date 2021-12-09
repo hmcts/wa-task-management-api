d41f5abd82dca2fa12fd53642bbbfbeb4e3277f4
Satvinder Hullait <1583258+satvinder-hullait@users.noreply.github.com>
2021-12-09 12:14:57 +0000

    RWA-811 [SH] Adding mutation tests for SearchRequestCustomDeserializer

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/search/parameter/SearchRequestCustomDeserializerTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/search/parameter/SearchRequestCustomDeserializerTest.java
@@ -13,12 +13,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.BadRequestException;
 
 import java.io.IOException;
 
 import static java.util.List.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.SearchOperator.BOOLEAN;
 import static uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.SearchOperator.IN;
 import static uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.parameter.SearchParameterKey.AVAILABLE_TASKS_ONLY;
@@ -30,6 +37,75 @@ class SearchRequestCustomDeserializerTest {
 
     @Autowired
     private JacksonTester<SearchParameter<?>> json;
+
+
+    @Mock
+    private JsonParser jsonParser;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private JsonNode searchNode;
+    @Mock
+    private JsonNode operatorNode;
+
+    @Mock
+    private DeserializationContext deserializationContext;
+
+    @Test
+    void testSearchParameterBooleanOperatorIsDeserialized() throws IOException {
+        when(operatorNode.asText()).thenReturn("BOOLEAN");
+        when(searchNode.get("operator")).thenReturn(operatorNode);
+        when(jsonParser.getCodec()).thenReturn(objectMapper);
+        when(objectMapper.readTree(jsonParser)).thenReturn(searchNode);
+        SearchRequestCustomDeserializer deserializer = new SearchRequestCustomDeserializer();
+
+        deserializer.deserialize(jsonParser, deserializationContext);
+
+        verify(objectMapper, times(1)).treeToValue(searchNode, SearchParameterBoolean.class);
+        verify(objectMapper, never()).treeToValue(searchNode, SearchParameterList.class);
+    }
+
+    @Test
+    void testSearchParameterListOperatorIsDeserialized() throws IOException {
+        when(operatorNode.asText()).thenReturn("IN");
+        when(searchNode.get("operator")).thenReturn(operatorNode);
+        when(jsonParser.getCodec()).thenReturn(objectMapper);
+        when(objectMapper.readTree(jsonParser)).thenReturn(searchNode);
+        SearchRequestCustomDeserializer deserializer = new SearchRequestCustomDeserializer();
+
+        deserializer.deserialize(jsonParser, deserializationContext);
+
+        verify(objectMapper, times(1)).treeToValue(searchNode, SearchParameterList.class);
+        verify(objectMapper, never()).treeToValue(searchNode, SearchParameterBoolean.class);
+    }
+
+    @Test
+    void testSearchParameterOperatorIsNull() throws IOException {
+        when(searchNode.get("operator")).thenReturn(null);
+        when(jsonParser.getCodec()).thenReturn(objectMapper);
+        when(objectMapper.readTree(jsonParser)).thenReturn(searchNode);
+
+        SearchRequestCustomDeserializer deserializer = new SearchRequestCustomDeserializer();
+        assertThrows(BadRequestException.class, () -> {
+            deserializer.deserialize(jsonParser, deserializationContext);
+        });
+    }
+
+    @Test
+    void testSearchParameterOperatorIsNotValid() throws IOException {
+        lenient().when(operatorNode.asText()).thenReturn("NotValid");
+        when(searchNode.get("operator")).thenReturn(operatorNode);
+        when(jsonParser.getCodec()).thenReturn(objectMapper);
+        when(objectMapper.readTree(jsonParser)).thenReturn(searchNode);
+
+        SearchRequestCustomDeserializer deserializer = new SearchRequestCustomDeserializer();
+        assertThrows(BadRequestException.class, () -> {
+            deserializer.deserialize(jsonParser, deserializationContext);
+        });
+    }
+
 
     @Test
     void testTheSearchParameterBooleanCanBeDeserialized() throws IOException {
