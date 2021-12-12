6013ef36731e47cddcf801ddcd92a273a072c9d0
Vamshidhar Chitti <indvamshi@gmail.com>
2021-12-12 21:15:53 +0000

    Updated object mapper in pact tests

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/service/TaskManagementProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/service/TaskManagementProviderTestConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.provider.service;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -13,6 +12,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.zalando.problem.ProblemModule;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.AccessControlService;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.IdamService;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.permission.PermissionEvaluatorService;
@@ -33,6 +34,9 @@ import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.services.TaskAu
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING;
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING;
 import static uk.gov.hmcts.reform.wataskmanagementapi.services.SystemDateProvider.DATE_TIME_FORMAT;
 
 public class TaskManagementProviderTestConfiguration {
@@ -99,32 +103,40 @@ public class TaskManagementProviderTestConfiguration {
 
     @Bean
     @Primary
-    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder(
-        SearchRequestCustomDeserializer searchRequestCustomDeserializer) {
-        SimpleModule customDeserializerModule = new JsonComponentModule();
-        customDeserializerModule.addDeserializer(SearchParameter.class, searchRequestCustomDeserializer);
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        SearchRequestCustomDeserializer searchRequestCustomDeserializer = new SearchRequestCustomDeserializer();
+        // Set default date to RFC3339 standards
+        SimpleDateFormat df = new SimpleDateFormat(DATE_TIME_FORMAT, Locale.ENGLISH);
+        SimpleModule module = new JsonComponentModule();
+        module.addDeserializer(SearchParameter.class, searchRequestCustomDeserializer);
 
         return new Jackson2ObjectMapperBuilder()
-            .serializationInclusion(JsonInclude.Include.NON_ABSENT)
             .propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+            .serializationInclusion(JsonInclude.Include.NON_ABSENT)
+            .featuresToEnable(READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+            .featuresToEnable(READ_ENUMS_USING_TO_STRING)
+            .featuresToEnable(WRITE_ENUMS_USING_TO_STRING)
+            .dateFormat(df)
             .modules(
                 new ParameterNamesModule(),
                 new JavaTimeModule(),
                 new Jdk8Module(),
-                customDeserializerModule
+                new ProblemModule(),
+                module
             );
     }
 
     @Bean
     @Primary
-    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
-        // Set default date to RFC3339 standards
-        SimpleDateFormat df = new SimpleDateFormat(DATE_TIME_FORMAT, Locale.ENGLISH);
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-        objectMapper.setDateFormat(df);
-        objectMapper.registerModule(new Jdk8Module());
-        return objectMapper;
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        Jackson2ObjectMapperBuilder builder = jackson2ObjectMapperBuilder();
+        return new MappingJackson2HttpMessageConverter(builder.build());
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        Jackson2ObjectMapperBuilder builder = jackson2ObjectMapperBuilder();
+        return builder.createXmlMapper(false).build();
     }
 }
