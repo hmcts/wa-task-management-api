f293d5baf1acfd1de32e8a164e6ed1e3a5448430
Satvinder Hullait <1583258+satvinder-hullait@users.noreply.github.com>
2021-12-13 15:32:09 +0000

    sonar fixes

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/query/TaskResourceSpecification.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/query/TaskResourceSpecification.java
@@ -41,11 +41,6 @@ import static uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.par
 @SuppressWarnings({"PMD.DataflowAnomalyAnalysis", "PMD.TooManyMethods", "PMD.LawOfDemeter", "PMD.ExcessiveImports"})
 public final class TaskResourceSpecification {
 
-    public static final String LOCATION = "location";
-    public static final String TASK_ID = "taskId";
-    public static final String TASK_TYPE = "taskType";
-    public static final String CASE_ID = "caseId";
-    public static final String WORK_TYPE = "workTypeResource";
 
     private TaskResourceSpecification() {
         // avoid creating object
@@ -107,10 +102,10 @@ public final class TaskResourceSpecification {
         }
         return searchByJurisdiction(jurisdictionParam == null ? Collections.emptyList() : jurisdictionParam.getValues())
             .and(searchByState(getCftTaskStates(stateParam))
-            .and(searchByLocation(locationParam == null ? Collections.emptyList() : locationParam.getValues())
-            .and(searchByCaseIds(caseIdParam == null ? Collections.emptyList() : caseIdParam.getValues())
-            .and(searchByUser(userParam == null ? Collections.emptyList() : userParam.getValues())
-            .and(searchByWorkType(workTypeParam == null ? Collections.emptyList() : workTypeParam.getValues()))))));
+                .and(searchByLocation(locationParam == null ? Collections.emptyList() : locationParam.getValues())
+                    .and(searchByCaseIds(caseIdParam == null ? Collections.emptyList() : caseIdParam.getValues())
+                        .and(searchByUser(userParam == null ? Collections.emptyList() : userParam.getValues())
+                            .and(searchByWorkType(workTypeParam == null ? Collections.emptyList() : workTypeParam.getValues()))))));
     }
 
     private static List<CFTTaskState> getCftTaskStates(SearchParameterList stateParam) {
