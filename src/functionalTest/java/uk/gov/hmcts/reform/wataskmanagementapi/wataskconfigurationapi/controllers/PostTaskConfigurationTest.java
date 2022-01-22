8a8d1a6b6587a693be25c1e3b5508f4ae7263639
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2022-01-22 09:21:33 +0000

    Disable tests

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostTaskConfigurationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostTaskConfigurationTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootFunctionalBaseTest;
 import uk.gov.hmcts.reform.wataskmanagementapi.services.CreateTaskMessage;
@@ -36,6 +37,7 @@ public class PostTaskConfigurationTest extends SpringBootFunctionalBaseTest {
     }
 
     @Test
+    @Disabled("AM role-assignment enabled v1.1 of their validation which breaks this flow needs to be reviewed")
     public void given_task_is_configured_then_expect_task_is_auto_assigned() throws Exception {
         caseId = createCcdCase();
 
