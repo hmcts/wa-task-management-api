52f5167b776df841773cb6a9f5fa1ee9ac1ddd04
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2022-01-24 14:25:00 +0000

    RWA-1031: Improve error messages when verifying role-assignments for assignee and assigner (#490)

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskAssignByIdControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskAssignByIdControllerTest.java
@@ -148,7 +148,8 @@ public class PostTaskAssignByIdControllerTest extends SpringBootFunctionalBaseTe
             .body("title", equalTo("Role Assignment Verification"))
             .body("status", equalTo(403))
             .body("detail", equalTo(
-                "Role Assignment Verification: The request failed the Role Assignment checks performed."));
+                "Role Assignment Verification: "
+                + "The user assigning the Task has failed the Role Assignment checks performed."));
 
         common.cleanUpTask(taskId);
     }
@@ -178,7 +179,8 @@ public class PostTaskAssignByIdControllerTest extends SpringBootFunctionalBaseTe
             .body("title", equalTo("Role Assignment Verification"))
             .body("status", equalTo(403))
             .body("detail", equalTo(
-                "Role Assignment Verification: The request failed the Role Assignment checks performed."));
+                "Role Assignment Verification: "
+                + "The user being assigned the Task has failed the Role Assignment checks performed."));
 
 
         common.cleanUpTask(taskId);
