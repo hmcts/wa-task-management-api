77ff56dfe891ee5b0864e2cacda77896c9984d6c
dharmendra kumar <dharmendra.kumar@hmcts.net>
2022-03-22 20:17:43 +0000

    Ignore RELEASE 1 test classes (#555)

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskCompleteByIdControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskCompleteByIdControllerTest.java
@@ -24,6 +24,7 @@ import static uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda.Ca
 import static uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda.CamundaVariableDefinition.REGION;
 import static uk.gov.hmcts.reform.wataskmanagementapi.services.SystemDateProvider.DATE_TIME_FORMAT;
 
+@Ignore("Release 1 test class")
 public class PostTaskCompleteByIdControllerTest extends SpringBootFunctionalBaseTest {
 
     private static final String ENDPOINT_BEING_TESTED = "task/{task-id}/complete";
