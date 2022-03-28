0cae8731f8c495a2127b8bd21431d9f76ffdc5da
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-03-28 19:18:00 +0100

    RWA-1143 fixed get task by id controller CFT test

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/GetTaskByIdControllerCFTTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/GetTaskByIdControllerCFTTest.java
@@ -151,8 +151,8 @@ public class GetTaskByIdControllerCFTTest extends SpringBootFunctionalBaseTest {
             .body("task.warnings", is(false))
             .body("task.case_management_category", equalTo("Protection"))
             .body("task.work_type_id", equalTo("decision_making_work"))
-            .body("task.permissions.values.size()", equalTo(3))
-            .body("task.permissions.values", hasItems("Read", "Refer", "Own"))
+            .body("task.permissions.values.size()", equalTo(5))
+            .body("task.permissions.values", hasItems("Read", "Refer", "Own", "Manage", "Cancel"))
             .body("task.role_category", equalTo("LEGAL_OPERATIONS"));
 
         common.cleanUpTask(taskId);
@@ -250,8 +250,8 @@ public class GetTaskByIdControllerCFTTest extends SpringBootFunctionalBaseTest {
             .and().contentType(MediaType.APPLICATION_JSON_VALUE)
             .and().body("task.id", equalTo(taskId))
             .body("task.warnings", is(true))
-            .body("task.permissions.values.size()", equalTo(3))
-            .body("task.permissions.values", hasItems("Read", "Refer", "Own"));
+            .body("task.permissions.values.size()", equalTo(5))
+            .body("task.permissions.values", hasItems("Read", "Refer", "Own", "Manage", "Cancel"));
 
         final List<Map<String, String>> actualWarnings = result.jsonPath().getList(
             "task.warning_list.values");
@@ -312,8 +312,8 @@ public class GetTaskByIdControllerCFTTest extends SpringBootFunctionalBaseTest {
             .body("task.case_name", notNullValue())
             .body("task.auto_assigned", notNullValue())
             .body("task.warnings", notNullValue())
-            .body("task.permissions.values.size()", equalTo(3))
-            .body("task.permissions.values", hasItems("Read", "Refer", "Own"))
+            .body("task.permissions.values.size()", equalTo(5))
+            .body("task.permissions.values", hasItems("Read", "Refer", "Own", "Manage", "Cancel"))
             .body("task.description", notNullValue())
             .body("task.role_category", equalTo("LEGAL_OPERATIONS"));
 
