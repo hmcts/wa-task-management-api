1536c6d4885606519602e770e97bd310c258d083
burale <burale2000@hotmail.com>
2022-05-04 18:07:28 +0100

    RWA-1259 Add tests for multiple authorisation types

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/CustomPostgreSQL94Dialect.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/CustomPostgreSQL94Dialect.java
@@ -1,15 +1,20 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.config;
 
 import org.hibernate.dialect.PostgreSQL10Dialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.StandardBasicTypes;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.NoteResource;
 
 import java.sql.Types;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
+@Component
 public class CustomPostgreSQL94Dialect extends PostgreSQL10Dialect {
 
     public CustomPostgreSQL94Dialect() {
         super();
         this.registerHibernateType(Types.JAVA_OBJECT, NoteResource.class.getName());
+        registerFunction("contains_text", new SQLFunctionTemplate(StandardBasicTypes.BOOLEAN, "?1 @> ?2::text[]"));
     }
 }
