6a431c514fe850cfe3b9722985149f060c9537f1
Serdar Oğuzhan AKIN <42956817+serdaroguzhanakin@users.noreply.github.com>
2022-05-05 14:41:01 +0100

    RWA-1353-pact-broker-fix (#589)
    * pact broker fix
    
    * revert pact folder
    
    * revert CcdConsumerTestBase
    
    * add logs for pact verification
    
    * add logs for pact verification
    
    * set true pact.verifier.publishResults
    
    * change pact folder path
    
    * try old version of pactBroler definition
    
    * use full url for pact broker
    
    * provider base class was added into contract test

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerCompleteTaskProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerCompleteTaskProviderTest.java
@@ -2,28 +2,16 @@ package uk.gov.hmcts.reform.wataskmanagementapi.provider;
 
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
-import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.AccessControlService;
+import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractProviderBaseTest;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
-import uk.gov.hmcts.reform.wataskmanagementapi.auth.restrict.ClientAccessControlService;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.TaskActionsController;
-import uk.gov.hmcts.reform.wataskmanagementapi.provider.service.TaskManagementProviderTestConfiguration;
-import uk.gov.hmcts.reform.wataskmanagementapi.services.SystemDateProvider;
-import uk.gov.hmcts.reform.wataskmanagementapi.services.TaskManagementService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -31,34 +19,8 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 @Provider("wa_task_management_api_complete_task_by_id")
-//Uncomment this and comment the @PacBroker line to test TaskManagerClaimTaskConsumerTest local consumer.
-//using this, import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
-//@PactFolder("target/pacts")
-@PactBroker(
-    url = "${PACT_BROKER_SCHEME:http}" + "://" + "${PACT_BROKER_URL:localhost}" + ":" + "${PACT_BROKER_PORT:9292}",
-    consumerVersionSelectors = {
-        @VersionSelector(tag = "master")}
-)
-@Import(TaskManagementProviderTestConfiguration.class)
-@IgnoreNoPactsToVerify
-public class TaskManagerCompleteTaskProviderTest {
-
-    @Mock
-    private AccessControlService accessControlService;
-
-    @Mock
-    private TaskManagementService taskManagementService;
-
-    @Autowired
-    private SystemDateProvider systemDateProvider;
-
-    @Mock
-    private ClientAccessControlService clientAccessControlService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+public class TaskManagerCompleteTaskProviderTest extends SpringBootContractProviderBaseTest {
 
     @State({"complete a task using taskId"})
     public void completeTaskById() {
