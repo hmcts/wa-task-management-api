8f8b29c610117cb9698b98ae39457975a692f0a0
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2022-05-16 14:50:10 +0100

    resolve import

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/service/TaskManagementProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/service/TaskManagementProviderTestConfiguration.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.boot.test.mock.mockito.MockBean;
