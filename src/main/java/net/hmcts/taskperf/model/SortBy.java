2131c7632e963ee9b62ce08f11922cc9d4a6d74c
Jon Moore <jonathan.moore@hmcts.net>
2022-05-20 16:19:17 +0100

    Change to use the task management api SortField class.

--- src/main/java/net/hmcts/taskperf/model/SortBy.java
+++ src/main/java/net/hmcts/taskperf/model/SortBy.java
@@ -1,49 +1,12 @@
 package net.hmcts.taskperf.model;
 
 import lombok.Value;
+import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.SortField;
 import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.search.SortOrder;
 
 @Value
 public class SortBy
 {
-	private Column column;
+	private SortField column;
 	private SortOrder direction;
-
-	public static enum Column
-	{
-	    DUE_DATE_TIME("due_date_time"),
-	    CASE_NAME("case_name"),
-	    CASE_CATEGORY("case_category"),
-	    LOCATION("location"),
-	    TASK_NAME("task_name"),
-	    CREATED("created"),
-		MAJOR_PRIORITY("major_priority"),
-		PRIORITY_DATE_TIME("priority_date_time"),
-		MINOR_PRIORITY("minor_priority");
-	
-	    private final String value;
-	
-		Column(String value) {
-	        this.value = value;
-	    }
-	
-	    public String value() {
-	        return value;
-	    }
-	}
-
-	public static enum Direction
-	{
-	    ASC("asc"),
-	    DESC("desc");
-	    private final String value;
-
-	    Direction(String value) {
-	        this.value = value;
-	    }
-
-	    public String value() {
-	        return value;
-	    }
-	}
 }
