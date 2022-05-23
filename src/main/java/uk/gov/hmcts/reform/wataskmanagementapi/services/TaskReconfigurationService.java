6a67797a0795aee00b0cf3890be6aec05b6e53e7
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-05-23 11:58:21 +0100

    RWA-1086 reconfiguration service added to mark tasks reconfigurable

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskReconfigurationService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskReconfigurationService.java
@@ -2,10 +2,13 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskResource;
+import uk.gov.hmcts.reform.wataskmanagementapi.cft.enums.CFTTaskState;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.TaskFilter;
 
 import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -21,20 +24,26 @@ public class TaskReconfigurationService {
         this.cftTaskDatabaseService = cftTaskDatabaseService;
     }
 
+    @Transactional
     public void markTasksToReconfigure(List<TaskFilter> taskFilters) {
         List<Object> caseIds = taskFilters.stream().filter(filter -> filter.getKey().equalsIgnoreCase("case_id"))
-            .flatMap(filter ->filter.getValues().stream())
+            .flatMap(filter -> filter.getValues().stream())
             .collect(Collectors.toList());
 
         List<TaskResource> taskResources = caseIds.stream()
-            .map(caseId -> cftTaskDatabaseService.findByCaseIdOnly((String)caseId))
-            .flatMap(tasks -> tasks.stream())
+            .map(caseId -> cftTaskDatabaseService.findByCaseIdOnly((String) caseId))
+            .flatMap(Collection::stream)
+            .filter(task -> task.getState().equals(CFTTaskState.ASSIGNED) || task.getState().equals(CFTTaskState.UNASSIGNED))
             .filter(task -> Objects.isNull(task.getReconfigureRequestTime()))
             .collect(Collectors.toList());
 
-        taskResources.stream().forEach(taskResource -> {
-            taskResource.setReconfigureRequestTime(OffsetDateTime.now());
-            cftTaskDatabaseService.saveTask(taskResource);
-        });
+        taskResources.stream()
+            .map(task -> cftTaskDatabaseService.findByIdAndObtainPessimisticWriteLock(task.getTaskId())
+                .orElseGet(() -> null))
+            .filter(Objects::nonNull)
+            .forEach(taskResource -> {
+                taskResource.setReconfigureRequestTime(OffsetDateTime.now());
+                cftTaskDatabaseService.saveTask(taskResource);
+            });
     }
 }
