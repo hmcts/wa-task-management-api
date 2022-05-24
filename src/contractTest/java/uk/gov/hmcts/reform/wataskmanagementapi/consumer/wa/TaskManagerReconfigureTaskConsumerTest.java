bb05fc3650aeefbc0de58f38c3f6d56d69bf69b9
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-05-24 22:59:55 +0100

    RWA-1086 added reconfiguration service pact tests

---
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/consumer/wa/TaskManagerReconfigureTaskConsumerTest.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.consumer.wa;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractBaseTest;
+import uk.gov.hmcts.reform.wataskmanagementapi.provider.service.CamundaConsumerApplication;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@PactTestFor(providerName = "wa_task_management_api_reconfigure_task_by_id", port = "8991")
+@ContextConfiguration(classes = {CamundaConsumerApplication.class})
+public class TaskManagerReconfigureTaskConsumerTest extends SpringBootContractBaseTest {
+
+    private static final String TASK_ID = "704c8b1c-e89b-436a-90f6-953b1dc40157";
+    private static final String WA_URL = "/task";
+    private static final String WA_RECONFIGURE_TASK_BY_ID = WA_URL + "/operation";
+
+    @Pact(provider = "wa_task_management_api_reconfigure_task_by_id", consumer = "wa_task_management_api")
+    public RequestResponsePact executeReconfigureTaskById204(PactDslWithProvider builder) {
+
+        return builder
+            .given("reconfigure a task using caseId")
+            .uponReceiving("caseId to reconfigure tasks")
+            .path(WA_RECONFIGURE_TASK_BY_ID)
+            .method(HttpMethod.POST.toString())
+            .body("", String.valueOf(ContentType.JSON))
+            .matchHeader(SERVICE_AUTHORIZATION, SERVICE_AUTH_TOKEN)
+            .willRespondWith()
+            .status(HttpStatus.NO_CONTENT.value())
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "executeReconfigureTaskById204", pactVersion = PactSpecVersion.V3)
+    void testReconfigureTaskByTaskId204Test(MockServer mockServer) {
+
+        SerenityRest
+            .given()
+            .headers(getHttpHeaders())
+            .contentType(ContentType.JSON)
+            .body("")
+            .post(mockServer.getUrl() + WA_RECONFIGURE_TASK_BY_ID)
+            .then()
+            .statusCode(204);
+        
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerReconfigureTaskProviderTest.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.provider;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractProviderBaseTest;
+import uk.gov.hmcts.reform.wataskmanagementapi.controllers.TaskActionsController;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@Provider("wa_task_management_api_reconfigure_task_by_case_id")
+public class TaskManagerReconfigureTaskProviderTest extends SpringBootContractProviderBaseTest {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        if (context != null) {
+            context.verifyInteraction();
+        }
+    }
+
+    @BeforeEach
+    void beforeCreate(PactVerificationContext context) {
+        MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        testTarget.setControllers(new TaskActionsController(
+            taskManagementService,
+            accessControlService,
+            systemDateProvider,
+            clientAccessControlService
+        ));
+        if (context != null) {
+            context.setTarget(testTarget);
+        }
+
+    }
+
+    @State({"reconfigure tasks using caseId"})
+    public void reconfigureTasksByCaseId() {
+        setInitMock();
+    }
+
+    private void setInitMock() {
+        doNothing().when(taskManagementService).performOperation(any());
+        when(clientAccessControlService.hasExclusiveAccess(anyString())).thenReturn(true);
+    }
+}
