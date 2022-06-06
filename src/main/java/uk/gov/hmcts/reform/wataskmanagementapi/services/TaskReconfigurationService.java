61d0e235c7978de36529932a66ee94541ff8d7a9
Lee Philip <leephilip365@yahoo.co.uk>
2022-06-06 18:14:11 +0100

    RWA-1095 execute reconfiguration

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskReconfigurationService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskReconfigurationService.java
@@ -7,6 +7,8 @@ import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.enums.CFTTaskState;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.MarkTaskToReconfigureTaskFilter;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.TaskFilter;
+import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.services.ConfigureTaskService;
+import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.services.TaskAutoAssignmentService;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -19,9 +21,15 @@ import java.util.stream.Collectors;
 public class TaskReconfigurationService {
 
     private final CFTTaskDatabaseService cftTaskDatabaseService;
+    private final ConfigureTaskService configureTaskService;
+    private final TaskAutoAssignmentService taskAutoAssignmentService;
 
-    public TaskReconfigurationService(CFTTaskDatabaseService cftTaskDatabaseService) {
+    public TaskReconfigurationService(CFTTaskDatabaseService cftTaskDatabaseService,
+                                      ConfigureTaskService configureTaskService,
+                                      TaskAutoAssignmentService taskAutoAssignmentService) {
         this.cftTaskDatabaseService = cftTaskDatabaseService;
+        this.configureTaskService = configureTaskService;
+        this.taskAutoAssignmentService = taskAutoAssignmentService;
     }
 
     @Transactional
@@ -46,5 +54,38 @@ public class TaskReconfigurationService {
             });
 
         return taskResources;
+    }
+
+    @Transactional
+    public List<TaskResource> executeReconfigure(List<TaskFilter<?>> taskFilters) {
+
+        OffsetDateTime reconfigureDate = OffsetDateTime.parse(getReconfigureRequestTime(taskFilters));
+
+        List<TaskResource> taskResources = cftTaskDatabaseService
+            .getActiveTasksAndReconfigureRequestTimeIsNotNull(
+                List.of(CFTTaskState.ASSIGNED, CFTTaskState.UNASSIGNED));
+
+        taskResources.stream()
+            .map(task -> cftTaskDatabaseService.findByIdAndObtainPessimisticWriteLock(task.getTaskId())
+                .orElseGet(() -> null))
+            .filter(Objects::nonNull)
+            .filter(task -> task.getReconfigureRequestTime().isAfter(reconfigureDate))
+            .forEach(taskResource -> {
+                configureTaskService.configureTask(taskResource.getTaskId());
+                taskAutoAssignmentService.reAutoAssignCFTTask(taskResource);
+                taskResource.setReconfigureRequestTime(null);
+                taskResource.setLastReconfigurationTime(OffsetDateTime.now());
+                cftTaskDatabaseService.saveTask(taskResource);
+            });
+
+        return taskResources;
+    }
+
+    @Transactional
+    public String getReconfigureRequestTime(List<TaskFilter<?>> taskFilters) {
+
+        return taskFilters.stream()
+            .filter(filter -> filter.getKey().equalsIgnoreCase("reconfigure_request_time"))
+            .findFirst().get().getValues().toString();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskReconfigurationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskReconfigurationServiceTest.java
@@ -7,9 +7,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.enums.CFTTaskState;
+import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.ExecuteReconfigureTaskFilter;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.MarkTaskToReconfigureTaskFilter;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.TaskFilter;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.enums.TaskFilterOperator;
+import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.services.ConfigureTaskService;
+import uk.gov.hmcts.reform.wataskmanagementapi.taskconfiguration.services.TaskAutoAssignmentService;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -29,6 +32,10 @@ class TaskReconfigurationServiceTest {
 
     @Mock
     private CFTTaskDatabaseService cftTaskDatabaseService;
+    @Mock
+    ConfigureTaskService configureTaskService;
+    @Mock
+    TaskAutoAssignmentService taskAutoAssignmentService;
 
     @InjectMocks
     private TaskReconfigurationService taskReconfigurationService;
@@ -128,5 +135,64 @@ class TaskReconfigurationServiceTest {
             "someCaseId"
         );
         return List.of(taskResource1, taskResource2);
+    }
+
+    @Test
+    void should_find_reconfigure_request_time() {
+        List<TaskFilter<?>> taskFilters = createReconfigureTaskFilters();
+
+        String reconfigureDateString = taskReconfigurationService.getReconfigureRequestTime(taskFilters);
+
+        assertEquals("2022-06-06T16:00:00.00000+01:00", reconfigureDateString);
+    }
+
+    private List<TaskFilter<?>> createReconfigureTaskFilters() {
+        ExecuteReconfigureTaskFilter filter = new ExecuteReconfigureTaskFilter(
+            "reconfigure_request_time", "2022-06-06T16:00:00.00000+01:00", TaskFilterOperator.AFTER);
+        return List.of(filter);
+    }
+
+    private List<TaskResource> taskResourcesToReconfigure(OffsetDateTime reconfigureTime) {
+        TaskResource taskResource1 = new TaskResource(
+            "1234",
+            "someTaskName",
+            "someTaskType",
+            CFTTaskState.UNASSIGNED,
+            "someCaseId"
+        );
+        TaskResource taskResource2 = new TaskResource(
+            "4567",
+            "someTaskName",
+            "someTaskType",
+            CFTTaskState.ASSIGNED,
+            "someCaseId"
+        );
+        if (Objects.nonNull(reconfigureTime)) {
+            taskResource1.setReconfigureRequestTime(reconfigureTime);
+            taskResource2.setReconfigureRequestTime(reconfigureTime);
+        }
+        return List.of(taskResource1, taskResource2);
+    }
+
+    @Test
+    void should_get_tasks_with_reconfigure_request_time_and_set_to_null() {
+
+        OffsetDateTime todayTestDatetime = OffsetDateTime.now();
+        List<TaskFilter<?>> taskFilters = createReconfigureTaskFilters();
+
+        List<TaskResource> taskResources = taskResourcesToReconfigure(OffsetDateTime.now());
+        when(cftTaskDatabaseService.getActiveTasksAndReconfigureRequestTimeIsNotNull(
+            anyList())).thenReturn(taskResources);
+        when(cftTaskDatabaseService.findByIdAndObtainPessimisticWriteLock(anyString()))
+            .thenReturn(Optional.of(taskResources.get(0)))
+            .thenReturn(Optional.of(taskResources.get(1)));
+
+        List<TaskResource> taskResourcesReconfigured = taskReconfigurationService.executeReconfigure(taskFilters);
+
+        taskResourcesReconfigured.forEach(taskResource -> {
+            assertNull(taskResource.getReconfigureRequestTime());
+            assertTrue(taskResource.getLastReconfigurationTime().isAfter(todayTestDatetime));
+        });
+
     }
 }
