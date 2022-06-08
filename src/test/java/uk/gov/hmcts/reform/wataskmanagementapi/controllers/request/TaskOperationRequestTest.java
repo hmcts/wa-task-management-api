80495e67ecf6a815d3992962f274104a9cce41cf
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-06-08 13:11:44 +0100

    RWA-1196 added addiitonal tests

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/request/TaskOperationRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/request/TaskOperationRequestTest.java
@@ -1,8 +1,7 @@
-package uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities;
+package uk.gov.hmcts.reform.wataskmanagementapi.controllers.request;
 
 import org.junit.jupiter.api.Test;
 import pl.pojo.tester.api.assertion.Method;
-import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.TaskOperationRequest;
 
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
 
