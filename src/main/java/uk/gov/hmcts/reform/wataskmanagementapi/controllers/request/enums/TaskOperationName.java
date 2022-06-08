af8e83f5c88c8dc5b0aab487f720c8c77983f6bb
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-06-08 13:08:50 +0100

    RWA-1086 added addiitonal tests

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/request/enums/TaskOperationName.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/request/enums/TaskOperationName.java
@@ -1,30 +1,16 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.enums;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-
 public enum TaskOperationName {
-    MARK_TO_RECONFIGURE(
-        "mark_to_reconfigure",
-        new TypeReference<String>() {
-        }),
-    EXECUTE_RECONFIGURE(
-        "execute_reconfigure",
-        new TypeReference<String>() {
-        });
+    MARK_TO_RECONFIGURE("mark_to_reconfigure"),
+    EXECUTE_RECONFIGURE("execute_reconfigure");
 
     private final String value;
-    private final TypeReference<String> typeReference;
 
-    TaskOperationName(String value, TypeReference<String> typeReference) {
+    TaskOperationName(String value) {
         this.value = value;
-        this.typeReference = typeReference;
     }
 
     public String value() {
         return value;
-    }
-
-    public TypeReference<String> getTypeReference() {
-        return typeReference;
     }
 }
