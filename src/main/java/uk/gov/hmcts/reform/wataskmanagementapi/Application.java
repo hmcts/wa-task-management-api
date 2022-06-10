8b904ca855c1da9fc587807c957d827f3a156fa4
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2022-06-10 10:55:58 +0100

    perftest run

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         "uk.gov.hmcts.reform.wataskmanagementapi"
     })
 @EnableCaching
+@ComponentScan({"uk.gov.hmcts.reform.wataskmanagementapi","net.hmcts.taskperf"})
 public class Application {
 
     public static void main(final String[] args) {
