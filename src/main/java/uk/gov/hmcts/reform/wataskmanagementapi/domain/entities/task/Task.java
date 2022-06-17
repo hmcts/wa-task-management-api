bb470e7e57c263650678cd1ded5232c690a8a9ae
Chris Davidson <christopher.davidson@btinternet.com>
2022-06-17 13:46:06 +0100

    RWA 1410 added fields to search results

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/task/Task.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/task/Task.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 
 import static uk.gov.hmcts.reform.wataskmanagementapi.services.SystemDateProvider.DATE_TIME_FORMAT;
 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @SuppressWarnings({"PMD.LawOfDemeter", "PMD.TooManyFields",
     "PMD.ExcessiveParameterList", "PMD.ShortClassName", "PMD.LinguisticNaming","PMD.ExcessiveImports"})
