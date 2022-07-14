6d9de0941ff0cddca4daf83b84b371e573fe1367
Chris Davidson <christopher.davidson@btinternet.com>
2022-07-14 16:36:13 +0100

    RWA 1410 Match base branch

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostConfigureTaskTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostConfigureTaskTest.java
@@ -132,4 +132,5 @@ public class PostConfigureTaskTest extends SpringBootFunctionalBaseTest {
             .body("title.value", is("task name"))
             .body("task-supervisor.value", is("Read,Refer,Execute,Manage,Cancel"));
     }
+    
 }
