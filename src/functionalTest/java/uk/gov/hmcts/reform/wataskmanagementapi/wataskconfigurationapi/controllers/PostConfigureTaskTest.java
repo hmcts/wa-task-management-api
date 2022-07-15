255eb5f225bc44a11369633d672116f03061d157
Chris Davidson <christopher.davidson@btinternet.com>
2022-07-15 10:11:42 +0100

    RWA 1369 import

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostConfigureTaskTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/wataskconfigurationapi/controllers/PostConfigureTaskTest.java
@@ -14,7 +14,6 @@ import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.TestAuthenticatio
 import uk.gov.hmcts.reform.wataskmanagementapi.services.CreateTaskMessage;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.is;
