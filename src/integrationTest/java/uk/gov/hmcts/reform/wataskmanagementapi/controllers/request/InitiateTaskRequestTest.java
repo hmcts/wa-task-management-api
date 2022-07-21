6b2840344a74f6a07c22cfe7c56ffea12b0336bd
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2022-07-21 15:42:12 +0100

    resolve failing test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/request/InitiateTaskRequestTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/request/InitiateTaskRequestTest.java
@@ -3,9 +3,8 @@ package uk.gov.hmcts.reform.wataskmanagementapi.controllers.request;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootIntegrationBaseTest;
-import uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.entities.TaskAttribute;
 
-import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.wataskmanagementapi.controllers.request.enums.InitiateTaskOperation.INITIATION;
@@ -19,37 +18,21 @@ class InitiateTaskRequestTest extends SpringBootIntegrationBaseTest {
     @Test
     void given_snake_case_initiate_body_request_when_deserializes_it_keeps_attribute_list_and_operation_fields()
         throws JsonProcessingException {
-        String expectedInitiateBodyRequest = "{\n"
-                                             + "  \"operation\": \"INITIATION\",\n"
-                                             + "  \"task_attributes\": [\n"
-                                             + "    {\n"
-                                             + "      \"name\": \"TASK_TYPE\",\n"
-                                             + "      \"value\": \"aTaskType\"\n"
-                                             + "    },\n"
-                                             + "    {\n"
-                                             + "      \"name\": \"TASK_NAME\",\n"
-                                             + "      \"value\": \"aTaskName\"\n"
-                                             + "    },\n"
-                                             + "    {\n"
-                                             + "      \"name\": \"TASK_CASE_ID\",\n"
-                                             + "      \"value\": \"1634748573864804\"\n"
-                                             + "    },\n"
-                                             + "    {\n"
-                                             + "      \"name\": \"TASK_TITLE\",\n"
-                                             + "      \"value\": \"A test task\"\n"
-                                             + "    }\n"
-                                             + "  ]\n"
-                                             + "}";
+        Map<String, Object> taskAttributes = Map.of(
+            TASK_TYPE.value(), "aTaskType",
+            TASK_NAME.value(), "aTaskName",
+            TASK_CASE_ID.value(), "1634748573864804",
+            TASK_TITLE.value(), "A test task"
+        );
+
+        InitiateTaskRequest initiateTaskRequest = new InitiateTaskRequest(INITIATION, taskAttributes);
+
+        String expectedInitiateBodyRequest = asJsonString(initiateTaskRequest);
 
         InitiateTaskRequest actual = objectMapper.readValue(expectedInitiateBodyRequest, InitiateTaskRequest.class);
 
         assertThat(actual.getTaskAttributes()).isNotNull();
-        assertThat(actual.getTaskAttributes()).isEqualTo(List.of(
-            new TaskAttribute(TASK_TYPE, "aTaskType"),
-            new TaskAttribute(TASK_NAME, "aTaskName"),
-            new TaskAttribute(TASK_CASE_ID, "1634748573864804"),
-            new TaskAttribute(TASK_TITLE, "A test task")
-        ));
+        assertThat(actual.getTaskAttributes()).isEqualTo(taskAttributes);
         assertThat(actual.getOperation()).isEqualTo(INITIATION);
     }
 
