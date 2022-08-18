cde699775bd73479bd330540d5b9b44c7470f9dc
Chris Davidson <christopher.davidson@btinternet.com>
2022-08-18 15:09:54 +0100

    RWA 1529 add update rather than default

--- src/main/resources/db/migration/V1.0.18__add_permissions_to_task_roles.sql
+++ src/main/resources/db/migration/V1.0.18__add_permissions_to_task_roles.sql
@@ -1,11 +1,24 @@
 ALTER TABLE task_roles
-    ADD COLUMN complete BOOLEAN   default false,
-    ADD COLUMN complete_own BOOLEAN   default false,
-    ADD COLUMN cancel_own BOOLEAN   default false,
-    ADD COLUMN claim BOOLEAN   default false,
-    ADD COLUMN unclaim BOOLEAN   default false,
-    ADD COLUMN assign BOOLEAN   default false,
-    ADD COLUMN unassign BOOLEAN   default false,
-    ADD COLUMN unclaim_assign BOOLEAN   default false,
-    ADD COLUMN unassign_claim BOOLEAN   default false,
-    ADD COLUMN unassign_assign BOOLEAN   default false;
+    ADD COLUMN complete BOOLEAN,
+    ADD COLUMN complete_own BOOLEAN,
+    ADD COLUMN cancel_own BOOLEAN,
+    ADD COLUMN claim BOOLEAN,
+    ADD COLUMN unclaim BOOLEAN,
+    ADD COLUMN assign BOOLEAN,
+    ADD COLUMN unassign BOOLEAN,
+    ADD COLUMN unclaim_assign BOOLEAN,
+    ADD COLUMN unassign_claim BOOLEAN,
+    ADD COLUMN unassign_assign BOOLEAN;
+UPDATE task_roles SET
+    complete = false,
+    complete_own = false,
+    cancel_own = false,
+    claim = false,
+    unclaim = false,
+    assign = false,
+    unassign = false,
+    unclaim_assign = false,
+    unassign_claim = false,
+    unassign_assign = false;
+
+
