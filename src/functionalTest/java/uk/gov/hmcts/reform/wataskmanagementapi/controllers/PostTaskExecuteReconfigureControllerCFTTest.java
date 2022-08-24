f2c2caccb5ee384fa1f07702ceb9f825dbbc8a7d
Chris Davidson <christopher.davidson@btinternet.com>
2022-08-24 15:30:27 +0100

    RWA 1724 fix change

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskExecuteReconfigureControllerCFTTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskExecuteReconfigureControllerCFTTest.java
@@ -52,6 +52,7 @@ public class PostTaskExecuteReconfigureControllerCFTTest extends SpringBootFunct
         authorizationProvider.deleteAccount(assigneeCredentials.getAccount().getUsername());
     }
 
+
     @Test
     public void should_return_a_204_after_tasks_are_marked_and_executed_for_reconfigure() {
         TestVariables taskVariables = common.setupWATaskAndRetrieveIds("requests/ccd/wa_case_data.json");
