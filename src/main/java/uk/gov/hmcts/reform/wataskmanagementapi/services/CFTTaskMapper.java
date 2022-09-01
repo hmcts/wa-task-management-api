35f9655ea7d19496ddd509b5c909893c7cd5a619
Lee Philip <leephilip365@yahoo.co.uk>
2022-09-01 19:29:54 +0100

    RWA-1528 updated DMN changes

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskMapper.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskMapper.java
@@ -323,6 +323,36 @@ public class CFTTaskMapper {
         if (taskRoleResource.getRefer()) {
             accumulator.add(PermissionTypes.REFER);
         }
+        if (taskRoleResource.getClaim()) {
+            accumulator.add(PermissionTypes.CLAIM);
+        }
+        if (taskRoleResource.getAssign()) {
+            accumulator.add(PermissionTypes.ASSIGN);
+        }
+        if (taskRoleResource.getUnassign()) {
+            accumulator.add(PermissionTypes.UNASSIGN);
+        }
+        if (taskRoleResource.getUnassignAssign()) {
+            accumulator.add(PermissionTypes.UNASSIGN_ASSIGN);
+        }
+        if (taskRoleResource.getComplete()) {
+            accumulator.add(PermissionTypes.COMPLETE);
+        }
+        if (taskRoleResource.getCompleteOwn()) {
+            accumulator.add(PermissionTypes.COMPLETE_OWN);
+        }
+        if (taskRoleResource.getCancelOwn()) {
+            accumulator.add(PermissionTypes.CANCEL_OWN);
+        }
+        if (taskRoleResource.getUnassignClaim()) {
+            accumulator.add(PermissionTypes.UNASSIGN_CLAIM);
+        }
+        if (taskRoleResource.getUnclaim()) {
+            accumulator.add(PermissionTypes.UNCLAIM);
+        }
+        if (taskRoleResource.getUnclaimAssign()) {
+            accumulator.add(PermissionTypes.UNCLAIM_ASSIGN);
+        }
         return accumulator;
     }
 
@@ -394,7 +424,17 @@ public class CFTTaskMapper {
                     autoAssignable,
                     roleCategory,
                     taskResource.getTaskId(),
-                    ZonedDateTime.now().toOffsetDateTime()
+                    ZonedDateTime.now().toOffsetDateTime(),
+                    permissionsFound.contains(PermissionTypes.CLAIM),
+                    permissionsFound.contains(PermissionTypes.ASSIGN),
+                    permissionsFound.contains(PermissionTypes.UNASSIGN),
+                    permissionsFound.contains(PermissionTypes.UNASSIGN_ASSIGN),
+                    permissionsFound.contains(PermissionTypes.COMPLETE),
+                    permissionsFound.contains(PermissionTypes.COMPLETE_OWN),
+                    permissionsFound.contains(PermissionTypes.CANCEL_OWN),
+                    permissionsFound.contains(PermissionTypes.UNASSIGN_CLAIM),
+                    permissionsFound.contains(PermissionTypes.UNCLAIM),
+                    permissionsFound.contains(PermissionTypes.UNCLAIM_ASSIGN)
                 );
             }).collect(Collectors.toSet());
     }
