2062e4f74129bf530a692b6ee1aaa7c8170b08a6
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2022-09-28 10:27:46 +0100

    Setting timeout to 100 seconds for initiation wait

--- src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/SpringBootFunctionalBaseTest.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/SpringBootFunctionalBaseTest.java
@@ -251,7 +251,7 @@ public abstract class SpringBootFunctionalBaseTest {
         if (initiationJobRunning) {
             await()
                 .pollInterval(10, SECONDS)
-                .atMost(120, SECONDS)
+                .atMost(100, SECONDS)
                 .until(
                     () -> {
                         Response response = restApiActions.get(
