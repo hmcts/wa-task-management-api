24c525bd275f52068f252d6281aad6ad269952aa
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-09-29 12:27:22 +0100

    RWA-1576 added new attributes for last updated actions

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskMapperTest.java
@@ -1328,7 +1328,7 @@ class CFTTaskMapperTest {
         TaskResource taskResource = cftTaskMapper.mapToTaskResource(taskId, attributes);
         Map<String, Object> taskAttributes = cftTaskMapper.getTaskAttributes(taskResource);
 
-        assertThat(taskAttributes).size().isEqualTo(38);
+        assertThat(taskAttributes).size().isEqualTo(41);
     }
 
     @Test
