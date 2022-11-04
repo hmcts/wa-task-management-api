0d771bcecb6185cf034ff5be68a48d56e76e3393
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-11-04 23:29:45 +0000

    RWA-1656 fixed code review comments

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CFTTaskMapperTest.java
@@ -1115,7 +1115,8 @@ class CFTTaskMapperTest {
         taskResource.setLastReconfigurationTime(OffsetDateTime.now());
         taskResource.setLastUpdatedAction("someAction");
         taskResource.setLastUpdatedUser("someUser");
-        taskResource.setLastUpdatedTimestamp(OffsetDateTime.now());
+        OffsetDateTime lastUpdatedTimeStamp = OffsetDateTime.now();
+        taskResource.setLastUpdatedTimestamp(lastUpdatedTimeStamp);
 
         Task task = cftTaskMapper.mapToTaskWithPermissions(taskResource, new HashSet<>());
 
@@ -1148,9 +1149,9 @@ class CFTTaskMapperTest {
         assertTrue(task.getPermissions().getValues().isEmpty());
         assertNotNull(task.getReconfigureRequestTime());
         assertNotNull(task.getLastReconfigurationTime());
-        assertNotNull(task.getLastUpdatedTimestamp());
-        assertNotNull(task.getLastUpdatedAction());
-        assertNotNull(task.getLastUpdatedUser());
+        assertEquals(lastUpdatedTimeStamp, task.getLastUpdatedTimestamp().toOffsetDateTime());
+        assertEquals("someAction", task.getLastUpdatedAction());
+        assertEquals("someUser", task.getLastUpdatedUser());
     }
 
     @Test
@@ -1334,7 +1335,7 @@ class CFTTaskMapperTest {
         String formattedDueDate = CAMUNDA_DATA_TIME_FORMATTER.format(dueDate);
 
         List<TaskAttribute> attributes = getDefaultAttributes(formattedCreatedDate, formattedDueDate,
-                                                              null);
+            null);
 
         TaskResource taskResource = cftTaskMapper.mapToTaskResource(taskId, attributes);
         taskResource.setPriorityDate(null);
@@ -1464,7 +1465,7 @@ class CFTTaskMapperTest {
             taskRoleResources,
             "caseCategory",
             EXPECTED_ADDITIONAL_PROPERTIES,
-                "nextHearingId",
+            "nextHearingId",
             OffsetDateTime.parse("2021-05-09T20:15:45.345875+01:00"),
             OffsetDateTime.parse("2021-05-09T20:15:45.345875+01:00")
         );
@@ -1856,7 +1857,7 @@ class CFTTaskMapperTest {
             "taskId",
             OffsetDateTime.parse("2021-05-09T20:15:45.345875+01:00"),
             false, false, false, false, false, false, false, false, false, false
-            );
+        );
 
         final TaskRolePermissions taskRolePermissions = cftTaskMapper.mapToTaskRolePermissions(roleResource);
 
@@ -2286,7 +2287,7 @@ class CFTTaskMapperTest {
             singleton(roleResource),
             "caseCategory",
             EXPECTED_ADDITIONAL_PROPERTIES,
-                "nextHearingId",
+            "nextHearingId",
             OffsetDateTime.parse("2021-05-09T20:15:45.345875+01:00"),
             OffsetDateTime.parse("2021-05-09T20:15:45.345875+01:00")
         );
@@ -2330,7 +2331,7 @@ class CFTTaskMapperTest {
             new TaskAttribute(TaskAttributeDefinition.TASK_ADDITIONAL_PROPERTIES, EXPECTED_ADDITIONAL_PROPERTIES),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_ID, "nextHearingId"),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_DATE,
-                              CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
+                CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
         );
     }
 
@@ -2407,7 +2408,7 @@ class CFTTaskMapperTest {
             new TaskAttribute(TaskAttributeDefinition.TASK_ADDITIONAL_PROPERTIES, EXPECTED_ADDITIONAL_PROPERTIES),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_ID, "nextHearingId"),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_DATE,
-                              CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
+                CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
         );
     }
 
@@ -2448,7 +2449,7 @@ class CFTTaskMapperTest {
             new TaskAttribute(TaskAttributeDefinition.TASK_ADDITIONAL_PROPERTIES, EXPECTED_ADDITIONAL_PROPERTIES),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_ID, "nextHearingId"),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_DATE,
-                              CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
+                CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
         );
     }
 
@@ -2488,7 +2489,7 @@ class CFTTaskMapperTest {
             new TaskAttribute(TaskAttributeDefinition.TASK_ADDITIONAL_PROPERTIES, EXPECTED_ADDITIONAL_PROPERTIES),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_ID, "nextHearingId"),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_DATE,
-                              CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
+                CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
         );
     }
 
@@ -2532,7 +2533,7 @@ class CFTTaskMapperTest {
             new TaskAttribute(TaskAttributeDefinition.TASK_ADDITIONAL_PROPERTIES, EXPECTED_ADDITIONAL_PROPERTIES),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_ID, "nextHearingId"),
             new TaskAttribute(TaskAttributeDefinition.TASK_NEXT_HEARING_DATE,
-                              CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
+                CAMUNDA_DATA_TIME_FORMATTER.format(ZonedDateTime.now()))
         );
 
     }
