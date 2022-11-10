8679c39c17a5c9839f9ea7ae27e013f4d0d03ad7
rafalkalita <rkalitainfo@gmail.com>
2022-11-10 12:19:16 +0000

    MI Reporting - Flyway FlywayMigrationStrategy introduced

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/db/FlywayReplicaMigrationConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/db/FlywayReplicaMigrationConfiguration.java
@@ -3,9 +3,10 @@ package uk.gov.hmcts.reform.wataskmanagementapi.config.db;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationVersion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 @Configuration
@@ -16,24 +17,34 @@ public class FlywayReplicaMigrationConfiguration {
     @Autowired
     private DataSource replicaDataSource;
 
-    @PostConstruct
-    public void migrateFlyway() {
-        Flyway flywayBase = Flyway.configure()
-            .dataSource(dataSource)
-            .schemas("cft_task_db")
-            .locations("db/migration")
-            .baselineOnMigrate(true)
-            .target(MigrationVersion.LATEST).load();
+    @Bean
+    public FlywayMigrationStrategy multiDBMigrateStrategy() {
+        FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
+            @Override
+            public void migrate(Flyway flyway) {
+                Flyway flywayBase = Flyway.configure()
+                    .dataSource(dataSource)
+                    .schemas("cft_task_db")
+                    .defaultSchema("cft_task_db")
+                    .locations("db/migration")
+                    .baselineOnMigrate(true)
+                    .target(MigrationVersion.LATEST).load();
 
-        flywayBase.migrate();
+                flywayBase.migrate();
 
-        Flyway flywayReplica = Flyway.configure()
-            .dataSource(replicaDataSource)
-            .schemas("cft_task_db")
-            .locations("dbreplica/migration")
-            .baselineOnMigrate(true)
-            .target(MigrationVersion.LATEST).load();
+                Flyway flywayReplica = Flyway.configure()
+                    .dataSource(replicaDataSource)
+                    .schemas("cft_task_db")
+                    .defaultSchema("cft_task_db")
+                    .locations("dbreplica/migration")
+                    .baselineOnMigrate(true)
+                    .target(MigrationVersion.LATEST).load();
 
-        flywayReplica.migrate();
+                flywayReplica.migrate();
+
+            }
+        };
+
+        return strategy;
     }
 }
