f5ed63155eccf0e9b07d156169ce75ccaa375d84
oguz akin <akinserdaroguzhan@gmail.com>
2022-11-16 12:31:15 +0000

    refactor extract values method

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/tasktype/TaskType.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/tasktype/TaskType.java
@@ -3,12 +3,12 @@ package uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.tasktype;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ToString
-@EqualsAndHashCode
 public class TaskType {
 
     private final String taskTypeId;
@@ -28,6 +28,19 @@ public class TaskType {
 
     public String getTaskTypeName() {
         return taskTypeName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskType taskType = (TaskType) o;
+        return taskTypeId.equalsIgnoreCase(taskType.taskTypeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(taskTypeId);
     }
 
 }
