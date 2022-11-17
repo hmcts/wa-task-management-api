b604133af776c854fedbd55f33ea6fd1e416b495
Chris Davidson <christopher.davidson@btinternet.com>
2022-11-17 11:20:08 +0000

    RWA 1886 added logging deleted test

--- src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/config/CcdRetryableClient.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/config/CcdRetryableClient.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.config;
 
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 
+@Slf4j
 @Service
 public class CcdRetryableClient {
     private final CoreCaseDataApi coreCaseDataApi;
@@ -17,10 +19,10 @@ public class CcdRetryableClient {
         this.coreCaseDataApi = coreCaseDataApi;
     }
 
-    //@Retryable(value = FeignException.class, maxAttempts = 3, backoff = @Backoff(delay = 100))
-    @Retryable(value = FeignException.class)
+    @Retryable(value = FeignException.class, maxAttempts = 3, backoff = @Backoff(delay = 100))
     public StartEventResponse startForCaseworker(String authorisation, String serviceAuthorization,
         String userId, String jurisdictionId, String caseType, String eventId) {
+        log.info("Calling submitEventForCaseWorker");
         return coreCaseDataApi.startForCaseworker(authorisation, serviceAuthorization,
                                                   userId, jurisdictionId, caseType, eventId);
     }
@@ -28,6 +30,7 @@ public class CcdRetryableClient {
     @Retryable(value = FeignException.class, maxAttempts = 3, backoff = @Backoff(delay = 100))
     public CaseDetails submitForCaseworker(String authorisation, String serviceAuthorisation, String userId,
         String jurisdictionId, String caseType, boolean ignoreWarning, CaseDataContent caseDataContent) {
+        log.info("Calling submitForCaseworker");
         return coreCaseDataApi.submitForCaseworker(authorisation, serviceAuthorisation, userId, jurisdictionId,
             caseType, ignoreWarning, caseDataContent);
     }
@@ -37,6 +40,7 @@ public class CcdRetryableClient {
     @Retryable(value = FeignException.class, maxAttempts = 3, backoff = @Backoff(delay = 100))
     public StartEventResponse startEventForCaseWorker(String authorisation, String serviceAuthorization,
         String userId, String jurisdictionId, String caseType, String caseId, String eventId) {
+        log.info("Calling startEventForCaseWorker");
         return coreCaseDataApi.startEventForCaseWorker(authorisation, serviceAuthorization,
             userId, jurisdictionId, caseType, caseId, eventId);
     }
@@ -45,6 +49,7 @@ public class CcdRetryableClient {
     public CaseDetails submitEventForCaseWorker(String authorisation, String serviceAuthorisation,
         String userId, String jurisdictionId, String caseType, String caseId, boolean ignoreWarning,
         CaseDataContent caseDataContent) {
+        log.info("Calling submitEventForCaseWorker");
         return coreCaseDataApi.submitEventForCaseWorker(authorisation, serviceAuthorisation, userId,
             jurisdictionId, caseType, caseId, ignoreWarning, caseDataContent);
     }
