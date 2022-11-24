732640425dc322c4e0feadfe137bb83303dc1b32
rafalkalita <rkalitainfo@gmail.com>
2022-11-24 15:09:23 +0000

    RWA-1834 Replication of data and tasks tables

--- src/main/resources/db/migration/V1.0.23__tasks_replica.sql
+++ src/main/resources/db/migration/V1.0.23__tasks_replica.sql
@@ -1,2 +1,14 @@
+-- drops replication slot if exists and creates it
+SELECT pg_drop_replication_slot(slot_name) FROM pg_replication_slots WHERE slot_name = 'test_slot_v1';
+SELECT pg_create_logical_replication_slot('test_slot_v1', 'pgoutput');
+
+CREATE TABLE cft_task_db.data(id serial primary key, data text);
+
 DROP PUBLICATION IF EXISTS task_publication;
-CREATE PUBLICATION task_publication FOR TABLE tasks WITH (publish = 'insert,update,delete');
+CREATE PUBLICATION task_publication WITH (publish = 'insert,update,delete');
+ALTER PUBLICATION task_publication ADD TABLE cft_task_db.data;
+ALTER PUBLICATION task_publication ADD TABLE cft_task_db.tasks;
+
+-- GRANT CONNECT ON DATABASE cft_task_db TO repl_user;
+GRANT USAGE ON SCHEMA cft_task_db TO repl_user;
+GRANT SELECT on cft_task_db.data TO repl_user;
--- src/main/resources/dbreplica/migration/V1.0.2__init_report_db.sql
+++ src/main/resources/dbreplica/migration/V1.0.2__init_report_db.sql
@@ -1,45 +1,91 @@
--- DROP TABLE IF EXISTS tasks CASCADE;
-CREATE TABLE cft_task_db_replica.tasks
+-- DROP TABLE IF EXISTS "cft_task_db"."tasks" CASCADE;
+CREATE TABLE "cft_task_db"."tasks"
 (
-  task_id                 TEXT,
-  task_name               TEXT,
-  task_type               TEXT,
-  due_date_time           TIMESTAMP,
-  state                   TEXT,
-  task_system             TEXT,
-  security_classification TEXT,
-  title                   TEXT,
-  description             TEXT,
-  notes                   JSONB,
-  major_priority          INTEGER,
-  minor_priority          INTEGER,
-  assignee                TEXT,
-  auto_assigned           BOOLEAN   default false,
-  execution_type_code     TEXT,
-  work_type               TEXT,
-  role_category           TEXT,
-  has_warnings            BOOLEAN   default false,
-  assignment_expiry       TIMESTAMP,
-  case_id                 TEXT,
-  case_type_id            TEXT,
-  case_category           TEXT,
-  case_name               TEXT,
-  jurisdiction            TEXT,
-  region                  TEXT,
-  region_name             TEXT,
-  location                TEXT,
-  location_name           TEXT,
-  business_context        TEXT,
-  termination_reason      TEXT,
-  created                 TIMESTAMP default CURRENT_TIMESTAMP,
-  updated                 TIMESTAMP default CURRENT_TIMESTAMP,
-  updated_by              TEXT,
-  update_action           TEXT,
-  PRIMARY KEY (task_id)
+  "task_id" Text NOT NULL,
+  "task_name" Text,
+  "task_type" Text,
+  "due_date_time" Timestamp Without Time Zone NOT NULL,
+  "state" Text,
+  "task_system" Text,
+  "security_classification" Text,
+  "title" Text,
+  "description" Text,
+  "notes" JSONB,
+  "major_priority" Integer DEFAULT 5000,
+  "minor_priority" Integer DEFAULT 500,
+  "assignee" Text,
+  "auto_assigned" Boolean DEFAULT false,
+  "execution_type_code" Text,
+  "work_type" Text,
+  "role_category" Text,
+  "has_warnings" Boolean DEFAULT false,
+  "assignment_expiry" Timestamp Without Time Zone,
+  "case_id" Text,
+  "case_type_id" Text,
+  "case_category" Text,
+  "case_name" Text,
+  "jurisdiction" Text,
+  "region" Text,
+  "region_name" Text,
+  "location" Text,
+  "location_name" Text,
+  "business_context" Text,
+  "termination_reason" Text,
+  "created" Timestamp Without Time Zone NOT NULL,
+  "additional_properties" JSONB,
+  "reconfigure_request_time" Timestamp Without Time Zone,
+  "next_hearing_id" Text,
+  "next_hearing_date" Timestamp Without Time Zone,
+  "priority_date" Timestamp Without Time Zone NOT NULL,
+  "last_reconfiguration_time" Timestamp Without Time Zone,
+  "last_updated_timestamp" Timestamp Without Time Zone,
+  "last_updated_user" Text,
+  "last_updated_action" Text,
+  PRIMARY KEY ( "task_id" )
 );
 
+-- DROP TABLE IF EXISTS tasks CASCADE;
+-- CREATE TABLE cft_task_db.tasks
+-- (
+--   task_id                 TEXT,
+--   task_name               TEXT,
+--   task_type               TEXT,
+--   due_date_time           TIMESTAMP,
+--   state                   TEXT,
+--   task_system             TEXT,
+--   security_classification TEXT,
+--   title                   TEXT,
+--   description             TEXT,
+--   notes                   JSONB,
+--   major_priority          INTEGER,
+--   minor_priority          INTEGER,
+--   assignee                TEXT,
+--   auto_assigned           BOOLEAN   default false,
+--   execution_type_code     TEXT,
+--   work_type               TEXT,
+--   role_category           TEXT,
+--   has_warnings            BOOLEAN   default false,
+--   assignment_expiry       TIMESTAMP,
+--   case_id                 TEXT,
+--   case_type_id            TEXT,
+--   case_category           TEXT,
+--   case_name               TEXT,
+--   jurisdiction            TEXT,
+--   region                  TEXT,
+--   region_name             TEXT,
+--   location                TEXT,
+--   location_name           TEXT,
+--   business_context        TEXT,
+--   termination_reason      TEXT,
+--   created                 TIMESTAMP default CURRENT_TIMESTAMP,
+--   updated                 TIMESTAMP default CURRENT_TIMESTAMP,
+--   updated_by              TEXT,
+--   update_action           TEXT,
+--   PRIMARY KEY (task_id)
+-- );
+
 -- drop table if exists task_history cascade;
-create table cft_task_db_replica.task_history
+create table cft_task_db.task_history
 (
   update_id               SERIAL NOT NULL,
   task_id                 TEXT,
@@ -74,8 +120,8 @@ create table cft_task_db_replica.task_history
   update_action           TEXT,
   PRIMARY KEY (update_id)
 );
-create index task_history_update_id_idx on cft_task_db_replica.task_history (task_id, update_id);
-create index task_history_updated_idx on cft_task_db_replica.task_history (task_id, updated);
+create index task_history_update_id_idx on cft_task_db.task_history (task_id, update_id);
+create index task_history_updated_idx on cft_task_db.task_history (task_id, updated);
 
 -- /*
 --  * Identifies the update record which holds the latest task data.
