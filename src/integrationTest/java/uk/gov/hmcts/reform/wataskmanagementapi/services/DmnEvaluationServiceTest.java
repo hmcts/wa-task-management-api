37d6390776266c266631feaf54dc1901b3c044b7
oguz akin <akinserdaroguzhan@gmail.com>
2022-11-24 11:34:01 +0000

    cache tests added

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/DmnEvaluationServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/DmnEvaluationServiceTest.java
@@ -1,13 +1,22 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.services;
 
+import com.github.benmanes.caffeine.cache.Ticker;
+import com.google.common.testing.FakeTicker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.wataskmanagementapi.Application;
 import uk.gov.hmcts.reform.wataskmanagementapi.clients.CamundaServiceApi;
 import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda.DmnRequest;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -15,6 +24,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.wataskmanagementapi.utils.ServiceMocks.SERVICE_AUTHORIZATION_TOKEN;
 
 @SpringBootTest
+@ContextConfiguration
 @ActiveProfiles({"integration"})
 public class DmnEvaluationServiceTest {
 
@@ -96,4 +106,71 @@ public class DmnEvaluationServiceTest {
 
     }
 
+
+    @Test
+    void should_use_cache_until_expires_then_call_service() {
+
+        // given
+        TestConfiguration.fakeTicker.advance(1, TimeUnit.DAYS);
+        String dmnKey = "wa-task-types-wa-wacasetype";
+
+        when(serviceAuthTokenGenerator.generate())
+            .thenReturn(SERVICE_AUTHORIZATION_TOKEN);
+
+        // when first attempt, call the service
+        dmnEvaluationService.evaluateTaskTypesDmn("wa", dmnKey);
+
+        // then
+        verify(camundaServiceApi, times(1))
+            .evaluateTaskTypesDmnTable(
+                SERVICE_AUTHORIZATION_TOKEN,
+                dmnKey,
+                "wa",
+                new DmnRequest<>()
+            );
+
+
+        // when 10 mins later use cache response
+        TestConfiguration.fakeTicker.advance(10, TimeUnit.MINUTES);
+        dmnEvaluationService.evaluateTaskTypesDmn("wa", dmnKey);
+
+        // then
+        verify(camundaServiceApi, times(1))
+            .evaluateTaskTypesDmnTable(
+                SERVICE_AUTHORIZATION_TOKEN,
+                dmnKey,
+                "wa",
+                new DmnRequest<>()
+            );
+
+
+        // when 61 mins later cache expired should call the service 1 more
+        TestConfiguration.fakeTicker.advance(61, TimeUnit.MINUTES);
+        dmnEvaluationService.evaluateTaskTypesDmn("wa", dmnKey);
+
+        // then
+        verify(camundaServiceApi, times(2))
+            .evaluateTaskTypesDmnTable(
+                SERVICE_AUTHORIZATION_TOKEN,
+                dmnKey,
+                "wa",
+                new DmnRequest<>()
+            );
+    }
+
+    /*
+     * This class provides us change system time to test caching
+     */
+    @Configuration
+    @Import(Application.class)
+    public static class TestConfiguration {
+
+        static FakeTicker fakeTicker = new FakeTicker();
+
+        @Bean
+        public Ticker ticker() {
+            return fakeTicker::read;
+        }
+
+    }
 }
