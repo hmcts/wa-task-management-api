635d2fc7588686e294df962f4750b00df7f23961
rafalkalita <rkalitainfo@gmail.com>
2022-11-28 16:36:49 +0000

    RWA-1834 Added plsql stored procedures

--- src/main/resources/db/migration/V1.0.23__tasks_replica.sql
+++ src/main/resources/db/migration/V1.0.23__tasks_replica.sql
@@ -2,13 +2,10 @@
 SELECT pg_drop_replication_slot(slot_name) FROM pg_replication_slots WHERE slot_name = 'test_slot_v1';
 SELECT pg_create_logical_replication_slot('test_slot_v1', 'pgoutput');
 
-CREATE TABLE cft_task_db.data(id serial primary key, data text);
-
 DROP PUBLICATION IF EXISTS task_publication;
 CREATE PUBLICATION task_publication WITH (publish = 'insert,update,delete');
-ALTER PUBLICATION task_publication ADD TABLE cft_task_db.data;
 ALTER PUBLICATION task_publication ADD TABLE cft_task_db.tasks;
 
 -- GRANT CONNECT ON DATABASE cft_task_db TO repl_user;
 GRANT USAGE ON SCHEMA cft_task_db TO repl_user;
-GRANT SELECT on cft_task_db.data TO repl_user;
+GRANT SELECT ON cft_task_db.tasks TO repl_user;
