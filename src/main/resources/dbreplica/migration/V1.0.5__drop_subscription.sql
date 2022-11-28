44a9931f7a572ba99a189316d263c44e5ff8521b
rafalkalita <rkalitainfo@gmail.com>
2022-11-28 16:38:39 +0000

    RWA-1834 Updated user privileges location

--- src/main/resources/dbreplica/migration/V1.0.5__drop_subscription.sql
+++ src/main/resources/dbreplica/migration/V1.0.5__drop_subscription.sql
@@ -1,5 +1,4 @@
 -- DROP SUBSCRIPTION task_subscription;
 
 -- GRANT CONNECT ON DATABASE cft_task_db_replica TO repl_user;
-GRANT USAGE ON SCHEMA cft_task_db TO repl_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cft_task_db TO repl_user;
+
