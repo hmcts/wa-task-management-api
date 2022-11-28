28bb76911bb1d96668dae2b4b0aa976e66d8dc7b
rafalkalita <rkalitainfo@gmail.com>
2022-11-28 16:38:39 +0000

    RWA-1834 Updated user privileges location

--- src/main/resources/dbreplica/migration/V1.0.6__configure_replication.sql
+++ src/main/resources/dbreplica/migration/V1.0.6__configure_replication.sql
@@ -1,3 +1,6 @@
+GRANT USAGE ON SCHEMA cft_task_db TO repl_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cft_task_db TO repl_user;
+
 create subscription task_subscription
   connection 'host=localhost port=5432 dbname=cft_task_db user=repl_user password=repl_password'
   publication task_publication
