772da093541a33177336ff7844495a7345afdc2f
Chris Davidson <christopher.davidson@btinternet.com>
2022-11-29 11:07:01 +0000

    RWA 1768 reviewed test

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/calendar/BankHolidaysTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/domain/entities/calendar/BankHolidaysTest.java
@@ -9,24 +9,24 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static uk.gov.hmcts.reform.wataskmanagementapi.services.calendar.DateCalculator.DEFAULT_NON_WORKING_CALENDAR;
 
 public class BankHolidaysTest {
 
     @Test
-    void should_create_full_object_and_checkDates() throws IOException {
+    void should_create_full_object_and_check_dates() throws IOException {
         ObjectMapper om = new ObjectMapper();
         BankHolidays bankHolidays = om.readValue(new URL(DEFAULT_NON_WORKING_CALENDAR), BankHolidays.class);
         assertTrue(bankHolidays.getDivision().equals("england-and-wales"));
         assertTrue(bankHolidays.getEvents().size() > 0);
         assertTrue(bankHolidays.getEvents().get(0).hashCode() != 0);
-        assertTrue(!bankHolidays.getEvents().get(0).equals(null));
+        assertNotNull(bankHolidays.getEvents().get(0));
         assertTrue(bankHolidays.getEvents().get(0).equals(bankHolidays.getEvents().get(0)));
 
         for (BankHolidays.EventDate eventDate: bankHolidays.getEvents()) {
             assertTrue(isValid(eventDate.getDate()));
-            assertTrue(eventDate.isWorkingDay() || !eventDate.isWorkingDay());
         }
 
     }
