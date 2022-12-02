8fee4c8a7303954316df972fe2d0f1c4cfe44ff4
dharmendra kumar <dharmendra.kumar@hmcts.net>
2022-12-02 12:54:20 +0000

    resolve due date issue

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/watasks/controllers/PostTaskInitiateByIdControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/watasks/controllers/PostTaskInitiateByIdControllerTest.java
@@ -228,11 +228,6 @@ public class PostTaskInitiateByIdControllerTest extends SpringBootFunctionalBase
         Consumer<Response> assertConsumer = (result) -> {
             result.prettyPrint();
 
-            String expectedDueDate = ZonedDateTime.of(2022, 10, 25,
-                                                      20, 0, 0, 0,
-                                                      ZoneId.of("Europe/London")
-            ).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
-
             result.then().assertThat()
                 .statusCode(HttpStatus.OK.value())
                 .and()
@@ -262,7 +257,7 @@ public class PostTaskInitiateByIdControllerTest extends SpringBootFunctionalBase
                 .body("task.major_priority", equalTo(1000))
                 .body("task.priority_date", equalTo("2022-12-07T13:00:00+0000"))
                 .body("task.due_date", notNullValue())
-                .body("task.due_date", equalTo(expectedDueDate));
+                .body("task.due_date", equalTo("2022-10-25T20:00:00+0000"));
         };
 
         initiateTask(taskVariables, assertConsumer);
