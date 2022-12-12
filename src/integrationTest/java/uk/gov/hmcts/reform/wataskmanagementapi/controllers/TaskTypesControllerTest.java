3d620e325ddb343491ae8b15235265752dfbb821
Chris Davidson <christopher.davidson@btinternet.com>
2022-12-12 16:37:53 +0000

    checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/TaskTypesControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/TaskTypesControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
