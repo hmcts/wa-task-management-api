854bec28cbcdc11d92c7a3f89f8ff0e893ede171
Chris Davidson <christopher.davidson@btinternet.com>
2023-01-05 12:10:28 +0000

    Added import

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/calendar/PublicHolidaysCollection.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/calendar/PublicHolidaysCollection.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.services.calendar;
 
 import feign.Feign;
-import feign.RequestInterceptor;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.entity.ContentType;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.wataskmanagementapi.clients.BankHolidaysApi;
+import uk.gov.hmcts.reform.wataskmanagementapi.config.SnakeCaseFeignConfiguration;
 import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.calendar.BankHolidays;
 
 import java.time.LocalDate;
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @Component
+@Import(SnakeCaseFeignConfiguration.class)
 @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
 public class PublicHolidaysCollection {
 
@@ -69,16 +70,9 @@ public class PublicHolidaysCollection {
 
     private BankHolidaysApi bankHolidaysApi(String uri) {
         return Feign.builder()
-            .requestInterceptor(requestInterceptor())
             .decoder(feignDecoder)
             .encoder(feignEncoder)
             .target(BankHolidaysApi.class, uri);
     }
 
-    public RequestInterceptor requestInterceptor() {
-        return requestTemplate -> {
-            log.debug("Setting content type to JSON");
-            requestTemplate.header("Content-Type", ContentType.APPLICATION_JSON.getMimeType());
-        };
-    }
 }
