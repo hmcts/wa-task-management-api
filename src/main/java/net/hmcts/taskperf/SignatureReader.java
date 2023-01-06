016b86b60f05717728e53f279b8bc7d44e16a0b4
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2023-01-06 13:17:55 +0000

    review comments fixed

---
+++ src/main/java/net/hmcts/taskperf/SignatureReader.java
@@ -0,0 +1,50 @@
+package net.hmcts.taskperf;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SignatureReader {
+    public static void main(String a[]) {
+
+        try {
+            InputStream stream = SignatureReader.class.getClassLoader().getResourceAsStream("skills.txt");
+
+            StringBuilder textBuilder = new StringBuilder();
+            try (Reader reader = new BufferedReader(new InputStreamReader
+                                                        (stream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+                int c = 0;
+                while ((c = reader.read()) != -1) {
+                    textBuilder.append((char) c);
+                }
+            }
+
+            List<String> allLines = Arrays.asList(textBuilder.toString().split(","));
+            List<Signature> signatures = allLines.stream().map(Signature::new).collect(Collectors.toList());
+            System.out.println("Total : " + signatures.size());
+
+            signatures.forEach(s -> s.setAuthorisations("*"));
+            Set<Signature> uniqueSignatureWithoutSkills = new HashSet<>(signatures);
+            System.out.println("Unique Signatures Without Skills : " + uniqueSignatureWithoutSkills.size());
+            uniqueSignatureWithoutSkills.forEach(s -> System.out.println(s.toString()));
+
+            Set<String> caseIds = signatures.stream().map(Signature::getCaseId).collect(Collectors.toSet());
+            System.out.println("Case Ids : " + caseIds.size());
+            caseIds.forEach(s -> System.out.println(s));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
