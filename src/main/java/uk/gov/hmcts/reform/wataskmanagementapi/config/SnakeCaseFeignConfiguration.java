564d9a17ae61b920eeeec4f1fe4c8c5cd5086959
Chris Davidson <christopher.davidson@btinternet.com>
2023-01-10 16:22:38 +0000

    RWA 1768 added text plain

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/SnakeCaseFeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/SnakeCaseFeignConfiguration.java
@@ -14,11 +14,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.util.Arrays;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 @Configuration
@@ -46,18 +48,14 @@ public class SnakeCaseFeignConfiguration {
         jacksonConverter.setSupportedMediaTypes(Arrays.asList(MediaType.valueOf(TEXT_PLAIN_VALUE + ";charset=utf-8"),
                                                               APPLICATION_JSON,
                                                               new MediaType("application", "*+json"),
-                                                              new MediaType("text", "plain")));
+                                                              TEXT_PLAIN));
         ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
         return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
     }
 
     @Bean
     public Encoder feignEncoder() {
-        MappingJackson2HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper);
-        jacksonConverter.setSupportedMediaTypes(Arrays.asList(MediaType.valueOf(TEXT_PLAIN_VALUE + ";charset=utf-8"),
-                                                              APPLICATION_JSON,
-                                                              new MediaType("application", "*+json"),
-                                                              new MediaType("text", "plain")));
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper);
         ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
         return new SpringEncoder(objectFactory);
     }
