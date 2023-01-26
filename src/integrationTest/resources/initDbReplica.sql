beb5bc7501f6ca4bf63ede8c3decfcbe7a7ff523
Chris Davidson <christopher.davidson@btinternet.com>
2023-01-26 15:54:48 +0000

    RWA 1835 got integration test working

--- src/integrationTest/resources/initDbReplica.sql
+++ src/integrationTest/resources/initDbReplica.sql
@@ -1,4 +1,4 @@
 -- CREATE DATABASE cft_task_db_replica WITH OWNER=postgres ENCODING='UTF-8' CONNECTION LIMIT=-1;
-CREATE ROLE repl_user REPLICATION LOGIN PASSWORD 'repl_password';
+CREATE ROLE repl_user SUPERUSER REPLICATION LOGIN PASSWORD 'repl_password';
 
 GRANT CONNECT ON DATABASE cft_task_db_replica TO repl_user;
