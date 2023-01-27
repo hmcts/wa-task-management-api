6fceb4e0ac0fa0b42a7a965ca5a8ff24efad3e95
Chris Davidson <christopher.davidson@btinternet.com>
2023-01-27 11:50:17 +0000

    RWA 1835 fixing build checkstyle and PMD

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/replicarepository/TaskHistoryResourceRepository.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/replicarepository/TaskHistoryResourceRepository.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.cft.replicarepository;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskHistoryResource;
 
 import java.util.List;
@@ -22,5 +19,5 @@ public interface TaskHistoryResourceRepository
     List<TaskHistoryResource> getByCaseId(String caseId);
 
     @Query(value = CHECK_SUBSCRIPTION, nativeQuery = true)
-    int isSubscriptionPresent();
+    int countSubscriptions();
 }
--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReportingService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReportingService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.entities.TaskHistoryResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.replicarepository.TaskHistoryResourceRepository;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.repository.TaskResourceRepository;
+import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.ReplicationException;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -33,6 +34,19 @@ public class MIReportingService {
     private final TaskHistoryResourceRepository taskHistoryRepository;
     private final TaskResourceRepository taskResourceRepository;
 
+    private final Pattern urlMatchingPattern = Pattern.compile(
+        "jdbc:(tc:)?"
+            + "(?<databaseType>[a-z0-9]+)"
+            + "(:(?<imageTag>[^:]+))?"
+            + "://"
+            + "(?<hostString>[^?]+)"
+            + ":"
+            + "(?<port>[^?]+)"
+            + "/"
+            + "(?<dbname>[^?]+)"
+            + "(?<queryParameters>\\?.*)?"
+    );
+
     public MIReportingService(TaskHistoryResourceRepository tasksHistoryRepository,
                               TaskResourceRepository taskResourceRepository) {
         this.taskHistoryRepository = tasksHistoryRepository;
@@ -45,21 +59,21 @@ public class MIReportingService {
 
     public void logicalReplicationCheck() {
         LOGGER.debug("Postgresql logical replication check executed");
-        if (!isReplicationSlotPresent()) {
-            LOGGER.info("Creating logical replication slot");
-            createReplicationSlot();
-        } else {
+        if (isReplicationSlotPresent()) {
             if (!isPublicationPresent()) {
                 createPublication();
             }
             if (!isSubscriptionPresent()) {
                 createSubscription();
             }
+        } else {
+            LOGGER.info("Creating logical replication slot");
+            createReplicationSlot();
         }
     }
 
     private boolean isReplicationSlotPresent() {
-        int count = taskResourceRepository.isReplicationSlotPresent();
+        int count = taskResourceRepository.countReplicationSlots();
         if (count == 0) {
             LOGGER.info("No logical replication slot present for " + MAIN_SLOT_NAME);
             return false;
@@ -75,7 +89,7 @@ public class MIReportingService {
     }
 
     private boolean isPublicationPresent() {
-        int count = taskResourceRepository.isPublicationPresent();
+        int count = taskResourceRepository.countPublications();
         if (count == 0) {
             LOGGER.info("No publication present");
             return false;
@@ -91,7 +105,7 @@ public class MIReportingService {
     }
 
     private boolean isSubscriptionPresent() {
-        int count = taskHistoryRepository.isSubscriptionPresent();
+        int count = taskHistoryRepository.countSubscriptions();
         if (count == 0) {
             LOGGER.info("No subscription present");
             return false;
@@ -102,15 +116,14 @@ public class MIReportingService {
     }
 
     private void createSubscription() {
-        try {
-            Connection connection = dataSource.getConnection();
-            LOGGER.info("Primary datasource URL: " + connection.getMetaData().getURL());
+        try (Connection connection = dataSource.getConnection();
+             Connection connection2 = replicaDataSource.getConnection();) {
 
-            Connection connection2 = replicaDataSource.getConnection();
+            LOGGER.info("Primary datasource URL: " + connection.getMetaData().getURL());
             LOGGER.info("Replica datasource URL: " + connection2.getMetaData().getURL());
 
-            Matcher urlMatcher = Patterns.URL_MATCHING_PATTERN.matcher(connection.getMetaData().getURL());
-            Matcher replicaUrlMatcher = Patterns.URL_MATCHING_PATTERN.matcher(connection2.getMetaData().getURL());
+            Matcher urlMatcher = urlMatchingPattern.matcher(connection.getMetaData().getURL());
+            Matcher replicaUrlMatcher = urlMatchingPattern.matcher(connection2.getMetaData().getURL());
             if (urlMatcher.matches() && replicaUrlMatcher.matches()) {
                 String host = urlMatcher.group("hostString");
                 String port = urlMatcher.group("port");
@@ -126,53 +139,57 @@ public class MIReportingService {
                 LOGGER.error("Cannot extract publication URL from the datasource");
             }
 
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOGGER.error("Error closing connection", e);
+            }
+
+            try {
+                connection2.close();
+            } catch (SQLException e) {
+                LOGGER.error("Error closing connection", e);
+            }
+
+
         } catch (SQLException ex) {
             LOGGER.error("Primary datasource connection exception.", ex);
         }
     }
 
-    void createSubscription(String host, String port, String dbName, String replicaHost, String replicaPort, String rDbName) {
+    void createSubscription(String host, String port, String dbName,
+                            String replicaHost, String replicaPort, String replicaDbName) {
         String user = "repl_user";
         String password = "repl_password";
 
-        String replicaUrl = "jdbc:postgresql://" + host + ":" + replicaPort + "/" + rDbName + "?user=" + user + "&password=" + password;
+        String replicaUrl = "jdbc:postgresql://" + host + ":" + replicaPort + "/" + replicaDbName
+            + "?user=" + user + "&password=" + password;
         LOGGER.info("replicaUrl = " + replicaUrl);
 
         String subscriptionUrl;
         if ("5432".equals(port)) {
             //hard coded host for local environment, will need fixing when we move to remote environments
-            subscriptionUrl = "postgresql://" + "ccd-shared-database-0" + ":" + port + "/" + dbName + "?user=" + user + "&password=" + password;
+            subscriptionUrl = "postgresql://" + "ccd-shared-database-0" + ":" + port + "/" + dbName
+                + "?user=" + user + "&password=" + password;
         } else {
             //this is hard coded for integration test locally
-            subscriptionUrl = "postgresql://" + "cft_task_db" + ":" + "5432" + "/" + dbName + "?user=" + user + "&password=" + password;
+            subscriptionUrl = "postgresql://" + "cft_task_db" + ":" + "5432" + "/" + dbName
+                + "?user=" + user + "&password=" + password;
         }
 
         try (Connection subscriptionConn = DriverManager.getConnection(replicaUrl);
              Statement subscriptionStatement = subscriptionConn.createStatement();) {
 
-            String sql = "CREATE SUBSCRIPTION task_subscription CONNECTION '" + subscriptionUrl + "' PUBLICATION task_publication WITH (slot_name = main_slot_v1, create_slot = FALSE);";
+            String sql = "CREATE SUBSCRIPTION task_subscription CONNECTION '" + subscriptionUrl
+                + "' PUBLICATION task_publication WITH (slot_name = main_slot_v1, create_slot = FALSE);";
             LOGGER.info("CREATE SUBSCRIPTION SQL = " + sql);
             subscriptionStatement.execute(sql);
 
             LOGGER.info("Subscription created");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            LOGGER.error("Error setting up replication", e);
+            throw new ReplicationException("An error occurred during setting up of replication", e);
         }
 
-    }
-
-    public interface Patterns {
-        Pattern URL_MATCHING_PATTERN = Pattern.compile(
-            "jdbc:(tc:)?" +
-                "(?<databaseType>[a-z0-9]+)" +
-                "(:(?<imageTag>[^:]+))?" +
-                "://" +
-                "(?<hostString>[^?]+)" +
-                ":" +
-                "(?<port>[^?]+)" +
-                "/" +
-                "(?<dbname>[^?]+)" +
-                "(?<queryParameters>\\?.*)?"
-        );
     }
 }
