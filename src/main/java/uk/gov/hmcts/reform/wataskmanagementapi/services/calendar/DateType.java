8d437cfc8ec828a0f27c0be3ff7c82108d0136ea
dharmendra kumar <dharmendra.kumar@hmcts.net>
2023-02-07 11:17:27 +0000

    increase code coverage

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/calendar/DateType.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/calendar/DateType.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services.calendar;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
-import static java.util.Arrays.stream;
 import static uk.gov.hmcts.reform.wataskmanagementapi.services.calendar.DateCalculator.DATE_TIME_FORMATTER;
 import static uk.gov.hmcts.reform.wataskmanagementapi.services.calendar.DateCalculator.DEFAULT_ZONED_DATE_TIME;
 
@@ -21,12 +19,6 @@ public enum DateType {
         this.type = type;
         this.defaultTime = defaultTime;
         this.dateTimeFormatter = dateTimeFormatter;
-    }
-
-    public static Optional<DateType> from(String name) {
-        return stream(values())
-            .filter(v -> v.getType().equalsIgnoreCase(name))
-            .findFirst();
     }
 
     public String getType() {
