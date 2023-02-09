4d8fbeae8ef0158db39ff2f89cb89af106b856fa
Chris Davidson <christopher.davidson@btinternet.com>
2023-02-09 15:31:44 +0000

    removed test replication

--- src/main/resources/db/migration/V1.0.24__tasks_replica.sql
+++ src/main/resources/db/migration/V1.0.24__tasks_replica.sql
@@ -1,6 +1,6 @@
 -- drops replication slot if exists and creates it
-SELECT pg_drop_replication_slot(slot_name) FROM pg_replication_slots WHERE slot_name = 'test_slot_v1';
-SELECT pg_create_logical_replication_slot('test_slot_v1', 'pgoutput');
+--SELECT pg_drop_replication_slot(slot_name) FROM pg_replication_slots WHERE slot_name = 'test_slot_v1';
+--SELECT pg_create_logical_replication_slot('test_slot_v1', 'pgoutput');
 
 DROP PUBLICATION IF EXISTS task_publication;
 CREATE PUBLICATION task_publication WITH (publish = 'insert,update,delete');
