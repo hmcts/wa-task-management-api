3e8d3b6b716e8c4baca153fac1197833fa4a68d0
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2023-02-28 19:32:10 +0000

    fix invalid flyway history

---
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/FlywayConfiguration.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfiguration {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
+
