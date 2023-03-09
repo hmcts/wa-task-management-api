8910c1b34c47766794cb7de9104a27053d298a1e
Chris Davidson <christopher.davidson@btinternet.com>
2023-03-09 16:59:40 +0000

    RWA 1835 get int tests working again

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/db/FlywayReplicaMigrationConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/db/FlywayReplicaMigrationConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
+@Profile("replica")
 @Configuration
 public class FlywayReplicaMigrationConfiguration {
     @Autowired
