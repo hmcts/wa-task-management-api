233031486c87eb1656b250dcf4e8dc1d648e933e
Chris Davidson <christopher.davidson@btinternet.com>
2023-03-09 17:07:10 +0000

    checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReportingServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReportingServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import junit.framework.AssertionFailedError;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
