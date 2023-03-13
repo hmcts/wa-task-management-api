3b96c3dba1b630d051c9ee152315cd49f654c16a
Lee Philip <leephilip365@yahoo.co.uk>
2023-03-13 11:49:02 +0000

    RWA-2342 task_assignments

--- src/main/resources/dbreplica/migration/V1.0.2__init_report_db.sql
+++ src/main/resources/dbreplica/migration/V1.0.2__init_report_db.sql
@@ -174,6 +174,22 @@ create table cft_task_db.reportable_task
   PRIMARY KEY ( "task_id" )
 );
 
+-- drop table if exists task_assignments cascade;
+create table cft_task_db.task_assignments
+(
+  assignment_id               SERIAL NOT NULL,
+  assignment_start            TIMESTAMP NOT NULL,
+  assignment_end              TIMESTAMP,
+  assignee                    TEXT NOT NULL,
+  task_id                     TEXT NOT NULL,
+  service                     TEXT NOT NULL,
+  location                    TEXT NOT NULL,
+  role_category               TEXT NOT NULL,
+  task_name                   TEXT NOT NULL,
+  assignment_end_reason       TEXT NOT NULL,
+  PRIMARY KEY ( "assignment_id" )
+);
+
 -- /*
 --  * Identifies the update record which holds the latest task data.
 --  */
