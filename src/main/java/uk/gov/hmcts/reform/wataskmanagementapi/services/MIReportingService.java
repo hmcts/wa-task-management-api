3904453f34174ebcf4035d9598dd1bb705d976f3
Chris Davidson <christopher.davidson@btinternet.com>
2023-04-14 10:59:26 +0100

    fixed pmd

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReportingService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReportingService.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.wataskmanagementapi.cft.replicarepository.SubscriptionCreator;
