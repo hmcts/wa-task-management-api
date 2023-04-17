6d76b8da1a25535fcb2a21c1fe42a6a2ab62d3d5
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2023-04-17 10:44:58 +0100

    RWA-2440 added execute task reconfiguration failure service

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/ExecuteTaskReconfigurationFailureServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/ExecuteTaskReconfigurationFailureServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -33,10 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExecuteTaskReconfigurationFailureServiceTest {
     @Autowired
     TaskResourceRepository taskResourceRepository;
-    @MockBean
-    private ConfigureTaskService configureTaskService;
-    @MockBean
-    private TaskAutoAssignmentService taskAutoAssignmentService;
+
     private ExecuteTaskReconfigurationFailureService executeTaskReconfigurationFailureService;
 
     @BeforeEach
