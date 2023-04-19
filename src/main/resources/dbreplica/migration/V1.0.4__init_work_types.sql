a9dc03eeeb8ab9718f754f6c16c4c5fc6e0c5164
Chris Davidson <christopher.davidson@btinternet.com>
2023-04-19 16:44:32 +0100

    RWA 2316 trying out replication

--- src/main/resources/dbreplica/migration/V1.0.4__init_work_types.sql
+++ src/main/resources/dbreplica/migration/V1.0.4__init_work_types.sql
@@ -5,3 +5,17 @@ CREATE TABLE work_types
     label TEXT,
     PRIMARY KEY (work_type_id)
 );
+
+INSERT INTO work_types(work_type_id, LABEL) VALUES
+    ('hearing-work','Hearing work'),
+    ('upper-tribunal','Upper Tribunal'),
+    ('routine-work','Routine work'),
+    ('decision-making-work','Decision-making work'),
+    ('applications','Applications'),
+    ('priority','Priority'),
+    ('access-requests','Access requests'),
+    ('error-management','Error management'),
+    ('review_case','Review Case'),
+    ('evidence','Evidence'),
+    ('follow_up','Follow Up');
+COMMIT;
