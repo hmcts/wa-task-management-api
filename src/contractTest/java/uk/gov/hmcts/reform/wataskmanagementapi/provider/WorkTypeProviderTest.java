683572f8d946300bcc4872c4e63066c83e8baa09
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2023-05-15 15:57:21 +0100

    RWA-2556: Add monitoring code for production problem

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/WorkTypeProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/WorkTypeProviderTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractProviderBaseTest;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.UserInfo;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.RoleAssignment;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.enums.ActorIdType;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.role.entities.enums.Classification;
@@ -70,6 +71,9 @@ public class WorkTypeProviderTest extends SpringBootContractProviderBaseTest {
         workTypes.add(new WorkType("post_hearing", "Post-Hearing"));
 
         AccessControlResponse accessControlResponse = mock((AccessControlResponse.class));
+        UserInfo userInfo = mock((UserInfo.class));
+        when(userInfo.getUid()).thenReturn("someUserId");
+        when(accessControlResponse.getUserInfo()).thenReturn(userInfo);
         when(accessControlService.getRoles(anyString())).thenReturn(accessControlResponse);
         when(workTypesService.getAllWorkTypes()).thenReturn(workTypes);
     }
