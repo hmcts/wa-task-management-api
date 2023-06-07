285abc3646744661140a92437e45ef15f2fbea90
vasu <vasu.kaveri@hmcts.net>
2023-06-07 14:20:15 +0100

    RWA-2373: disable test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/ExecuteTaskReconfigurationFailureServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/ExecuteTaskReconfigurationFailureServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
