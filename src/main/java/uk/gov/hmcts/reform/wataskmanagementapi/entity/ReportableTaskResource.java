35585d12e869be8f40da2a8f2f584cae04e08bc6
Lee Philip <leephilip365@yahoo.co.uk>
2023-06-12 22:14:56 +0100

    RWA-2381_12 sonar

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/entity/ReportableTaskResource.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/entity/ReportableTaskResource.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @SuppressWarnings({"PMD.TooManyFields", "PMD.LinguisticNaming", "PMD.UnusedPrivateField"})
+@EqualsAndHashCode(callSuper = false)
 @Entity(name = "reportableTask")
 @Table(name = "reportable_task")
 public class ReportableTaskResource extends BaseTaskHistoryResource {
