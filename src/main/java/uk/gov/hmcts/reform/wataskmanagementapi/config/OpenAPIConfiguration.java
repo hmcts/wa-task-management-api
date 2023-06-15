c8787350ae6441497ceca4367fba32b5425f788b
Sammi Chong <shinyee.chong@hmcts.net>
2023-06-15 16:13:36 +0100

    Fix formatting

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/OpenAPIConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/OpenAPIConfiguration.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.ExternalDocumentation;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfiguration {
