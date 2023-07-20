ef8d5aa2f6383b57a5fd7792af377cf6922e5e50
vasu <vasu.kaveri@hmcts.net>
2023-07-20 14:05:47 +0100

    RWA-2657: mi reporting enable tests

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReplicaReportingServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReplicaReportingServiceTest.java
@@ -160,7 +160,7 @@ class MIReplicaReportingServiceTest extends SpringBootIntegrationBaseTest {
                 });
     }
 
-    /*@ParameterizedTest
+    @ParameterizedTest
     @CsvSource(value = {
         "LEGAL_OPERATIONS,Legal Operations",
         "CTSC,CTSC",
@@ -220,7 +220,7 @@ class MIReplicaReportingServiceTest extends SpringBootIntegrationBaseTest {
                     assertEquals(reportableTaskJurisdictionLabel, reportableTaskList.get(0).getJurisdictionLabel());
                     return true;
                 });
-    }*/
+    }
 
     @Test
     void should_save_auto_assigned_task_and_get_task_from_reportable_task() {
