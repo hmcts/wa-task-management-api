19192ed5478f60f620fa6aa19e146c2c6c3ea7ff
vasu <vasu.kaveri@hmcts.net>
2023-08-01 15:09:04 +0100

    RWA-2645: Fixed timestamp specs on new fields

--- src/main/resources/dbreplica/migration/V1.0.10__add_missing_columns_plsql.sql
+++ src/main/resources/dbreplica/migration/V1.0.10__add_missing_columns_plsql.sql
@@ -3,19 +3,19 @@
  ALTER TABLE cft_task_db.task_history ADD COLUMN region_name Text;
  ALTER TABLE cft_task_db.task_history ADD COLUMN location_name Text;
  ALTER TABLE cft_task_db.task_history ADD COLUMN additional_properties JSONB;
- ALTER TABLE cft_task_db.task_history ADD COLUMN reconfigure_request_time Timestamp Without Time Zone;
+ ALTER TABLE cft_task_db.task_history ADD COLUMN reconfigure_request_time Timestamp;
  ALTER TABLE cft_task_db.task_history ADD COLUMN next_hearing_id Text;
- ALTER TABLE cft_task_db.task_history ADD COLUMN next_hearing_date Timestamp Without Time Zone;
- ALTER TABLE cft_task_db.task_history ADD COLUMN priority_date Timestamp Without Time Zone NOT NULL;
- ALTER TABLE cft_task_db.task_history ADD COLUMN last_reconfiguration_time Timestamp Without Time Zone;
+ ALTER TABLE cft_task_db.task_history ADD COLUMN next_hearing_date Timestamp;
+ ALTER TABLE cft_task_db.task_history ADD COLUMN priority_date Timestamp;
+ ALTER TABLE cft_task_db.task_history ADD COLUMN last_reconfiguration_time Timestamp;
 
  ALTER TABLE cft_task_db.reportable_task ADD COLUMN description Text;
  ALTER TABLE cft_task_db.reportable_task ADD COLUMN notes JSONB;
  ALTER TABLE cft_task_db.reportable_task ADD COLUMN region_name Text;
  ALTER TABLE cft_task_db.reportable_task ADD COLUMN location_name Text;
  ALTER TABLE cft_task_db.reportable_task ADD COLUMN additional_properties JSONB;
- ALTER TABLE cft_task_db.reportable_task ADD COLUMN reconfigure_request_time Timestamp Without Time Zone;
+ ALTER TABLE cft_task_db.reportable_task ADD COLUMN reconfigure_request_time Timestamp;
  ALTER TABLE cft_task_db.reportable_task ADD COLUMN next_hearing_id Text;
- ALTER TABLE cft_task_db.reportable_task ADD COLUMN next_hearing_date Timestamp Without Time Zone;
- ALTER TABLE cft_task_db.reportable_task ADD COLUMN priority_date Timestamp Without Time Zone NOT NULL;
- ALTER TABLE cft_task_db.reportable_task ADD COLUMN last_reconfiguration_time Timestamp Without Time Zone;
+ ALTER TABLE cft_task_db.reportable_task ADD COLUMN next_hearing_date Timestamp;
+ ALTER TABLE cft_task_db.reportable_task ADD COLUMN priority_date Timestamp;
+ ALTER TABLE cft_task_db.reportable_task ADD COLUMN last_reconfiguration_time Timestamp;
