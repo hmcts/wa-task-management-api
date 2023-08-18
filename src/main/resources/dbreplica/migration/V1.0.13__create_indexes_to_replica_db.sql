1bf9fc6ec8b7306446920735ca7099856a1cf550
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2023-08-18 11:57:34 +0100

    Renaming indexes for consistency

--- src/main/resources/dbreplica/migration/V1.0.13__create_indexes_to_replica_db.sql
+++ src/main/resources/dbreplica/migration/V1.0.13__create_indexes_to_replica_db.sql
@@ -13,16 +13,16 @@ CREATE INDEX idx_rt_created_date_update_id ON cft_task_db.reportable_task (creat
 CREATE INDEX idx_rt_due_date_update_id ON cft_task_db.reportable_task (due_date, update_id);
 CREATE INDEX idx_rt_state_rep ON cft_task_db.reportable_task (state);
 CREATE INDEX idx_rt_final_state_label ON cft_task_db.reportable_task (final_state_label);
-CREATE INDEX idx_is_within_sla ON cft_task_db.reportable_task (is_within_sla);
-CREATE INDEX idx_state_termination_reason ON cft_task_db.reportable_task (state, termination_reason);
-CREATE INDEX idx_completed_date_time ON cft_task_db.reportable_task (completed_date_time);
-CREATE INDEX idx_completed_date ON cft_task_db.reportable_task (completed_date);
-CREATE INDEX idx_wait_time_days ON cft_task_db.reportable_task (wait_time_days);
-CREATE INDEX idx_wait_time ON cft_task_db.reportable_task (wait_time);
-CREATE INDEX idx_handling_time ON cft_task_db.reportable_task (handling_time);
-CREATE INDEX idx_processing_time ON cft_task_db.reportable_task (processing_time);
-CREATE INDEX idx_first_assigned_date ON cft_task_db.reportable_task (first_assigned_date);
-CREATE INDEX idx_first_assigned_date_time ON cft_task_db.reportable_task (first_assigned_date_time);
+CREATE INDEX idx_rt_is_within_sla ON cft_task_db.reportable_task (is_within_sla);
+CREATE INDEX idx_rt_state_termination_reason ON cft_task_db.reportable_task (state, termination_reason);
+CREATE INDEX idx_rt_completed_date_time ON cft_task_db.reportable_task (completed_date_time);
+CREATE INDEX idx_rt_completed_date ON cft_task_db.reportable_task (completed_date);
+CREATE INDEX idx_rt_wait_time_days ON cft_task_db.reportable_task (wait_time_days);
+CREATE INDEX idx_rt_wait_time ON cft_task_db.reportable_task (wait_time);
+CREATE INDEX idx_rt_handling_time ON cft_task_db.reportable_task (handling_time);
+CREATE INDEX idx_rt_processing_time ON cft_task_db.reportable_task (processing_time);
+CREATE INDEX idx_rt_first_assigned_date ON cft_task_db.reportable_task (first_assigned_date);
+CREATE INDEX idx_rt_first_assigned_date_time ON cft_task_db.reportable_task (first_assigned_date_time);
 
 -- task_assignments indexes
 
