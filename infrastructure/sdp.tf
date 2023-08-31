a89cede02c61825e905e0ccb6f2ac79c27445ef7
Tony Chow <36265570+tchow8@users.noreply.github.com>
2023-08-31 10:08:33 +0100

    Changed source url format for terraform-module-sdp-db-user

--- infrastructure/sdp.tf
+++ infrastructure/sdp.tf
@@ -43,7 +43,7 @@ module "sdp_db_user" {
     azurerm.sdp_vault = azurerm.sdp_vault
   }
 
-  source = "git::https://github.com/hmcts/terraform-module-sdp-db-user.git?ref=master"
+  source = "git@github.com:hmcts/terraform-module-sdp-db-user?ref=master"
   env    = local.sdp_environment
 
   server_name       = "${var.postgres_db_component_name}-postgres-db-flexible-replica"
