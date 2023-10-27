2747ed9c887ddde51c3fcb0378f0e059bbe86b69
vasu <vasu.kaveri@hmcts.net>
2023-10-27 13:19:53 +0100

    RWA-2858: Rebase from master

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReplicaReportingServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/MIReplicaReportingServiceTest.java
@@ -24,6 +24,7 @@ import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
