fa95b264f3e9a3fe994813c011c396872945b729
skandala-cgi <sita.kandala@hmcts.net>
2024-02-12 14:35:18 +0000

    RWA-3103 updated logging

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CaseConfigurationProviderService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CaseConfigurationProviderService.java
@@ -67,7 +67,7 @@ public class CaseConfigurationProviderService {
         String caseDataString = writeValueAsString(caseDetails.getData());
         String taskAttributesString = writeValueAsString(taskAttributes);
         log.debug("Case Configuration : case data {}", caseDataString);
-        log.debug("Case Configuration : task Attributes {}", taskAttributesString);
+        log.info("Case Configuration : task Attributes {}", taskAttributesString);
         // Evaluate Dmns
         List<ConfigurationDmnEvaluationResponse> taskConfigurationDmnResults =
             dmnEvaluationService.evaluateTaskConfigurationDmn(
