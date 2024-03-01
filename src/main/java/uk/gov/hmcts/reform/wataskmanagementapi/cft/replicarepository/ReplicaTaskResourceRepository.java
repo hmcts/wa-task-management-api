412b02b4f55e3ed4d6127f347f93b25caeba7685
vasu <vasu.kaveri@hmcts.net>
2024-03-01 12:25:00 +0000

    RWA-2902: MI created Replica Task Resource and replaced postgresql function with JPA query.

---
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/cft/replicarepository/ReplicaTaskResourceRepository.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.cft.replicarepository;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
+import uk.gov.hmcts.reform.wataskmanagementapi.entity.replica.ReplicaTaskResource;
+
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings({
+    "PMD.UseVarargs", "PMD.TooManyMethods"})
+@Profile("replica | preview")
+public interface ReplicaTaskResourceRepository extends CrudRepository<ReplicaTaskResource, String> {
+
+    Optional<ReplicaTaskResource> getByTaskId(String id);
+
+    List<ReplicaTaskResource> findAllByTaskIdIn(List<String> taskIds, Sort order);
+
+}
---
+++ src/main/resources/dbreplica/migration/V1.0.28__drop_get_report_refresh_request_times.sql
@@ -0,0 +1,1 @@
+DROP FUNCTION cft_task_db.get_report_refresh_request_times();
