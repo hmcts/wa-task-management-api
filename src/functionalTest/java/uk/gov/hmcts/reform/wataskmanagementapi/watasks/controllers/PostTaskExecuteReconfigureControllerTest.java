f0dfe3089a38aed91bd706dc73258ebad91028cb
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2024-04-29 10:52:45 +0100

    RWA-3288 Add a Functional Test to verify PRL scenario works (#1253)
    * RWA-3288 Add a Functional Test to verify PRL scenario works
    
    * RWA-3288 Add a Functional Test to verify PRL scenario works
    
    * RWA-3288 Add a Functional Test to verify PRL scenario works

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/watasks/controllers/PostTaskExecuteReconfigureControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/watasks/controllers/PostTaskExecuteReconfigureControllerTest.java
@@ -944,15 +944,19 @@ public class PostTaskExecuteReconfigureControllerTest extends SpringBootFunction
 
     @Test
     public void should_reconfigure_camunda_task_attributes_after_validation_reconfigure_flag() {
-        TestVariables taskVariables = common.setupWATaskAndRetrieveIds(
-            "reconfigTaskAttributesTask",
+        String roleAssignmentId = UUID.randomUUID().toString();
+        Map<String, String> additionalProperties = Map.of(
+            "roleAssignmentId", roleAssignmentId
+        );
+        TestVariables taskVariables = common.setupWATaskWithAdditionalPropertiesAndRetrieveIds(
+            additionalProperties,
+            "requests/ccd/wa_case_data.json",
             "reconfigTaskAttributesTask"
         );
         String taskId = taskVariables.getTaskId();
-
         common.setupWAOrganisationalRoleAssignment(assignerCredentials.getHeaders(), "case-manager");
 
-        initiateTask(taskVariables, assignerCredentials.getHeaders());
+        initiateTask(taskVariables, assignerCredentials.getHeaders(), additionalProperties);
 
         Response result = restApiActions.get(
             "/task/{task-id}",
@@ -966,7 +970,12 @@ public class PostTaskExecuteReconfigureControllerTest extends SpringBootFunction
         result.then().assertThat()
             .statusCode(HttpStatus.OK.value())
             .and().contentType(MediaType.APPLICATION_JSON_VALUE)
-            .and().body("task.id", equalTo(taskId));
+            .and().body("task.id", equalTo(taskId))
+            .body("task.additional_properties", equalToObject(Map.of(
+                "key1", "value1",
+                "key2", "value1",
+                "roleAssignmentId", roleAssignmentId
+            )));
 
         common.setupWAOrganisationalRoleAssignment(assigneeCredentials.getHeaders(), "judge");
 
@@ -1023,6 +1032,7 @@ public class PostTaskExecuteReconfigureControllerTest extends SpringBootFunction
                 );
 
                 taskResult.prettyPrint();
+                String taskName = taskVariables.getTaskName();
 
                 taskResult.then().assertThat()
                     .statusCode(HttpStatus.OK.value())
@@ -1032,9 +1042,14 @@ public class PostTaskExecuteReconfigureControllerTest extends SpringBootFunction
                     .body("task.reconfigure_request_time", nullValue())
                     .body("task.last_reconfiguration_time", notNullValue())
                     .body("task.task_title",
-                          is("name - reconfigTaskAttributesTask - state - ASSIGNED - category - Protection"))
+                          is("name - " + taskName + " - state - ASSIGNED - category - Protection"))
                     .body("task.due_date", notNullValue())
-                    .body("task.role_category", is("CTSC"));
+                    .body("task.role_category", is("CTSC"))
+                    .body("task.additional_properties", equalToObject(Map.of(
+                        "key1", "value1",
+                        "key2", "reconfigValue2",
+                        "roleAssignmentId", roleAssignmentId
+                    )));
             });
         common.cleanUpTask(taskId);
     }
