86aaf5ac6607cfad6bb61cff3edecc92170268f4
skandala-cgi <sita.kandala@hmcts.net>
2024-07-31 11:53:29 +0100

    RWA-3193 only for testing

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/utils/TaskMandatoryFieldsValidator.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/utils/TaskMandatoryFieldsValidator.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.wataskmanagementapi.config.LaunchDarklyFeatureFlagProvider;
 import uk.gov.hmcts.reform.wataskmanagementapi.config.features.FeatureFlag;
 import uk.gov.hmcts.reform.wataskmanagementapi.entity.TaskResource;
@@ -15,6 +16,7 @@ import java.util.List;
 import javax.validation.ValidationException;
 
 @Slf4j
+@Service
 public class TaskMandatoryFieldsValidator {
 
     @Autowired
