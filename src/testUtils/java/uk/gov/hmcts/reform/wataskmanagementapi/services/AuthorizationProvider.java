a3d4c3c4c1e975e3a0c564f441166a45a60b1a59
skandala-cgi <sita.kandala@hmcts.net>
2024-09-10 09:56:17 +0100

    RWA-3803 - Fix flaky test

--- src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/services/AuthorizationProvider.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/services/AuthorizationProvider.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.services;
 
+import feign.FeignException;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import lombok.extern.slf4j.Slf4j;
@@ -19,8 +20,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.util.Arrays.asList;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static uk.gov.hmcts.reform.wataskmanagementapi.config.SecurityConfiguration.AUTHORIZATION;
 import static uk.gov.hmcts.reform.wataskmanagementapi.config.SecurityConfiguration.SERVICE_AUTHORIZATION;
 
@@ -46,8 +52,13 @@ public class AuthorizationProvider {
     public void deleteAccount(String username) {
 
         if (testUserDeletionEnabled) {
-            log.info("Deleting test account '{}'", username);
-            idamServiceApi.deleteTestUser(username);
+            //If error is thrown while deleting the user, it will be caught and logged
+            try {
+                log.info("Deleting test account '{}'", username);
+                idamServiceApi.deleteTestUser(username);
+            } catch (FeignException e) {
+                log.error("Failed to delete test account '{}'", username, e);
+            }
         } else {
             log.info("Test User deletion feature flag was not enabled, user '{}' was not deleted", username);
         }
@@ -273,24 +284,35 @@ public class AuthorizationProvider {
     }
 
     private TestAccount generateIdamTestAccount(String emailPrefix, List<RoleCode> requiredRoles) {
-        String email = emailPrefix + UUID.randomUUID() + "@fake.hmcts.net";
         String password = "London01";
-
-        log.info("Attempting to create a new test account {}", email);
-
         RoleCode userGroup = new RoleCode("caseworker");
 
         Map<String, Object> body = new ConcurrentHashMap<>();
-        body.put("email", email);
         body.put("password", password);
         body.put("forename", "WAFTAccount");
         body.put("surname", "Functional");
         body.put("roles", requiredRoles);
         body.put("userGroup", userGroup);
-
-        idamServiceApi.createTestUser(body);
+        AtomicBoolean accountCreated = new AtomicBoolean(false);
+        AtomicReference<String> email = new AtomicReference<>("");
+        await().ignoreException(Exception.class)
+            .pollInterval(500, MILLISECONDS)
+            .atMost(30, SECONDS)
+            .until(() -> {
+                try {
+                    email.set(emailPrefix + UUID.randomUUID() + "@fake.hmcts.net");
+                    log.info("Attempting to create a new test account {}", email);
+                    body.put("email", email);
+                    idamServiceApi.createTestUser(body);
+                    accountCreated.set(true);
+                } catch (FeignException e) {
+                    log.error("Failed to create test account, retrying...", e);
+                    accountCreated.set(false);
+                }
+                return accountCreated.get();
+            });
 
         log.info("Test account created successfully");
-        return new TestAccount(email, password);
+        return new TestAccount(email.get(), password);
     }
 }
