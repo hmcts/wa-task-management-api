14897ad44f0d9005bc612d6bf05b87db5479e46e
Sonny Lloyd <sonny.lloyd@hmcts.net>
2024-10-11 16:27:45 +0100

    check style fixes

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/utils/ResponseEntityBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/utils/ResponseEntityBuilder.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.services.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Slf4j
 public final class ResponseEntityBuilder {
@@ -17,12 +14,12 @@ public final class ResponseEntityBuilder {
                                                                            final Exception exception) {
 
         log.error("deleteTasks API call failed due to error - {}",
-                exception.getMessage(),
-                exception
+            exception.getMessage(),
+            exception
         );
 
         return ResponseEntity
-                .status(httpStatus)
-                .cacheControl(CacheControl.noCache()).build();
+            .status(httpStatus)
+            .cacheControl(CacheControl.noCache()).build();
     }
 }
