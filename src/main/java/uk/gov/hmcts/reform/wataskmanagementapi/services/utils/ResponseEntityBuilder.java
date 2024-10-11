61606c261348f45655ce04d62dcd862c8bce41ad
Sonny Lloyd <sonny.lloyd@hmcts.net>
2024-10-11 16:03:57 +0100

    use lombok for getting the logger

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/utils/ResponseEntityBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/utils/ResponseEntityBuilder.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.services.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+@Slf4j
 public final class ResponseEntityBuilder {
-
-    private static final Logger LOG = getLogger(ResponseEntityBuilder.class);
 
     private ResponseEntityBuilder() {
     }
@@ -16,7 +16,7 @@ public final class ResponseEntityBuilder {
     public static ResponseEntity<Void> buildErrorResponseEntityAndLogError(final int httpStatus,
                                                                            final Exception exception) {
 
-        LOG.error("deleteTasks API call failed due to error - {}",
+        log.error("deleteTasks API call failed due to error - {}",
                 exception.getMessage(),
                 exception
         );
