628771eff948e2ec656e15f21dc7f8fc2f08b886
skandala-cgi <sita.kandala@hmcts.net>
2025-01-10 17:16:17 +0000

    RWA-3970 validation

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CaseConfigurationProviderService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/CaseConfigurationProviderService.java
@@ -15,8 +15,8 @@ import uk.gov.hmcts.reform.wataskmanagementapi.domain.configuration.TaskConfigur
 import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.BadRequestException;
 import uk.gov.hmcts.reform.wataskmanagementapi.services.calendar.DateTypeConfigurator;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
