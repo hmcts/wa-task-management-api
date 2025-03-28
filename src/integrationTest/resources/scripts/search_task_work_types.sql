6a8dd7c7857580bad155f15f13dd4109c52806e7
sonny-hmcts <sonny.lloyd@hmcts.net>
2025-03-28 14:02:33 +0000

    added welsh translations to new work type test

--- src/integrationTest/resources/scripts/search_task_work_types.sql
+++ src/integrationTest/resources/scripts/search_task_work_types.sql
@@ -96,7 +96,12 @@ false, 'WA', '765324', 'Taylor House', 0, 0, '[{"user": "userVal", "noteType": "
 ('8d6cc5cf-c973-11eb-bdba-0242ac111029', 'SELF', '2025-05-09T20:15:45.345875+01:00', false, 'CFT_TASK',
 '1623278362431016', 'TestCase', 'WaCaseType', '2025-05-09T20:15:45.345875+01:00', 'description', '2025-05-09T20:15:45.345875+01:00',
 false, 'WA', '765324', 'Taylor House', 0, 0, '[{"user": "userVal", "noteType": "noteTypeVal"}]', '1', 'TestRegion', 'ADMIN',
-'PUBLIC', 'ASSIGNED', 'taskName', 'SELF', 'processApplication', null, 'title', 'routine_work', 'MANUAL', '2025-05-10T20:15:45.345875+01:00', '2025-05-09T20:15:45.345875+01:00');
+'PUBLIC', 'ASSIGNED', 'taskName', 'SELF', 'processApplication', null, 'title', 'routine_work', 'MANUAL', '2025-05-10T20:15:45.345875+01:00', '2025-05-09T20:15:45.345875+01:00'),
+
+('8d6cc5cf-c973-11eb-bdba-0242ac111030', 'SELF', '2025-05-09T20:15:45.345875+01:00', false, 'CFT_TASK',
+'1623278362431016', 'TestCase', 'WaCaseType', '2025-05-09T20:15:45.345875+01:00', 'description', '2025-05-09T20:15:45.345875+01:00',
+false, 'WA', '765324', 'Taylor House', 0, 0, '[{"user": "userVal", "noteType": "noteTypeVal"}]', '1', 'TestRegion', 'ADMIN',
+'PUBLIC', 'ASSIGNED', 'taskName', 'SELF', 'processApplication', null, 'title', 'welsh_translation_work', 'MANUAL', '2025-05-10T20:15:45.345875+01:00', '2025-05-09T20:15:45.345875+01:00');
 
 INSERT INTO cft_task_db.task_roles (task_role_id, role_name,
 "read", own, "execute", manage, cancel,
@@ -120,4 +125,5 @@ VALUES
 ('8d6cc5cf-c973-11eb-bdba-0242ac115026', 'tribunal-caseworker', true, false , false , false , false, null, 0, false, 'ADMIN', '8d6cc5cf-c973-11eb-bdba-0242ac111026', '2025-05-09T20:15:45.345875+01:00'),
 ('8d6cc5cf-c973-11eb-bdba-0242ac115027', 'tribunal-caseworker', true, false , false , false , false, null, 0, false, 'ADMIN', '8d6cc5cf-c973-11eb-bdba-0242ac111027', '2025-05-09T20:15:45.345875+01:00'),
 ('8d6cc5cf-c973-11eb-bdba-0242ac115028', 'tribunal-caseworker', true, false , false , false , false, null, 0, false, 'ADMIN', '8d6cc5cf-c973-11eb-bdba-0242ac111028', '2025-05-09T20:15:45.345875+01:00'),
-('8d6cc5cf-c973-11eb-bdba-0242ac115029', 'tribunal-caseworker', true, false , false , false , false, null, 0, false, 'ADMIN', '8d6cc5cf-c973-11eb-bdba-0242ac111029', '2025-05-09T20:15:45.345875+01:00');
+('8d6cc5cf-c973-11eb-bdba-0242ac115029', 'tribunal-caseworker', true, false , false , false , false, null, 0, false, 'ADMIN', '8d6cc5cf-c973-11eb-bdba-0242ac111029', '2025-05-09T20:15:45.345875+01:00'),
+('8d6cc5cf-c973-11eb-bdba-0242ac115030', 'tribunal-caseworker', true, false , false , false , false, null, 0, false, 'ADMIN', '8d6cc5cf-c973-11eb-bdba-0242ac111030', '2025-05-09T20:15:45.345875+01:00');
