0fe6afbdc850625cc527c5898653ad41a9154a87
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2025-04-08 16:45:41 +0100

    remove shadowing

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskManagementServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskManagementServiceTest.java
@@ -583,23 +583,25 @@ class TaskManagementServiceTest extends SpringBootIntegrationBaseTest {
         @Test
         @DisplayName("should_log_error_and_throw_exception_when_task_save_fails_after_camunda_update")
         void should_log_error_and_throw_exception_when_task_save_fails_after_camunda_update(CapturedOutput output) {
-            String taskId = UUID.randomUUID().toString();
-            createAndAssignTestTask(taskId);
-            Optional<TaskResource> savedTaskResource = taskResourceRepository.findById(taskId);
+            String randomTaskId = UUID.randomUUID().toString();
+            createAndAssignTestTask(randomTaskId);
+            Optional<TaskResource> savedTaskResource = taskResourceRepository.findById(randomTaskId);
             TaskResource taskResource = savedTaskResource.orElse(null);
             assertNotNull(taskResource);
             doThrow(new RuntimeException("Database error")).when(cftTaskDatabaseService).saveTask(taskResource);
             TerminateInfo terminateInfo = new TerminateInfo("deleted");
             assertThatThrownBy(() -> taskManagementService.terminateTask(
-                    taskId,
+                    randomTaskId,
                     terminateInfo
                 ))
                 .isInstanceOf(RuntimeException.class);
 
-            verify(camundaService, times(1)).deleteCftTaskState(taskId);
+            verify(camundaService, times(1)).deleteCftTaskState(randomTaskId);
             verify(cftTaskDatabaseService).saveTask(taskResource);
-            assertTrue(output.getOut().contains("Error saving task with id " + taskId
-                                                    + " after successfully deleting Camunda task state:"));
+            assertTrue(output.getOut().contains("Error saving task with id " + randomTaskId
+                                                + " after successfully deleting Camunda task state:"),
+                       "Received log message:" + output.getOut());
+
         }
 
         @Test
