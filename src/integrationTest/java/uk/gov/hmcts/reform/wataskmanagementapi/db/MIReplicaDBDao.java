aec1b79309846a6667807db25901504839734681
Aditya Dwadasi <aditya.dwadasi@hmcts.net>
2025-06-12 15:55:12 +0100

    Refactored the code.

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/db/MIReplicaDBDao.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/db/MIReplicaDBDao.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.db;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.ReplicationException;
 
 import java.sql.Connection;
@@ -11,6 +13,8 @@ import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Objects;
+import javax.sql.DataSource;
+
 
 @Slf4j
 public class MIReplicaDBDao {
@@ -18,11 +22,15 @@ public class MIReplicaDBDao {
     private final String jdbcUrl;
     private final String userName;
     private final String password;
+    private final JdbcTemplate jdbcTemplate;
 
     public MIReplicaDBDao(String jdbcUrl, String userName, String password) {
         this.jdbcUrl = jdbcUrl;
         this.userName = userName;
         this.password = password;
+
+        DataSource dataSource = new DriverManagerDataSource(jdbcUrl, userName, password);
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
@@ -91,26 +99,23 @@ public class MIReplicaDBDao {
 
     public void callReplicaCleanupFunction(OffsetDateTime createdTimeFrom, OffsetDateTime createdTimeTo) {
 
-        log.info("From time: " + Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
-        log.info("To time: " + Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
-        String runFunction = " select cft_task_db.task_cleanup_between_dates_replica( ?,? ) ";
+        log.info("From time: {}", Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
+        log.info("To time: {}", Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
 
-        try (Connection conn = DriverManager.getConnection(jdbcUrl, userName, password);
-             PreparedStatement preparedStatement = conn.prepareStatement(runFunction)) {
+        String runFunction = "select cft_task_db.task_cleanup_between_dates_replica(?, ?)";
 
-            preparedStatement.setTimestamp(1, Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
-            preparedStatement.setTimestamp(2, Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
+        try {
 
-            preparedStatement.execute();
+            jdbcTemplate.execute(runFunction, (PreparedStatement ps) -> {
+                ps.setTimestamp(1, Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
+                ps.setTimestamp(2, Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
+                ps.execute();
+                return null;
+            });
 
-        } catch (SQLException e) {
-            log.error("Procedure call callMarkReportTasksForRefresh failed with SQL State : {}, {} ",
-                      e.getSQLState(), e.getMessage());
-            throw new ReplicationException("An error occurred while executing mark_report_tasks_for_refresh", e);
         } catch (Exception e) {
-            log.error("Procedure call callMarkReportTasksForRefresh failed with SQL State : {}, {} ",
-                      e.getCause(), e.getMessage());
-            throw new ReplicationException("An error occurred while executing mark_report_tasks_for_refresh", e);
+            log.error("Procedure call callReplicaCleanupFunction failed: {}", e.getMessage(), e);
+            throw new ReplicationException("An error occurred while executing task_cleanup_between_dates_replica", e);
         }
     }
 
--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/db/PrimaryDBDao.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/db/PrimaryDBDao.java
@@ -1,50 +1,43 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.db;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.ReplicationException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.OffsetDateTime;
+import javax.sql.DataSource;
 
 @Slf4j
 public class PrimaryDBDao {
 
-    private final String jdbcUrl;
-    private final String userName;
-    private final String password;
+    private final JdbcTemplate jdbcTemplate;
 
     public PrimaryDBDao(String jdbcUrl, String userName, String password) {
-        this.jdbcUrl = jdbcUrl;
-        this.userName = userName;
-        this.password = password;
+        DataSource dataSource = new DriverManagerDataSource(jdbcUrl, userName, password);
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void callPrimaryCleanupFunction(OffsetDateTime createdTimeFrom, OffsetDateTime createdTimeTo) {
+        log.info("From time: {}", createdTimeFrom);
+        log.info("To time: {}", createdTimeTo);
 
-        log.info("From time: " + Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
-        log.info("To time: " + Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
-        String runFunction = " select cft_task_db.task_cleanup_between_dates_primary( ?,? ) ";
+        String runFunction = "select cft_task_db.task_cleanup_between_dates_primary(?, ?)";
 
-        try (Connection conn = DriverManager.getConnection(jdbcUrl, userName, password);
-             PreparedStatement preparedStatement = conn.prepareStatement(runFunction)) {
+        try {
 
-            preparedStatement.setTimestamp(1, Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
-            preparedStatement.setTimestamp(2, Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
+            jdbcTemplate.execute(runFunction, (PreparedStatement ps) -> {
+                ps.setTimestamp(1, Timestamp.valueOf(createdTimeFrom.toLocalDateTime()));
+                ps.setTimestamp(2, Timestamp.valueOf(createdTimeTo.toLocalDateTime()));
+                ps.execute();
+                return null;
+            });
 
-            preparedStatement.execute();
-
-        } catch (SQLException e) {
-            log.error("Procedure call callMarkReportTasksForRefresh failed with SQL State : {}, {} ",
-                      e.getSQLState(), e.getMessage());
-            throw new ReplicationException("An error occurred while executing mark_report_tasks_for_refresh", e);
         } catch (Exception e) {
-            log.error("Procedure call callMarkReportTasksForRefresh failed with SQL State : {}, {} ",
-                      e.getCause(), e.getMessage());
-            throw new ReplicationException("An error occurred while executing mark_report_tasks_for_refresh", e);
+            log.error("Procedure call failed: {}", e.getMessage(), e);
+            throw new ReplicationException("An error occurred while executing task_cleanup_between_dates_primary", e);
         }
     }
 
