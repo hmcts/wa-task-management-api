5aedf628dcc4c70183e5be9d4fef71a5da6eb1ff
skandala-cgi <sita.kandala@hmcts.net>
2025-07-29 10:21:37 +0100

    RWA-3581 testing

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/FeatureToggleConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/config/FeatureToggleConfiguration.java
@@ -5,6 +5,7 @@ import com.launchdarkly.sdk.server.Components;
 import com.launchdarkly.sdk.server.LDClient;
 import com.launchdarkly.sdk.server.LDConfig;
 import com.launchdarkly.sdk.server.interfaces.LDClientInterface;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import java.time.Duration;
 
 @Configuration
+@Slf4j
 public class FeatureToggleConfiguration {
 
     @Value("${launchDarkly.sdkKey}")
@@ -25,6 +27,7 @@ public class FeatureToggleConfiguration {
 
     @Bean
     public LDConfig ldConfig() {
+        log.info("SDK key {}, connection timeout {}", sdkKey, connectionTimeout);
         return new LDConfig.Builder()
             .http(Components
                 .httpConfiguration()
@@ -36,6 +39,7 @@ public class FeatureToggleConfiguration {
 
     @Bean
     public LDClientInterface ldClient(LDConfig ldConfig) {
+        log.info("SDK key", sdkKey);
         return new LDClient(sdkKey, ldConfig);
     }
 
