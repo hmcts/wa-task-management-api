6ca1332f479b8574413e066fb37df41c59baf2a4
Martin <martin.spasov@hmcts.net>
2025-09-19 13:25:48 +0100

    Enable QPA on AAT

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -114,6 +114,7 @@ module "wa_task_management_api_database_flexible_replica" {
   ]
 
   pgsql_version = 14
+  enable_qpi = var.is_qpa_enabled
   common_tags   = local.common_tags
 
   admin_user_object_id = var.jenkins_AAD_objectId
