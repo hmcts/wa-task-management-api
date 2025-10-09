20069ec5c2e56dd3a3cb590a071824c29484d451
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2025-10-09 11:55:34 +0100

    Update rwa-4667_update_completion_process_rollback.sql

--- src/main/resources/scripts/rwa-4667_update_completion_process_rollback.sql
+++ src/main/resources/scripts/rwa-4667_update_completion_process_rollback.sql
@@ -1,5 +1,5 @@
 --check count of rows in backup table
-select count(*) from cft_task_db.sscs_completed_tasks_ids_backup and jurisdiction = 'SSCS' and state in ('COMPLETED','TERMINATED') and termination_reason = 'completed';
+select count(*) from cft_task_db.sscs_completed_tasks_ids_backup where jurisdiction = 'SSCS' and state in ('COMPLETED','TERMINATED') and termination_reason = 'completed';
 -- 8
 
 update cft_task_db.tasks set termination_reason = 'completed' where task_id in (select task_id from cft_task_db.cft_task_db.sscs_completed_tasks_ids_backup);
