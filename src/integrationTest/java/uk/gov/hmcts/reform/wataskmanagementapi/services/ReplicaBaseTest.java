103d6b154e5a7a9116fc560fbb5bc2e90c254e77
Aditya Dwadasi <aditya.dwadasi@hmcts.net>
2025-11-10 17:10:54 +0000

    RWA-4751: Changed the Annotation from BeforeEach to BeforeAll

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/ReplicaBaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/services/ReplicaBaseTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.wataskmanagementapi.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.opentest4j.AssertionFailedError;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,7 +61,7 @@ public abstract class ReplicaBaseTest extends SpringBootIntegrationBaseTest {
     protected JdbcDatabaseContainer container;
     protected JdbcDatabaseContainer containerReplica;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
         //Logical Replication is a pre-requisite for all tests here
         waitForReplication();
