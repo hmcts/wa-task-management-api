38a672399ecc1ff0205fc7e4b85363636b11e4f9
lars <lars.lunde@hmcts.net>
2025-11-26 14:46:14 +0000

    deleting iac worktypes

---
+++ src/main/resources/db/migration/V1.0.40__remove_iac_work_types.sql
@@ -0,0 +1,9 @@
+DELETE FROM work_types WHERE work_type_id IN (
+  'stf_24w_hearing_work',
+  'stf_24w_routine_work',
+  'stf_24w_decision_making_work',
+  'stf_24w_applications',
+  'stf_24w_upper_tribunal',
+  'stf_24w_access_requests',
+  'stf_24w_review_case'
+  );
