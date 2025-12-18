bab608b93268a3f6743156f584ccc5606f617569
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2025-12-18 16:26:04 +0000

    Rwa 4626 (#1524)
    * RWA-4627 Add LaunchDarkly Flag and functional tests
    
    * RWA-4622 initial PR
    
    * RWA-4622 initial PR
    
    * RWA-4622 initial PR
    
    * RWA-4622 initial PR
    
    * RWA-4622 initial PR
    
    * RWA-4622 review comments
    
    * RWA-4622 review comments
    
    * RWA-4622 review comments
    
    * RWA-4622 review comments
    
    * RWA-4622 review comments
    
    * RWA-4622 review comments
    
    * RWA-4622, RWA-4623 initial changes
    
    * RWA-4623 changes after code refactoring
    
    * RWA-4623 changes after code refactoring
    
    * RWA-4623 changes after code refactoring
    
    * RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-46232 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4622 and RWA-4623 changes after code refactoring
    
    * RWA-4624 initial changes
    
    * RWA-4624 initial changes
    
    * RWA-4624 initial changes
    
    * RWA-4624 initial changes
    
    * RWA-4623 resolve conflicts after merging changes from master
    
    * RWA-4623 resolve conflicts after merging changes from master
    
    * RWA-4623 sonarqube issues
    
    * RWA-4737 initial changes
    
    * RWA-4624 review comments
    
    * RWA-4737 initial changes
    
    * RWA-4737 initial changes
    
    * RWA-4624 changes after refactoring
    
    * RWA-4624 changes after refactoring
    
    * RWA-4624 changes after refactoring
    
    * RWA-4624 changes after refactoring
    
    * RWA-4624 changes after refactoring
    
    * RWA-4624 changes after refactoring
    
    * RWA-4624 changes after refactoring
    
    * RWA-4626 resolve merge conflicts

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerCancelTaskProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/TaskManagerCancelTaskProviderTest.java
@@ -11,10 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import uk.gov.hmcts.reform.wataskmanagementapi.SpringBootContractProviderBaseTest;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.UserInfo;
+import uk.gov.hmcts.reform.wataskmanagementapi.config.features.FeatureFlag;
 import uk.gov.hmcts.reform.wataskmanagementapi.controllers.TaskActionsController;
+
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -53,6 +57,11 @@ public class TaskManagerCancelTaskProviderTest extends SpringBootContractProvide
         setInitMock();
     }
 
+    @State({"cancel a task using taskId and with cancellation process"})
+    public void cancelTaskByIdWithCancellationProcess() {
+        setInitMockWithoutPrivilegedAccessWithCancellationProcess();
+    }
+
     private void setInitMock() {
         doNothing().when(taskManagementService).cancelTask(any(), any(), any());
         AccessControlResponse accessControlResponse = mock((AccessControlResponse.class));
@@ -60,5 +69,25 @@ public class TaskManagerCancelTaskProviderTest extends SpringBootContractProvide
         when(userInfo.getUid()).thenReturn("someUserId");
         when(accessControlResponse.getUserInfo()).thenReturn(userInfo);
         when(accessControlService.getRoles(anyString())).thenReturn(accessControlResponse);
+    }
+
+    private void setInitMockWithoutPrivilegedAccessWithCancellationProcess() {
+        doNothing().when(taskManagementService).cancelTask(any(), any(), any());
+        AccessControlResponse accessControlResponse = mock((AccessControlResponse.class));
+        UserInfo userInfo = mock((UserInfo.class));
+        when(userInfo.getUid()).thenReturn("someUserId");
+        when(accessControlResponse.getUserInfo()).thenReturn(userInfo);
+        when(accessControlService.getRoles(anyString())).thenReturn(accessControlResponse);
+        when(clientAccessControlService.hasPrivilegedAccess(any(), any())).thenReturn(false);
+        when(launchDarklyFeatureFlagProvider.getBooleanValue(eq(FeatureFlag.WA_CANCELLATION_PROCESS_FEATURE),
+                                                             anyString(), anyString())).thenReturn(true);
+        when(cancellationProcessValidator.validate(anyString(), anyString(), any())).thenAnswer(invocation -> {
+            if (Math.random() < 0.5) {
+                return Optional.of("EXUI_USER_CANCELLATION!");
+            } else {
+                return Optional.empty();
+            }
+        });
+
     }
 }
