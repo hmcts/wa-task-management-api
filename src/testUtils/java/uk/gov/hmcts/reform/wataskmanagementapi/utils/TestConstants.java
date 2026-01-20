2470062266d35b557794b482ea60be6f7f82476c
jasonpittock <jason.pittock1@hmcts.net>
2026-01-20 10:47:47 +0000

    RWA - 4827 Add missing constants file

---
+++ src/testUtils/java/uk/gov/hmcts/reform/wataskmanagementapi/utils/TestConstants.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.utils;
+
+public final class TestConstants {
+    public static final int MAX_ROLE_ASSIGNMENT_RECORDS = 50;
+
+    private TestConstants() {
+    }
+}
