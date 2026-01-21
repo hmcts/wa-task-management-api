012cbae238ed7343fd487c82b7080b9dba6bd685
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2026-01-21 10:00:29 +0000

    TMDPC-2 and TMDPC-3 initial changes (#1550)
    * TMDPC-2 and TMDPC-3 initial changes
    
    * TMDPC-2 and TMDPC-3 initial changes
    
    * TMDPC-2 and TMDPC-3 review comments update
    
    * TMDPC-2 and TMDPC-3 commit with callback error
    
    * TMDPC-2 and TMDPC-3 commit with callback error
    
    * require only s2s token for new task creation endpoint
    
    ---------
    
    Co-authored-by: Alex McAusland <a.m@user.com>

---
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/poc/controller/TaskCreateController.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.wataskmanagementapi.poc.controller;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import uk.gov.hmcts.reform.wataskmanagementapi.entity.TaskResource;
+import uk.gov.hmcts.reform.wataskmanagementapi.poc.api.TasksApi;
+import uk.gov.hmcts.reform.wataskmanagementapi.poc.request.CreateTaskRequest;
+import uk.gov.hmcts.reform.wataskmanagementapi.services.TaskManagementService;
+import uk.gov.hmcts.reform.wataskmanagementapi.auth.restrict.ClientAccessControlService;
+import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.v2.GenericForbiddenException;
+import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+import static uk.gov.hmcts.reform.wataskmanagementapi.exceptions.v2.enums.ErrorMessages.GENERIC_FORBIDDEN_ERROR;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class TaskCreateController implements TasksApi {
+
+    private final TaskManagementService taskManagementService;
+    private final ClientAccessControlService clientAccessControlService;
+
+    @Override
+    @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<uk.gov.hmcts.reform.wataskmanagementapi.entity.TaskResource> createTask(
+        @NotNull @RequestHeader(value = "ServiceAuthorization") String serviceAuthorization,
+        @Valid @RequestBody CreateTaskRequest createTaskRequest
+    ) {
+        boolean hasAccess = clientAccessControlService.hasExclusiveAccess(serviceAuthorization);
+        if (!hasAccess) {
+            throw new GenericForbiddenException(GENERIC_FORBIDDEN_ERROR);
+        }
+
+        TaskResource savedTask = taskManagementService.addTask(createTaskRequest.getTask());
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .cacheControl(CacheControl.noCache())
+            .body(savedTask);
+    }
+}
