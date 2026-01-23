950b8b45b865180e4d3d5c9d3602426c0762da20
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2026-01-23 11:20:41 +0000

    Tmdpc 9 (#1553)
    * TMDPC-2 and TMDPC-3 initial changes
    
    * TMDPC-2 and TMDPC-3 initial changes
    
    * TMDPC-2 and TMDPC-3 review comments update
    
    * TMDPC-2 and TMDPC-3 commit with callback error
    
    * TMDPC-2 and TMDPC-3 commit with callback error
    
    * require only s2s token for new task creation endpoint
    
    * TMDPC-6,7,8 initial commit
    
    * TMDPC-6,7,8 initial commit
    
    * TMDPC-6,7,8 initial commit
    
    * TMDPC-6,7,8 initial commit
    
    * TMDPC-6,7,8 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 initial commit
    
    * TMDPC-9 checkstyle fixes
    
    * TMDPC-9 checkstyle fixes
    
    * TMDPC-9 build issues fixes
    
    * TMDPC-9 build issues fixes
    
    ---------
    
    Co-authored-by: Alex McAusland <a.m@user.com>

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskManagementService.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/services/TaskManagementService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.zalando.problem.violations.Violation;
-
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.entities.AccessControlResponse;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.IdamTokenGenerator;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.idam.entities.UserInfo;
