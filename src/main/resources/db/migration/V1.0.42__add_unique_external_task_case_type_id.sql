18a2eb3935274f59208959c0b0b6abd92d5a8185
Chris Bellingham <chris.bellingham1@hmcts.net>
2026-01-26 13:29:01 +0000

    TMDPC-5: Update constraint to support backward compatibility

--- src/main/resources/db/migration/V1.0.42__add_unique_external_task_case_type_id.sql
+++ src/main/resources/db/migration/V1.0.42__add_unique_external_task_case_type_id.sql
@@ -1,3 +1,3 @@
-ALTER TABLE cft_task_db.tasks
-ADD CONSTRAINT uq_tasks_external_task_id_case_type_id
-UNIQUE (external_task_id, case_type_id);
+CREATE UNIQUE INDEX uq_tasks_external_task_id_case_type_id
+ON cft_task_db.tasks (external_task_id, case_type_id)
+WHERE external_task_id IS NOT NULL AND case_type_id IS NOT NULL;
