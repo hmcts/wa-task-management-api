35f0210654ab763b3244ab7fa0c231d6ae295e22
skandala-cgi <66466864+skandala-cgi@users.noreply.github.com>
2026-02-03 17:08:45 +0000

    Task Termination TMDPC-13 (#1563)
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 build issues fixes
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit
    
    * TMDPC-13 initial commit

--- src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/poc/controller/TaskPocController.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmanagementapi/poc/controller/TaskPocController.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.wataskmanagementapi.poc.controller;
 
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +16,7 @@ import uk.gov.hmcts.reform.wataskmanagementapi.entity.TaskResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.exceptions.v2.GenericForbiddenException;
 import uk.gov.hmcts.reform.wataskmanagementapi.poc.api.TasksApi;
 import uk.gov.hmcts.reform.wataskmanagementapi.poc.request.CreateTaskRequest;
+import uk.gov.hmcts.reform.wataskmanagementapi.poc.request.TerminateTasksRequest;
 import uk.gov.hmcts.reform.wataskmanagementapi.services.TaskManagementService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,7 +29,7 @@ import static uk.gov.hmcts.reform.wataskmanagementapi.exceptions.v2.enums.ErrorM
 @Slf4j
 @RestController
 
-public class TaskCreateController implements TasksApi {
+public class TaskPocController implements TasksApi {
 
     private final TaskManagementService taskManagementService;
     private final ClientAccessControlService clientAccessControlService;
@@ -46,10 +47,32 @@ public class TaskCreateController implements TasksApi {
         }
         TaskResource savedTask = taskManagementService.addTask(createTaskRequest.getTask());
         taskManagementService.updateTaskIndex(savedTask.getTaskId());
-
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .cacheControl(CacheControl.noCache())
             .body(savedTask);
     }
+
+    @PostMapping(value = "/tasks/terminate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> terminateTasks(
+        @NotNull @RequestHeader(value = "ServiceAuthorization", required = true) String serviceAuthorization,
+        @Parameter(name = "TerminateTasksRequest", description = "", required = true) @Valid @RequestBody TerminateTasksRequest terminateTasksRequest
+    ) {
+        boolean hasAccess = clientAccessControlService.hasExclusiveAccess(serviceAuthorization);
+        if (!hasAccess) {
+            throw new GenericForbiddenException(GENERIC_FORBIDDEN_ERROR);
+        }
+
+        taskManagementService.terminateTasks(terminateTasksRequest);
+        return ResponseEntity
+            .noContent()
+            .cacheControl(CacheControl.noCache())
+            .build();
+    }
+
+
+
+
+
+
 }
