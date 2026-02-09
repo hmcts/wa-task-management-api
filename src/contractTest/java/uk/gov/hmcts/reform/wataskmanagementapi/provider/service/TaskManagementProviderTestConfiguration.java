1bdf7b7c3d3a45b149da9fffe3bc6730051e4e86
jasonpittock <jason.pittock1@hmcts.net>
2026-02-09 11:18:23 +0000

    RWA-4723 Refactor integration tests to stub IDAM
    Remove unnecessary Bean in Test Config
    
    https://tools.hmcts.net/jira/browse/RWA-4723

--- src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/service/TaskManagementProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskmanagementapi/provider/service/TaskManagementProviderTestConfiguration.java
@@ -15,11 +15,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.zalando.problem.jackson.ProblemModule;
 import uk.gov.hmcts.reform.wataskmanagementapi.auth.access.AccessControlService;
@@ -90,17 +87,6 @@ public class TaskManagementProviderTestConfiguration {
     @MockitoBean
     TaskMandatoryFieldsValidator taskMandatoryFieldsValidator;
     private RoleAssignmentVerificationService roleAssignmentVerificationService;
-
-    @Bean
-    @Primary
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-            .oauth2Client(Customizer.withDefaults());
-        return http.build();
-    }
 
     @Bean
     @Primary
