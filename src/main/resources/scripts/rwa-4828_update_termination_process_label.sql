b87916fe555c0602928989366f825325b14b423b
skandala-cgi <sita.kandala@hmcts.net>
2026-04-30 09:55:24 +0100

    RGA-4828 initial PR

--- src/main/resources/scripts/rwa-4828_update_termination_process_label.sql
+++ src/main/resources/scripts/rwa-4828_update_termination_process_label.sql
@@ -10,7 +10,7 @@ select count(*) from cft_task_db.cft_task_db.reportable_task where termination_p
 
 
 -- Get count of tasks where termination_process and termination_process_label updated for User cancelled tasks
-select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_label ='USER_CANCELLATION' and termination_process is null  and outcome = 'Cancelled';
+select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_label ='USER_CANCELLED' and termination_process is null  and outcome = 'Cancelled';
 
 -- Get count of tasks where termination_process and termination_process_label updated for System cancelled tasks
 select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_label is null and termination_process is null and agent_name = 'aeabbbcb-4909-4346-acd5-4e95df4357a3' and outcome = 'Cancelled';
@@ -18,16 +18,16 @@ select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_l
 
 --Create a backup tables with task id for all the tasks that need to be updated
 create table IF NOT EXISTS cft_task_db.cft_task_db.automated_completed_tasks_ids_backup AS
-select task_id from cft_task_db.cft_task_db.reportable_tasks where termination_process_label = 'Automated' and termination_process = 'EXUI_CASE_EVENT_COMPLETION';
+select task_id from cft_task_db.cft_task_db.reportable_task where termination_process_label = 'Automated' and termination_process = 'EXUI_CASE_EVENT_COMPLETION';
 
 create table IF NOT EXISTS cft_task_db.cft_task_db.manual_completed_tasks_ids_backup AS
-select task_id from cft_task_db.cft_task_db.reportable_tasks where termination_process_label = 'Manual' and termination_process = 'EXUI_USER_COMPLETION';
+select task_id from cft_task_db.cft_task_db.reportable_task where termination_process_label = 'Manual' and termination_process = 'EXUI_USER_COMPLETION';
 
 create table IF NOT EXISTS cft_task_db.cft_task_db.automated_cancelled_tasks_ids_backup AS
-select task_id from cft_task_db.cft_task_db.reportable_tasks where final_state_label is null and termination_process is null and agent_name = 'aeabbbcb-4909-4346-acd5-4e95df4357a3' and outcome = 'Cancelled';
+select task_id from cft_task_db.cft_task_db.reportable_task where final_state_label is null and termination_process is null and agent_name = 'aeabbbcb-4909-4346-acd5-4e95df4357a3' and outcome = 'Cancelled';
 
 create table IF NOT EXISTS cft_task_db.cft_task_db.manual_cancelled_tasks_ids_backup AS
-select task_id from cft_task_db.cft_task_db.reportable_tasks where final_state_label ='USER_CANCELLATION' and termination_process is null  and outcome = 'Cancelled';
+select task_id from cft_task_db.cft_task_db.reportable_task where final_state_label ='USER_CANCELLED' and termination_process is null  and outcome = 'Cancelled';
 
 -- Check count of records in backup tables
 select count(*) from cft_task_db.cft_task_db.automated_completed_tasks_ids_backup;
@@ -42,13 +42,13 @@ update cft_task_db.cft_task_db.reportable_task set termination_process_label = '
 
 update cft_task_db.cft_task_db.reportable_task set termination_process_label = 'Manual Completion' where  termination_process_label = 'Manual' and termination_process = 'EXUI_USER_COMPLETION';
 
-update cft_task_db.cft_task_db.reportable_task set termination_process_label = 'Automated Cancellation' and termination_process = 'EXUI_CASE_EVENT_CANCELLATION' where final_state_label is null and termination_process is null and agent_name = 'aeabbbcb-4909-4346-acd5-4e95df4357a3' and outcome = 'Cancelled';
+update cft_task_db.cft_task_db.reportable_task set termination_process_label = 'Automated Cancellation', termination_process = 'EXUI_CASE_EVENT_CANCELLATION' where final_state_label is null and termination_process is null and agent_name = 'aeabbbcb-4909-4346-acd5-4e95df4357a3' and outcome = 'Cancelled';
 
-update cft_task_db.cft_task_db.reportable_task set termination_process_label = 'Manual Cancellation' and termination_process = 'EXUI_USER_CANCELLATION' where final_state_label ='USER_CANCELLATION' and termination_process is null  and outcome = 'Cancelled';
+update cft_task_db.cft_task_db.reportable_task set termination_process_label = 'Manual Cancellation', termination_process = 'EXUI_USER_CANCELLATION' where final_state_label = 'USER_CANCELLED' and termination_process is null  and outcome = 'Cancelled';
 select count(*) from cft_task_db.cft_task_db.reportable_task where termination_process_label in ('Automated', 'Manual') and termination_process in ('EXUI_CASE_EVENT_COMPLETION', 'EXUI_USER_COMPLETION');
 -- 0
 
-select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_label ='USER_CANCELLATION' and termination_process is null  and outcome = 'Cancelled';
+select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_label ='USER_CANCELLED' and termination_process is null  and outcome = 'Cancelled';
 -- 0
 select count(*) from cft_task_db.cft_task_db.reportable_task where final_state_label is null and termination_process is null and agent_name = 'aeabbbcb-4909-4346-acd5-4e95df4357a3' and outcome = 'Cancelled';
 -- 0
