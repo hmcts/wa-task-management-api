12d741d770568f49759d6b8f6c91bbea31fc317a
Aditya Dwadasi <131441305+adityadwadasi@users.noreply.github.com>
2026-06-16 16:15:09 +0100

    Revert "RWA-5032: Added auto vacuum config (#1637)"
    This reverts commit 4f0c607c66b2254cc59424de5945b5d6de8c58ef.

--- src/main/resources/db/migration/V1.0.43__tune_autovacuum_for_tasks_table.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE cft_task_db.tasks SET (
-    autovacuum_vacuum_scale_factor = 0.02,
-    autovacuum_analyze_scale_factor = 0.01,
-    autovacuum_vacuum_threshold = 5000,
-    autovacuum_analyze_threshold = 5000
-);
