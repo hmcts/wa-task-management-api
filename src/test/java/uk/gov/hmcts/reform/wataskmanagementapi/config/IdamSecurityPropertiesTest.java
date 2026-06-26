04faa9d25af766216b3e7c7dccb358457f3be830
Martin <martin.spasov@hmcts.net>
2026-06-26 11:16:14 +0100

    Rework IDAM security properties

--- src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/config/IdamSecurityPropertiesTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskmanagementapi/config/IdamSecurityPropertiesTest.java
@@ -9,9 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class IdamSecurityPropertiesTest {
 
-    private static final String IDAM_WEB_ISSUER = "https://idam-web-public.aat.platform.hmcts.net/o";
-    private static final String IDAM_ACCESS_ISSUER = "https://idam-access.aat.platform.hmcts.net/o";
-    private static final String FORGEROCK = "https://forgerock-am.service.core-compute-idam-perftest.internal:8443/openam/oauth2/realms/root/realms/hmcts";
+    private static final String IDAM_SECURITY_ALLOWED_ISSUER_0 = "https://idam-web-public.aat.platform.hmcts.net/o";
+    private static final String IDAM_SECURITY_ALLOWED_ISSUER_1 = "https://idam-access.aat.platform.hmcts.net/o";
+    private static final String IDAM_SECURITY_ALLOWED_ISSUER_2 = "https://forgerock-am.service.core-compute-idam-perftest.internal:8443/openam/oauth2/realms/root/realms/hmcts";
 
     private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
         .withUserConfiguration(TestConfiguration.class);
@@ -20,12 +20,12 @@ class IdamSecurityPropertiesTest {
     void shouldBindAllowedIssuers() {
         contextRunner
             .withPropertyValues(
-                "idam.security.allowed-issuers[0]=" + IDAM_WEB_ISSUER,
-                "idam.security.allowed-issuers[1]=" + IDAM_ACCESS_ISSUER,
-                "idam.security.allowed-issuers[2]=" + FORGEROCK
+                "idam.security.allowed-issuers[0]=" + IDAM_SECURITY_ALLOWED_ISSUER_0,
+                "idam.security.allowed-issuers[1]=" + IDAM_SECURITY_ALLOWED_ISSUER_1,
+                "idam.security.allowed-issuers[2]=" + IDAM_SECURITY_ALLOWED_ISSUER_2
             )
             .run(context -> assertThat(context.getBean(IdamSecurityProperties.class).getAllowedIssuers())
-                .containsExactly(IDAM_WEB_ISSUER, IDAM_ACCESS_ISSUER, FORGEROCK));
+                .containsExactly(IDAM_SECURITY_ALLOWED_ISSUER_0, IDAM_SECURITY_ALLOWED_ISSUER_1, IDAM_SECURITY_ALLOWED_ISSUER_2));
     }
 
     @Configuration
