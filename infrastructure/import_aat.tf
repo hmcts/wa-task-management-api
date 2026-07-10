dc6486b72fe0a4e8a0cd8f80961abd5a1dac8016
lars <lars.lunde@hmcts.net>
2026-07-10 12:05:53 +0100

    import aat state

---
+++ infrastructure/import_aat.tf
@@ -0,0 +1,13 @@
+import {
+  for_each = var.env == "aat" ? toset(["import"]) : toset([])
+
+  to = module.sdp_db_user.azurerm_key_vault_secret.sdp_vault_sdp_read_user_name
+  id = "https://mi-vault-dev.vault.azure.net/secrets/cft-task-postgres-db-flexible-replica-read-user-name/523938717c914b0a8fa4cda906212d54"
+}
+
+import {
+  for_each = var.env == "aat" ? toset(["import"]) : toset([])
+
+  to = module.sdp_db_user.azurerm_key_vault_secret.sdp_vault_sdp_read_user_password
+  id = "https://mi-vault-dev.vault.azure.net/secrets/cft-task-postgres-db-flexible-replica-read-user-password/4602bbc69cda4c49b21264fc23125fbd"
+}
