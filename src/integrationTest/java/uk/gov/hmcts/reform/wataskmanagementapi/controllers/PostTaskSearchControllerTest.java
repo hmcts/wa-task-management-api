506887d4b53604d9b804dd455dfc813f4bcf131f
Aditya Dwadasi <aditya.dwadasi@hmcts.net>
2026-07-20 15:45:31 +0100

    RWA-4193: Refactored the tests

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskSearchControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskmanagementapi/controllers/PostTaskSearchControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.wataskmanagementapi.controllers;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -52,7 +51,9 @@ import uk.gov.hmcts.reform.wataskmanagementapi.entity.NoteResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.entity.TaskResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.entity.TaskRoleResource;
 import uk.gov.hmcts.reform.wataskmanagementapi.entity.WorkTypeResource;
+import uk.gov.hmcts.reform.wataskmanagementapi.repository.TaskResourceRepository;
 import uk.gov.hmcts.reform.wataskmanagementapi.services.CFTTaskDatabaseService;
+import uk.gov.hmcts.reform.wataskmanagementapi.utils.IntegrationTestIndexUtils;
 import uk.gov.hmcts.reform.wataskmanagementapi.utils.IntegrationTestUtils;
 import uk.gov.hmcts.reform.wataskmanagementapi.utils.ServiceMocks;
 
@@ -120,9 +121,15 @@ class PostTaskSearchControllerTest {
     protected MockMvc mockMvc;
     @Autowired
     IntegrationTestUtils integrationTestUtils;
+
+    @Autowired
+    TaskResourceRepository taskResourceRepository;
+
     RoleAssignmentHelper roleAssignmentHelper = new RoleAssignmentHelper();
     private String taskId;
     private ServiceMocks mockServices;
+
+    IntegrationTestIndexUtils integrationTestIndexUtils = new IntegrationTestIndexUtils();
 
 
     @BeforeAll
@@ -212,7 +219,6 @@ class PostTaskSearchControllerTest {
         other with SSCS and Case.
         When a task is searched with SSCS , test returns only single result with SSCS Jurisdiction
      */
-    @Disabled
     @Test
     void should_return_single_task_when_two_role_assignments_with_one_restricted_is_given() throws Exception {
         String caseId = "searchCriteriaCaseId2";
@@ -237,6 +243,8 @@ class PostTaskSearchControllerTest {
         when(idamWebApi.token(any())).thenReturn(new Token(IDAM_AUTHORIZATION_TOKEN, "scope"));
         when(serviceAuthorisationApi.serviceToken(any())).thenReturn(SERVICE_AUTHORIZATION_TOKEN);
 
+        integrationTestIndexUtils.indexRecord(taskResourceRepository);
+
         SearchTaskRequest searchTaskRequest = new SearchTaskRequest(singletonList(
             new SearchParameterList(JURISDICTION, IN, singletonList("SSCS"))
         ));
@@ -256,7 +264,6 @@ class PostTaskSearchControllerTest {
         );
     }
 
-    @Disabled
     @Test
     void should_return_a_200_with_search_results_and_warnings() throws Exception {
         String caseId = "searchCriteriaCaseId3";
@@ -303,6 +310,8 @@ class PostTaskSearchControllerTest {
                                                                workTypeResource,
                                                                taskRoleResource);
 
+        integrationTestIndexUtils.indexRecord(taskResourceRepository);
+
         when(idamWebApi.token(any())).thenReturn(new Token(IDAM_AUTHORIZATION_TOKEN, "scope"));
         when(serviceAuthorisationApi.serviceToken(any())).thenReturn(SERVICE_AUTHORIZATION_TOKEN);
 
@@ -330,7 +339,6 @@ class PostTaskSearchControllerTest {
         );
     }
 
-    @Disabled
     @Test
     void should_return_a_200_with_limited_tasks_with_pagination() throws Exception {
         String caseId = "searchCriteriaCaseId4";
@@ -372,6 +380,8 @@ class PostTaskSearchControllerTest {
         when(idamWebApi.token(any())).thenReturn(new Token(IDAM_AUTHORIZATION_TOKEN, "scope"));
         when(serviceAuthorisationApi.serviceToken(any())).thenReturn(SERVICE_AUTHORIZATION_TOKEN);
 
+        integrationTestIndexUtils.indexRecord(taskResourceRepository);
+
         SearchTaskRequest searchTaskRequest = new SearchTaskRequest(singletonList(
             new SearchParameterList(JURISDICTION, IN, singletonList("IA"))
         ));
@@ -390,7 +400,6 @@ class PostTaskSearchControllerTest {
         );
     }
 
-    @Disabled
     @Test
     void should_return_task_with_granular_permissions_when_permission_flag_on() throws Exception {
         String caseId = "searchCriteriaCaseId4";
@@ -417,6 +426,8 @@ class PostTaskSearchControllerTest {
 
         when(idamWebApi.token(any())).thenReturn(new Token(IDAM_AUTHORIZATION_TOKEN, "scope"));
         when(serviceAuthorisationApi.serviceToken(any())).thenReturn(SERVICE_AUTHORIZATION_TOKEN);
+
+        integrationTestIndexUtils.indexRecord(taskResourceRepository);
 
         SearchTaskRequest searchTaskRequest = new SearchTaskRequest(singletonList(
             new SearchParameterList(JURISDICTION, IN, singletonList("SSCS"))
@@ -1315,7 +1326,6 @@ class PostTaskSearchControllerTest {
                 ));
     }
 
-    @Disabled
     @Test
     void should_return_200_correctly_parse_is_available_task_only_true()
         throws Exception {
@@ -1369,7 +1379,7 @@ class PostTaskSearchControllerTest {
 
         SearchRequest searchRequest = SearchTaskRequestMapper.map(expectedReq);
 
-        verify(cftQueryService, times(1)).searchForTasks(
+        verify(cftTaskDatabaseService, times(1)).searchForTasks(
             0,
             50,
             searchRequest,
@@ -1377,7 +1387,6 @@ class PostTaskSearchControllerTest {
         );
     }
 
-    @Disabled
     @Test
     void should_return_200_correctly_parse_is_available_task_only_false() throws Exception {
         UserInfo userInfo = mockServices.mockUserInfo();
@@ -1428,7 +1437,7 @@ class PostTaskSearchControllerTest {
         );
         SearchRequest searchRequest = SearchTaskRequestMapper.map(expectedReq);
 
-        verify(cftQueryService, times(1)).searchForTasks(
+        verify(cftTaskDatabaseService, times(1)).searchForTasks(
             0,
             50,
             searchRequest,
@@ -1502,7 +1511,6 @@ class PostTaskSearchControllerTest {
         );
     }
 
-    @Disabled
     @Test
     void should_return_200_given_sort_by_parameter_should_support_snake_case() throws Exception {
         UserInfo userInfo = mockServices.mockUserInfo();
@@ -1559,7 +1567,7 @@ class PostTaskSearchControllerTest {
         );
         SearchRequest searchRequest = SearchTaskRequestMapper.map(expectedReq);
 
-        verify(cftQueryService, times(1)).searchForTasks(
+        verify(cftTaskDatabaseService, times(1)).searchForTasks(
             0,
             50,
             searchRequest,
@@ -1567,7 +1575,6 @@ class PostTaskSearchControllerTest {
         );
     }
 
-    @Disabled
     @ParameterizedTest
     @EnumSource(RequestContext.class)
     void should_correctly_parse_request_context_and_return_200(RequestContext context) throws Exception {
@@ -1602,6 +1609,7 @@ class PostTaskSearchControllerTest {
                 ]
               }
             """, context.toString());
+
         mockMvc.perform(
             post("/task")
                 .header(AUTHORIZATION, IDAM_AUTHORIZATION_TOKEN)
@@ -1619,7 +1627,7 @@ class PostTaskSearchControllerTest {
 
         SearchRequest searchRequest = SearchTaskRequestMapper.map(expectedReq);
 
-        verify(cftQueryService, times(1)).searchForTasks(
+        verify(cftTaskDatabaseService, times(1)).searchForTasks(
             0,
             50,
             searchRequest,
